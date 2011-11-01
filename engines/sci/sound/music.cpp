sev-/scummvm-new

commit 8944d7d1309c1184460180440626253a17eb4b30

SCI: Fixed bug #3266480 and partially fixed bug #3041738
Hopefully, this will fix some of the oddities happening because of music fading

engines/sci/sound/music.cpp
@@ -442,6 +442,25 @@ void SciMusic::soundPlay(MusicEntry *pSnd) {
 		if (pSnd->pMidiParser) {
 			Common::StackLock lock(_mutex);
 			pSnd->pMidiParser->mainThreadBegin();
+
+			if (pSnd->status != kSoundPaused) {
+				// Stop any in progress music fading, as that will reset the
+				// volume of the sound channels that the faded song occupies..
+				// Fixes bug #3266480 and partially fixes bug #3041738.
+				for (uint i = 0; i < playListCount; i++) {
+					// Is another MIDI song being faded? If yes, stop it
+					// immediately instead
+					if (_playList[i]->fadeStep && _playList[i]->pMidiParser) {
+						_playList[i]->status = kSoundStopped;
+						if (_soundVersion <= SCI_VERSION_0_LATE)
+							_playList[i]->isQueued = false;
+						_playList[i]->pMidiParser->stop();
+						freeChannels(_playList[i]);
+						_playList[i]->fadeStep = 0;
+					}
+				}
+			}
+
 			pSnd->pMidiParser->tryToOwnChannels();
 			if (pSnd->status != kSoundPaused)
 				pSnd->pMidiParser->sendInitCommands();

commit a8b0d8006b8f70e33ce7959b4cfd017c2f559957

SCI: Fixed bug #3430834 - "SCI: KQ6: Music stops after leaving shop"
A regression from commit 8944d7d

engines/sci/sound/music.cpp
@@ -472,9 +472,9 @@ void SciMusic::soundPlay(MusicEntry *pSnd) {
 				// volume of the sound channels that the faded song occupies..
 				// Fixes bug #3266480 and partially fixes bug #3041738.
 				for (uint i = 0; i < playListCount; i++) {
-					// Is another MIDI song being faded? If yes, stop it
+					// Is another MIDI song being faded down? If yes, stop it
 					// immediately instead
-					if (_playList[i]->fadeStep && _playList[i]->pMidiParser) {
+					if (_playList[i]->fadeStep < 0 && _playList[i]->pMidiParser) {
 						_playList[i]->status = kSoundStopped;
 						if (_soundVersion <= SCI_VERSION_0_LATE)
 							_playList[i]->isQueued = false;