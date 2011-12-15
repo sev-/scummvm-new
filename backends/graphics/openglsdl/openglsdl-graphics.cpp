sev-/scummvm-new

commit 61ab9c2926d62c0329baaec2bd0e5b1b3f737b60

OPENGL: Fix aspect ratio correction behavior.
Now only 320x200 and 640x400 will result in aspect ratio correction to be used
if the user requested it. This should fix some strechting in Myst/Riven.

backends/graphics/openglsdl/openglsdl-graphics.cpp
@@ -313,14 +313,17 @@ bool OpenGLSdlGraphicsManager::loadGFXMode() {
 		_videoMode.overlayWidth = _videoMode.hardwareWidth = _videoMode.screenWidth * scaleFactor;
 		_videoMode.overlayHeight = _videoMode.hardwareHeight = _videoMode.screenHeight * scaleFactor;
 
-		int screenAspectRatio = _videoMode.screenWidth * 10000 / _videoMode.screenHeight;
-		int desiredAspectRatio = getAspectRatio();
-
-		// Do not downscale dimensions, only enlarge them if needed
-		if (screenAspectRatio > desiredAspectRatio)
-			_videoMode.hardwareHeight = (_videoMode.overlayWidth * 10000  + 5000) / desiredAspectRatio;
-		else if (screenAspectRatio < desiredAspectRatio)
-			_videoMode.hardwareWidth = (_videoMode.overlayHeight * desiredAspectRatio + 5000) / 10000;
+		// The only modes where we need to adapt the aspect ratio are 320x200
+		// and 640x400. That is since our aspect ratio correction in fact is
+		// only used to ensure that the original pixel size aspect for these
+		// modes is used.
+		// (Non-square pixels on old monitors vs square pixel on new ones).
+		if (_videoMode.aspectRatioCorrection
+		    && ((_videoMode.screenWidth == 320 && _videoMode.screenHeight == 200)
+		    || (_videoMode.screenWidth == 640 && _videoMode.screenHeight == 400)))
+			_videoMode.overlayHeight = _videoMode.hardwareHeight = 240 * scaleFactor;
+		else
+			_videoMode.overlayHeight = _videoMode.hardwareHeight = _videoMode.screenHeight * scaleFactor;
 	}
 
 	_screenResized = false;

backends/graphics/opengl/opengl-graphics.cpp
@@ -1245,12 +1245,16 @@ void OpenGLGraphicsManager::toggleAntialiasing() {
 	_transactionDetails.filterChanged = true;
 }
 
-uint OpenGLGraphicsManager::getAspectRatio() {
+uint OpenGLGraphicsManager::getAspectRatio() const {
 	// In case we enable aspect ratio correction we force a 4/3 ratio.
+	// But just for 320x200 and 640x400 games, since other games do not need
+	// this.
 	// TODO: This makes OpenGL Normal behave like OpenGL Conserve, when aspect
 	// ratio correction is enabled, but it's better than the previous 4/3 mode
 	// mess at least...
-	if (_videoMode.aspectRatioCorrection)
+	if (_videoMode.aspectRatioCorrection
+	    && ((_videoMode.screenWidth == 320 && _videoMode.screenHeight == 200)
+	    || (_videoMode.screenWidth == 640 && _videoMode.screenHeight == 400)))
 		return 13333;
 	else if (_videoMode.mode == OpenGL::GFX_NORMAL)
 		return _videoMode.hardwareWidth * 10000 / _videoMode.hardwareHeight;

backends/graphics/opengl/opengl-graphics.h
@@ -214,10 +214,7 @@ protected:
 	virtual void calculateDisplaySize(int &width, int &height);
 	virtual void refreshDisplaySize();
 
-	/**
-	 * Returns the current target aspect ratio x 10000
-	 */
-	virtual uint getAspectRatio();
+	uint getAspectRatio() const;
 
 	bool _formatBGR;
 

commit 974fbd451eefb00894eb0b788c1a359a290bc081

OPENGLSDL: Fix video mode initialization for 640x400 games with AR enabled.
Formerly they always used a height of 240 * scaleFactor, which would for
exmaple make the games requesting a 1x scaler use a height of 240 if AR
is enabled.

This was a regression from 61ab9c2926d62c0329baaec2bd0e5b1b3f737b60.

backends/graphics/openglsdl/openglsdl-graphics.cpp
@@ -318,12 +318,14 @@ bool OpenGLSdlGraphicsManager::loadGFXMode() {
 		// only used to ensure that the original pixel size aspect for these
 		// modes is used.
 		// (Non-square pixels on old monitors vs square pixel on new ones).
-		if (_videoMode.aspectRatioCorrection
-		    && ((_videoMode.screenWidth == 320 && _videoMode.screenHeight == 200)
-		    || (_videoMode.screenWidth == 640 && _videoMode.screenHeight == 400)))
-			_videoMode.overlayHeight = _videoMode.hardwareHeight = 240 * scaleFactor;
-		else
+		if (_videoMode.aspectRatioCorrection) {
+		 	if (_videoMode.screenWidth == 320 && _videoMode.screenHeight == 200)
+				_videoMode.overlayHeight = _videoMode.hardwareHeight = 240 * scaleFactor;
+			else if (_videoMode.screenWidth == 640 && _videoMode.screenHeight == 400)
+				_videoMode.overlayHeight = _videoMode.hardwareHeight = 480 * scaleFactor;
+		} else {
 			_videoMode.overlayHeight = _videoMode.hardwareHeight = _videoMode.screenHeight * scaleFactor;
+		}
 	}
 
 	_screenResized = false;