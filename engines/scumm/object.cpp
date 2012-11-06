sev-/scummvm-new

commit 7926e23741366d70b6c256b0ab9b9ad37b4ce7f8

SCUMM: use command stack and SentenceTab in mm c64
- MM C64 uses command stack (SentenceTab, doSentence()) now
- _cmdObject... added for current SentenceTab. The _active... variables are only used to build a sentence in the inventory but never by a script.
-> many routines are not needed anymore and are removed

engines/scumm/object.cpp
@@ -434,8 +434,15 @@ void ScummEngine::getObjectXYPos(int object, int &x, int &y, int &dir) {
 			y = od.y_pos + (int16)READ_LE_UINT16(&imhd->old.hotspot[state].y);
 		}
 	} else if (_game.version <= 2) {
-		x = od.walk_x >> V12_X_SHIFT;
-		y = od.walk_y >> V12_Y_SHIFT;
+		if (od.actordir) {
+			x = od.walk_x;
+			y = od.walk_y;
+		} else {
+			x = od.x_pos + od.width/2;
+			y = od.y_pos + od.height/2;
+		}
+		x = x >> V12_X_SHIFT;
+		y = y >> V12_Y_SHIFT;
 	} else {
 		x = od.walk_x;
 		y = od.walk_y;

engines/scumm/script.cpp
@@ -28,6 +28,7 @@
 #include "scumm/object.h"
 #include "scumm/resource.h"
 #include "scumm/util.h"
+#include "scumm/scumm_v0.h"
 #include "scumm/scumm_v2.h"
 #include "scumm/sound.h"
 #include "scumm/verbs.h"
@@ -194,9 +195,14 @@ int ScummEngine::getVerbEntrypoint(int obj, int entry) {
 		return verboffs + 8 + READ_LE_UINT32(ptr + 1);
 	} else if (_game.version <= 2) {
 		do {
-			if (!*verbptr)
-				return 0;
-			if (*verbptr == entry || *verbptr == 0xFF)
+			const int kFallbackEntry = (_game.version == 0 ? 0x0F : 0xFF);
+			if (!*verbptr) {
+				if (_game.version == 0 && entry == kVerbWalkTo)
+					return 13;
+				else
+					return 0;
+			}
+			if (*verbptr == entry || *verbptr == kFallbackEntry)
 				break;
 			verbptr += 2;
 		} while (1);
@@ -1126,6 +1132,130 @@ void ScummEngine::checkAndRunSentenceScript() {
 	_currentScript = 0xFF;
 	if (sentenceScript)
 		runScript(sentenceScript, 0, 0, localParamList);
+}
+
+void ScummEngine_v0::walkToActorOrObject(int object) {
+	int x, y, dir;
+	ActorC64 *a = (ActorC64 *)derefActor(VAR(VAR_EGO), "walkToObject");
+
+	_walkToObject = object;
+	_walkToObjectIdx = getObjectIndex(object);
+
+	if (OBJECT_V0_TYPE(object) == kObjectTypeActor) {
+		walkActorToActor(VAR(VAR_EGO), OBJECT_V0_NR(object), 4);
+		x = a->getRealPos().x;
+		y = a->getRealPos().y;
+	} else {
+		walkActorToObject(VAR(VAR_EGO), object);
+		getObjectXYPos(object, x, y, dir);
+	}
+
+	VAR(6) = x;
+	VAR(7) = y;
+
+	if (!(a->_miscflags & kActorMiscFlagFreeze)) {
+		// FIXME: walking already started -> should be stopped if condition not true
+		//actorStartWalk();
+	}
+}
+
+void ScummEngine_v0::checkAndRunSentenceScript() {
+	const ScriptSlot *ss;
+
+	if (_walkToObjectIdx) {
+		ActorC64 *a = (ActorC64 *)derefActor(VAR(VAR_EGO), "checkAndRunSentenceScript");
+		if (a->_moving)
+			return;
+		// TODO: change actor facing
+		_walkToObjectIdx = 0;
+		runSentenceScript();
+		return;
+	}
+
+	if (!_sentenceNum || _sentence[_sentenceNum - 1].freezeCount)
+		return;
+
+	//_sentenceNum--;
+	SentenceTab &st = _sentence[_sentenceNum - 1];
+
+	if (st.preposition && st.objectB == st.objectA)
+		return;
+
+	// FIXME: should this be executed?
+	//_currentScript = 0xFF;
+
+	int obj1Nr = OBJECT_V0_NR(st.objectA);
+	int obj1Type = OBJECT_V0_TYPE(st.objectA);
+	int obj2Nr = OBJECT_V0_NR(st.objectB);
+	int obj2Type = OBJECT_V0_TYPE(st.objectB);
+	assert(obj1Nr);
+
+	// If two objects are involved, at least one must be in the actors inventory
+	if (obj2Nr &&
+		(obj1Type != kObjectTypeFG || _objectOwnerTable[obj1Nr] != VAR(VAR_EGO)) &&
+	    (obj2Type != kObjectTypeFG || _objectOwnerTable[obj2Nr] != VAR(VAR_EGO)))
+	{
+		if (getVerbEntrypoint(st.objectA, kVerbPickUp))
+			doSentence(kVerbPickUp, st.objectA, 0);
+		else if (getVerbEntrypoint(st.objectB, kVerbPickUp))
+			doSentence(kVerbPickUp, st.objectB, 0);
+		else
+			_sentenceNum--;
+		return;
+	}
+
+	_cmdVerb = st.verb;
+	_cmdObjectNr = obj1Nr;
+	_cmdObjectType = obj1Type;
+	_cmdObject2Nr = obj2Nr;
+	_cmdObject2Type = obj2Type;
+	_sentenceNum--;
+
+	// TODO: check sentenceNum
+
+	if (whereIsObject(st.objectA) != WIO_INVENTORY) {
+		if (_currentMode != kModeKeypad) {
+			walkToActorOrObject(st.objectA);
+			return;
+		}
+	} else if (st.objectB && whereIsObject(st.objectB) != WIO_INVENTORY) {
+		walkToActorOrObject(st.objectB);
+		return;
+	}
+
+	runSentenceScript();
+	if (_currentMode == kModeKeypad) {
+		_walkToObjectIdx = 0;
+	}
+}
+
+void ScummEngine_v0::runSentenceScript() {
+	int obj = OBJECT_V0(_cmdObjectNr, _cmdObjectType);
+
+	// FIXME: should it really ever return 0xD on WalkTo, seems wrong?
+	if (getVerbEntrypoint(obj, _cmdVerb) != 0) {
+		if (_cmdVerb == kVerbRead && VAR(VAR_CURRENT_LIGHTS) == 0) {
+			//slot = 0xFF;
+			VAR(VAR_ACTIVE_VERB) = _cmdVerb;
+			runScript(3, 0, 0, 0);
+			return;
+		} else {
+			VAR(VAR_ACTIVE_ACTOR) = _cmdObject2Nr;
+			runObjectScript(obj, _cmdVerb, false, false, NULL);
+			return;
+		}
+	} else {
+		if (_cmdVerb == kVerbGive) {
+			if (_cmdObject2Nr < 8)
+				setOwnerOf(obj, _cmdObject2Nr);
+			return;
+		} else if (_cmdVerb == kVerbWalkTo) {
+			//slot = 0xFF;
+			VAR(VAR_ACTIVE_VERB) = _cmdVerb;
+			runScript(3, 0, 0, 0);
+			return;
+		}
+	}
 }
 
 void ScummEngine_v2::runInputScript(int clickArea, int val, int mode) {

engines/scumm/script_v0.cpp
@@ -177,7 +177,7 @@ void ScummEngine_v0::setupOpcodes() {
 	OPCODE(0x70, o_lights);
 	OPCODE(0x71, o_getBitVar);
 	OPCODE(0x72, o_nop);
-	OPCODE(0x73, o5_getObjectOwner);
+	OPCODE(0x73, o_getObjectOwner);
 	/* 74 */
 	OPCODE(0x74, o5_getDist);
 	OPCODE(0x75, o_printEgo_c64);
@@ -337,7 +337,7 @@ void ScummEngine_v0::setupOpcodes() {
 	OPCODE(0xf0, o_lights);
 	OPCODE(0xf1, o_getBitVar);
 	OPCODE(0xf2, o_nop);
-	OPCODE(0xf3, o5_getObjectOwner);
+	OPCODE(0xf3, o_getObjectOwner);
 	/* F4 */
 	OPCODE(0xf4, o5_getDist);
 	OPCODE(0xf5, o_stopCurrentScript);
@@ -365,7 +365,7 @@ uint ScummEngine_v0::fetchScriptWord() {
 
 int ScummEngine_v0::getActiveObject() {
 	if (_opcode & PARAM_2)
-		return _activeObjectNr;
+		return _cmdObjectNr;
 
 	return fetchScriptByte();
 }
@@ -693,7 +693,7 @@ void ScummEngine_v0::o_putActorAtObject() {
 
 void ScummEngine_v0::o_pickupObject() {
 	int objNr = fetchScriptByte();
-	int obj = OBJECT_V0((objNr ? objNr : _activeObjectNr), 0);
+	int obj = OBJECT_V0((objNr ? objNr : _cmdObjectNr), 0);
 
 	/* Don't take an object twice */
 	if (whereIsObject(obj) == WIO_INVENTORY)
@@ -742,6 +742,12 @@ void ScummEngine_v0::o_setActorBitVar() {
 	debug(0, "o_setActorBitVar(%d, %d, %d)", act, mask, mod);
 }
 
+void ScummEngine_v0::o_getObjectOwner() {
+	getResultPos();
+	int obj = getVarOrDirectWord(PARAM_1);
+	setResult(getOwner(obj ? obj : _cmdObjectNr));
+}
+
 void ScummEngine_v0::o_getActorBitVar() {
 	getResultPos();
 	byte act = getVarOrDirectByte(PARAM_1);
@@ -787,17 +793,35 @@ void ScummEngine_v0::o_printEgo_c64() {
 }
 
 void ScummEngine_v0::o_doSentence() {
-	byte entry = fetchScriptByte();
-	byte obj = fetchScriptByte();
-	fetchScriptByte();
+	byte verb = fetchScriptByte();
+	int obj, obj2;
+	byte b;
 
-	runObjectScript(obj, entry, false, false, NULL);
+	b = fetchScriptByte();
+	if (b == 0xFF) {
+		obj = OBJECT_V0(_cmdObject2Nr, _cmdObject2Type);
+	} else if (b == 0xFE) {
+		obj = OBJECT_V0(_cmdObjectNr, _cmdObjectType);
+	} else {
+		obj = OBJECT_V0(b, (_opcode & 0x80) ? 1 : 0);
+	}
+
+	b = fetchScriptByte();
+	if (b == 0xFF) {
+		obj2 = OBJECT_V0(_cmdObject2Nr, _cmdObject2Type);
+	} else if (b == 0xFE) {
+		obj2 = OBJECT_V0(_cmdObjectNr, _cmdObjectType);
+	} else {
+		obj2 = OBJECT_V0(b, (_opcode & 0x40) ? 1 : 0);
+	}
+
+	doSentence(verb, obj, obj2);
 }
 
-bool ScummEngine_v0::ifEqualActiveObject2Common(bool inventoryObject) {
+bool ScummEngine_v0::ifEqualActiveObject2Common(bool ignoreType) {
 	byte obj = fetchScriptByte();
-	if (!inventoryObject || (_activeObject2Type == kObjectTypeFG))
-		return (obj == _activeObject2Nr);
+	if (!ignoreType || (_cmdObject2Type == kObjectTypeFG))
+		return (obj == _cmdObject2Nr);
 	return false;
 }
 
@@ -894,7 +918,7 @@ void ScummEngine_v0::o_setOwnerOf() {
 	owner = getVarOrDirectByte(PARAM_2);
 
 	if (obj == 0)
-		obj = _activeObjectNr;
+		obj = _cmdObjectNr;
 
 	// FIXME: the original interpreter seems to set the owner of 
 	// an item to remove (new owner 0) to 13 (purple tentacle).
@@ -909,21 +933,11 @@ void ScummEngine_v0::o_setOwnerOf() {
 
 void ScummEngine_v0::resetSentence(bool walking) {
 	_activeVerb = kVerbWalkTo;
-
-	// If the actor is walking, or the screen is a keypad (no sentence verbs/objects are drawn)
-	// Then reset all active objects (stops the radio crash, bug #3077966)
-	if (!walking || !(_userState & 32)) {
-		_activeObjectNr = 0;
-		_activeObjectType = kObjectTypeBG;
-		_activeObject2Nr = 0;
-		_activeObject2Type = kObjectTypeBG;
-	}
-
-	_verbExecuting = false;
-	_verbPickup = false;
-
-	_activeObjectObtained = false;
-	_activeObject2Obtained = false;
+	_activeObjectNr = 0;
+	_activeObjectType = kObjectTypeBG;
+	_activeObject2Nr = 0;
+	_activeObject2Type = kObjectTypeBG;
+	_walkToObjectIdx = 0;
 }
 
 } // End of namespace Scumm

engines/scumm/script_v2.cpp
@@ -1219,20 +1219,23 @@ void ScummEngine_v2::o2_panCameraTo() {
 	panCameraTo(getVarOrDirectByte(PARAM_1) * V12_X_MULTIPLIER, 0);
 }
 
+void ScummEngine_v2::walkActorToObject(int actor, int obj) {
+	int x, y, dir;
+	getObjectXYPos(obj, x, y, dir);
+
+	Actor *a = derefActor(actor, "walkActorToObject");
+	AdjustBoxResult r = a->adjustXYToBeInBox(x, y);
+	x = r.x;
+	y = r.y;
+
+	a->startWalkActor(x, y, dir);
+}
+
 void ScummEngine_v2::o2_walkActorToObject() {
-	int obj;
-	Actor *a;
-
-	a = derefActor(getVarOrDirectByte(PARAM_1), "o2_walkActorToObject");
-	obj = getVarOrDirectWord(PARAM_2);
+	int actor = getVarOrDirectByte(PARAM_1);
+	int obj = getVarOrDirectWord(PARAM_2);
 	if (whereIsObject(obj) != WIO_NOT_FOUND) {
-		int x, y, dir;
-		getObjectXYPos(obj, x, y, dir);
-		AdjustBoxResult r = a->adjustXYToBeInBox(x, y);
-		x = r.x;
-		y = r.y;
-
-		a->startWalkActor(x, y, dir);
+		walkActorToObject(actor, obj);
 	}
 }
 

engines/scumm/script_v5.cpp
@@ -2464,8 +2464,43 @@ void ScummEngine_v5::o5_walkActorTo() {
 	a->startWalkActor(x, y, -1);
 }
 
+void ScummEngine_v5::walkActorToActor(int actor, int toActor, int dist) {
+	Actor *a = derefActor(actor, "walkActorToActor");
+	Actor *to = derefActor(toActor, "walkActorToActor(2)");
+
+	if (_game.version <= 2) {
+		dist *= V12_X_MULTIPLIER;
+	} else if (dist == 0xFF) {
+		dist = a->_scalex * a->_width / 0xFF;
+		dist += (to->_scalex * to->_width / 0xFF) / 2;
+	}
+	int x = to->getPos().x;
+	int y = to->getPos().y;
+	if (x < a->getPos().x)
+		x += dist;
+	else
+		x -= dist;
+
+	if (_game.version <= 2) {
+		x /= V12_X_MULTIPLIER;
+		y /= V12_Y_MULTIPLIER;
+	}
+	if (_game.version <= 3) {
+		AdjustBoxResult abr = a->adjustXYToBeInBox(x, y);
+		x = abr.x;
+		y = abr.y;
+	}
+	a->startWalkActor(x, y, -1);
+
+	// WORKAROUND: See bug #2971126 for details on why this is here.
+	if (_game.version == 0) {
+		// FIXME(TOBIAS): is this still needed?
+		// (updateScriptPtr/_currentScript might now be called automatically) 
+		o5_breakHere();
+	}
+}
+
 void ScummEngine_v5::o5_walkActorToActor() {
-	int x, y;
 	Actor *a, *a2;
 	int nr = getVarOrDirectByte(PARAM_1);
 	int nr2 = getVarOrDirectByte(PARAM_2);
@@ -2499,33 +2534,7 @@ void ScummEngine_v5::o5_walkActorToActor() {
 	if (!a2->isInCurrentRoom())
 		return;
 
-	if (_game.version <= 2) {
-		dist *= V12_X_MULTIPLIER;
-	} else if (dist == 0xFF) {
-		dist = a->_scalex * a->_width / 0xFF;
-		dist += (a2->_scalex * a2->_width / 0xFF) / 2;
-	}
-	x = a2->getPos().x;
-	y = a2->getPos().y;
-	if (x < a->getPos().x)
-		x += dist;
-	else
-		x -= dist;
-
-	if (_game.version <= 2) {
-		x /= V12_X_MULTIPLIER;
-		y /= V12_Y_MULTIPLIER;
-	}
-	if (_game.version <= 3) {
-		AdjustBoxResult abr = a->adjustXYToBeInBox(x, y);
-		x = abr.x;
-		y = abr.y;
-	}
-	a->startWalkActor(x, y, -1);
-
-	// WORKAROUND: See bug #2971126 for details on why this is here.
-	if (_game.version == 0)
-		o5_breakHere();
+	walkActorToActor(nr, nr2, dist);
 }
 
 void ScummEngine_v5::o5_walkActorToObject() {

engines/scumm/scumm.cpp
@@ -712,17 +712,19 @@ ScummEngine_v2::ScummEngine_v2(OSystem *syst, const DetectorResult &dr)
 ScummEngine_v0::ScummEngine_v0(OSystem *syst, const DetectorResult &dr)
 	: ScummEngine_v2(syst, dr) {
 
-	_verbExecuting = false;
-	_verbPickup = false;
 	_currentMode = 0;
 
 	_activeVerb = kVerbNone;
 	_activeObjectNr = 0;
+	_activeObjectType = 0;
 	_activeObject2Nr = 0;
-	_activeObjectType = kObjectTypeBG;
-	_activeObject2Type = kObjectTypeBG;
-	_activeObjectObtained = false;
-	_activeObject2Obtained = false;
+	_activeObject2Type = 0;
+
+	_cmdVerb = kVerbNone;
+	_cmdObjectNr = 0;
+	_cmdObjectType = 0;
+	_cmdObject2Nr = 0;
+	_cmdObject2Type = 0;
 
 	VAR_ACTIVE_ACTOR = 0xFF;
 	VAR_IS_SOUND_RUNNING = 0xFF;

engines/scumm/scumm.h
@@ -642,7 +642,7 @@ protected:
 	void updateScriptPtr();
 	virtual void runInventoryScript(int i);
 	void inventoryScriptIndy3Mac();
-	void checkAndRunSentenceScript();
+	virtual void checkAndRunSentenceScript();
 	void runExitScript();
 	void runEntryScript();
 	void runAllScripts();

engines/scumm/scumm_v0.h
@@ -54,8 +54,6 @@ protected:
 
 protected:
 	byte _currentMode;
-	bool _verbExecuting;			// is a verb executing
-	bool _verbPickup;				// are we picking up an object during a verb execute
 
 	int _activeVerb;
 	int _activeObjectNr;			// 1st Object Number
@@ -63,8 +61,14 @@ protected:
 	int _activeObject2Nr;			// 2nd Object Number
 	int _activeObject2Type;			// 2nd Object Type (0: inventory (or room), 1: room, 2: actor)
 
-	bool _activeObjectObtained;		// collected _activeobject?
-	bool _activeObject2Obtained;	// collected _activeObject2?
+	int _cmdVerb;
+	int _cmdObjectNr;
+	int _cmdObjectType;
+	int _cmdObject2Nr;
+	int _cmdObject2Type;
+
+	int _walkToObject;
+	int _walkToObjectIdx;
 
 public:
 	ScummEngine_v0(OSystem *syst, const DetectorResult &dr);
@@ -83,17 +87,16 @@ protected:
 
 	virtual void processInput();
 
-	virtual void runObject(int obj, int entry);
 	virtual void saveOrLoad(Serializer *s);
 
 	// V0 MM Verb commands
 	int getVerbPrepId();
 	int activeVerbPrep();
-	bool verbMove(int object);
-	bool verbMoveToActor(int actor);
-	bool verbObtain(int object);
-	bool verbExec();
+	void walkToActorOrObject(int object);
+	void verbExec();
 
+	virtual void runSentenceScript();
+	virtual void checkAndRunSentenceScript();
 	virtual void checkExecVerbs();
 	virtual void handleMouseOver(bool updateInventory);
 	int verbPrepIdType(int verbid);
@@ -138,6 +141,7 @@ protected:
 	void o_unlockScript();
 	void o_decrement();
 	void o_nop();
+	void o_getObjectOwner();
 	void o_getActorBitVar();
 	void o_setActorBitVar();
 	void o_getBitVar();

commit 6b5d0d219b2482b23041279d27588a641c52ea94

SCUMM: Fix bug #3526089 - "SCUMM: ZAK Can't get objects in the bus on Mars"
This fixes a regression in V1/V2 games when no actor direction
is set (like the Shuttle Bus scene in Zak V2). The regression
was caused by commit 7926e23741366d70b6c256b0ab9b9ad37b4ce7f8.
Thanks to digitall for his bisecting work, which aided a lot in
finding the actual issue for this bug

engines/scumm/object.cpp
@@ -433,10 +433,14 @@ void ScummEngine::getObjectXYPos(int object, int &x, int &y, int &dir) {
 			y = od.y_pos + (int16)READ_LE_UINT16(&imhd->old.hotspot[state].y);
 		}
 	} else if (_game.version <= 2) {
-		if (od.actordir) {
-			x = od.walk_x;
-			y = od.walk_y;
-		} else {
+		x = od.walk_x;
+		y = od.walk_y;
+
+		// Adjust x, y when no actor direction is set, but only perform this
+		// adjustment for V0 games (e.g. MM C64), otherwise certain scenes in
+		// newer games are affected as well (e.g. the interior of the Shuttle
+		// Bus scene in Zak V2, where no actor is present). Refer to bug #3526089.
+		if (!od.actordir && _game.version == 0) {
 			x = od.x_pos + od.width / 2;
 			y = od.y_pos + od.height / 2;
 		}