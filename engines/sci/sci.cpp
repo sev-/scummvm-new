sev-/scummvm-new

commit bb705dc1a1b1afce0e49f83c342b855ec57d067b

SCI: Resync audio options when a game is restarted (bug #3589453)

engines/sci/sci.cpp
@@ -683,9 +683,8 @@ void SciEngine::runGame() {
 	if (DebugMan.isDebugChannelEnabled(kDebugLevelOnStartup))
 		_console->attach();
 
-	_gamestate->_syncedAudioOptions = false;
-
 	do {
+		_gamestate->_syncedAudioOptions = false;
 		_gamestate->_executionStackPosChanged = false;
 		run_vm(_gamestate);
 		exitGame();

commit 71a8f6d8d7445edc17209fafd90495b4056e5f66

SCI: Fix a small glitch when loading (regression)
This is caused by commit bb705dc1a1b1afce0e49f83c342b855ec57d067b,
and caused players to click an extra time when trying to change the
game audio options after loading a game

engines/sci/sci.cpp
@@ -683,8 +683,9 @@ void SciEngine::runGame() {
 	if (DebugMan.isDebugChannelEnabled(kDebugLevelOnStartup))
 		_console->attach();
 
+	_gamestate->_syncedAudioOptions = false;
+
 	do {
-		_gamestate->_syncedAudioOptions = false;
 		_gamestate->_executionStackPosChanged = false;
 		run_vm(_gamestate);
 		exitGame();
@@ -702,6 +703,7 @@ void SciEngine::runGame() {
 			if (_gfxMenu)
 				_gfxMenu->reset();
 			_gamestate->abortScriptProcessing = kAbortNone;
+			_gamestate->_syncedAudioOptions = false;
 		} else if (_gamestate->abortScriptProcessing == kAbortLoadGame) {
 			_gamestate->abortScriptProcessing = kAbortNone;
 			_gamestate->_executionStack.clear();
@@ -713,6 +715,7 @@ void SciEngine::runGame() {
 
 			syncSoundSettings();
 			syncIngameAudioOptions();
+			// Games do not set their audio settings when loading
 		} else {
 			break;	// exit loop
 		}