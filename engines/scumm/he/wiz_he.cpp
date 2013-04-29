sev-/scummvm-new

commit 51c439e1d398294d363c6d3b971aa86c0c14baf7

SCUMM HE: Plug memory leak. CID 1003582

engines/scumm/he/wiz_he.cpp
@@ -1548,12 +1548,16 @@ uint8 *Wiz::drawWizImage(int resNum, int state, int maskNum, int maskState, int 
 		if (rScreen.intersects(clip)) {
 			rScreen.clip(clip);
 		} else {
+			free(dst);
+
 			return 0;
 		}
 	} else if (_rectOverrideEnabled) {
 		if (rScreen.intersects(_rectOverride)) {
 			rScreen.clip(_rectOverride);
 		} else {
+			free(dst);
+
 			return 0;
 		}
 	}

commit d769253e67a3f7875eacac4a25125bd8d64256e7

SCUMM HE: fix crash introduced in 51c439e1d39

engines/scumm/he/wiz_he.cpp
@@ -1548,7 +1548,8 @@ uint8 *Wiz::drawWizImage(int resNum, int state, int maskNum, int maskState, int 
 		if (rScreen.intersects(clip)) {
 			rScreen.clip(clip);
 		} else {
-			free(dst);
+			if (flags & kWIFBlitToMemBuffer)
+				free(dst);
 
 			return 0;
 		}
@@ -1556,7 +1557,8 @@ uint8 *Wiz::drawWizImage(int resNum, int state, int maskNum, int maskState, int 
 		if (rScreen.intersects(_rectOverride)) {
 			rScreen.clip(_rectOverride);
 		} else {
-			free(dst);
+			if (flags & kWIFBlitToMemBuffer)
+				free(dst);
 
 			return 0;
 		}