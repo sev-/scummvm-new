sev-/scummvm-new

commit 9a8fcf199277a7e00a9b10c24ccf9581c2041efc

KYRA: clean up audio resource handling
(this was really ugly, now it's somewhat less ugly)

engines/kyra/staticres.cpp
@@ -778,22 +778,6 @@ void KyraEngine_LoK::initStaticResource() {
 
 	_storyStrings = _staticres->loadStrings(k1PC98StoryStrings, _storyStringsSize);
 
-	int size1, size2;
-	const char *const *soundfiles1 = _staticres->loadStrings(k1AudioTracks, size1);
-	const char *const *soundfiles2 = _staticres->loadStrings(k1AudioTracks2, size2);
-	_soundFilesSize = size1 + size2;
-	if (_soundFilesSize) {
-		delete[] _soundFiles;
-		const char **soundfiles = new const char*[_soundFilesSize];
-		for (int i = 0; i < _soundFilesSize; i++)
-			soundfiles[i] = (i < size1) ? soundfiles1[i] : soundfiles2[i - size1];
-		_soundFiles = soundfiles;
-	}
-	_soundFilesIntro = _staticres->loadStrings(k1AudioTracksIntro, _soundFilesIntroSize);
-	_cdaTrackTable = (const int32 *)_staticres->loadRawData(k1TownsCDATable, _cdaTrackTableSize);
-
-	// copied static res
-
 	// room list
 	const Room *tempRoomList = _staticres->loadRoomTable(k1RoomList, _roomTableSize);
 
@@ -820,34 +804,34 @@ void KyraEngine_LoK::initStaticResource() {
 		_staticres->unloadId(k1DefaultShapes);
 	}
 
-	// audio data tables
-	static const char *const tIntro98[] = { "INTRO%d.DAT" };
-	static const char *const tIngame98[] = { "KYRAM%d.DAT" };
+	// audio resource assignment
+	int size1, size2;
+	const char *const *soundfiles1 = _staticres->loadStrings(k1AudioTracks, size1);
+	const char *const *soundfiles2 = _staticres->loadStrings(k1AudioTracks2, size2);
+	int soundFilesSize = size1 + size2;
+	int soundFilesIntroSize = 0;
+	int cdaTableSize = 0;
+	const char **soundFiles = 0;
+
+	if (soundFilesSize) {
+		soundFiles = new const char*[soundFilesSize];
+		for (int i = 0; i < soundFilesSize; i++)
+			soundFiles[i] = (i < size1) ? soundfiles1[i] : soundfiles2[i - size1];
+	}
+	const char *const *soundFilesIntro = _staticres->loadStrings(k1AudioTracksIntro, temp);
+	const int32 *cdaTable = (const int32 *)_staticres->loadRawData(k1TownsCDATable, cdaTableSize);
 
 	// FIXME: It seems Kyra1 MAC CD includes AdLib and MIDI music and sfx, thus we enable
 	// support for those for now. (Based on patch #2767489 "Support for Mac Kyrandia 1 CD" by satz).
-	memset(_soundData, 0, sizeof(_soundData));
 	if (_flags.platform == Common::kPlatformPC || _flags.platform == Common::kPlatformMacintosh) {
-		_soundData[0].fileList = _soundFilesIntro;
-		_soundData[0].fileListLen = _soundFilesIntroSize;
-		_soundData[1].fileList = _soundFiles;
-		_soundData[1].fileListLen = _soundFilesSize;
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_PC(soundFilesIntro, soundFilesIntroSize));
+		_sound->initAudioResourceInfo(kMusicIngame, &SoundResourceInfo_PC(soundFiles, soundFilesSize));
 	} else if (_flags.platform == Common::kPlatformFMTowns) {
-		_soundData[0].fileList = _soundFiles;
-		_soundData[0].fileListLen = _soundFilesSize;
-		_soundData[0].cdaTracks = _cdaTrackTable;
-		_soundData[0].cdaNumTracks = _cdaTrackTableSize;
-		_soundData[1].fileList = _soundFiles;
-		_soundData[1].fileListLen = _soundFilesSize;
-		_soundData[1].cdaTracks = _cdaTrackTable;
-		_soundData[1].cdaNumTracks = _cdaTrackTableSize;
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_Towns(soundFiles, soundFilesSize, cdaTable, cdaTableSize));
+		_sound->initAudioResourceInfo(kMusicIngame, &SoundResourceInfo_Towns(soundFiles, soundFilesSize, cdaTable, cdaTableSize));
 	} else if (_flags.platform == Common::kPlatformPC98) {
-		_soundData[0].fileList = tIntro98;
-		_soundData[0].fileListLen = 1;
-		_soundData[0].extraOffset = -1;
-		_soundData[1].fileList = tIngame98;
-		_soundData[1].fileListLen = 1;
-		_soundData[1].extraOffset = -1;
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_PC98("INTRO%d.DAT"));
+		_sound->initAudioResourceInfo(kMusicIngame, &SoundResourceInfo_PC98("KYRAM%d.DAT"));
 	}
 }
 
@@ -1009,42 +993,18 @@ void KyraEngine_HoF::initStaticResource() {
 	_itemAnimDefinition = _staticres->loadItemAnimDefinition(k2IngameShapeAnimData, _itemAnimDefinitionSize);
 
 	// assign music data
-	static const char *const fmtMusicFileListIntro[] = { "intro%d.twn" };
-	static const char *const fmtMusicFileListFinale[] = { "finale%d.twn" };
-	static const char *const fmtMusicFileListIngame[] = { "km%02d.twn" };
-
-	static const char *const pc98MusicFileListIntro[] = { "intro%d.86" };
-	static const char *const pc98MusicFileListFinale[] = { "finale%d.86" };
-	static const char *const pc98MusicFileListIngame[] = { "km%02d.86" };
-
-	memset(_soundData, 0, sizeof(_soundData));
 	if (_flags.platform == Common::kPlatformPC) {
-		_soundData[0].fileList = _musicFileListIntro;
-		_soundData[0].fileListLen = _musicFileListIntroSize;
-		_soundData[1].fileList = _musicFileListIngame;
-		_soundData[1].fileListLen = _musicFileListIngameSize;
-		_soundData[2].fileList = _musicFileListFinale;
-		_soundData[2].fileListLen = _musicFileListIntroSize;
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_PC(_musicFileListIntro, _musicFileListIntroSize));
+		_sound->initAudioResourceInfo(kMusicIngame, &SoundResourceInfo_PC(_musicFileListIngame, _musicFileListIngameSize));
+		_sound->initAudioResourceInfo(kMusicFinale, &SoundResourceInfo_PC(_musicFileListFinale, _musicFileListFinaleSize));
 	} else if (_flags.platform == Common::kPlatformFMTowns) {
-		_soundData[0].fileList = fmtMusicFileListIntro;
-		_soundData[0].fileListLen = 1;
-		_soundData[0].cdaTracks = _cdaTrackTableIntro;
-		_soundData[0].cdaNumTracks = _cdaTrackTableIntroSize >> 1;
-		_soundData[1].fileList = fmtMusicFileListIngame;
-		_soundData[1].fileListLen = 1;
-		_soundData[1].cdaTracks = _cdaTrackTableIngame;
-		_soundData[1].cdaNumTracks = _cdaTrackTableIngameSize >> 1;
-		_soundData[2].fileList = fmtMusicFileListFinale;
-		_soundData[2].fileListLen = 1;
-		_soundData[2].cdaTracks = _cdaTrackTableFinale;
-		_soundData[2].cdaNumTracks = _cdaTrackTableFinaleSize >> 1;
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_TownsPC98V2(0, 0, "intro%d.twn", (const uint16*)_cdaTrackTableIntro, _cdaTrackTableIntroSize >> 1));
+		_sound->initAudioResourceInfo(kMusicIngame, &SoundResourceInfo_TownsPC98V2(0, 0, "km%02d.twn", (const uint16*)_cdaTrackTableIngame, _cdaTrackTableIngameSize >> 1));
+		_sound->initAudioResourceInfo(kMusicFinale, &SoundResourceInfo_TownsPC98V2(0, 0, "finale%d.twn", (const uint16*)_cdaTrackTableFinale, _cdaTrackTableFinaleSize >> 1));
 	} else if (_flags.platform == Common::kPlatformPC98) {
-		_soundData[0].fileList = pc98MusicFileListIntro;
-		_soundData[0].fileListLen = 1;
-		_soundData[1].fileList = pc98MusicFileListIngame;
-		_soundData[1].fileListLen = 1;
-		_soundData[2].fileList = pc98MusicFileListFinale;
-		_soundData[2].fileListLen = 1;
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_TownsPC98V2(0, 0, "intro%d.86", 0, 0));
+		_sound->initAudioResourceInfo(kMusicIngame, &SoundResourceInfo_TownsPC98V2(0, 0, "km%02d.86", 0, 0));
+		_sound->initAudioResourceInfo(kMusicFinale, &SoundResourceInfo_TownsPC98V2(0, 0, "finale%d.86", 0, 0));
 	}
 }
 

engines/kyra/kyra_hof.cpp
@@ -274,7 +274,7 @@ Common::Error KyraEngine_HoF::go() {
 }
 
 void KyraEngine_HoF::startup() {
-	_sound->setSoundList(&_soundData[kMusicIngame]);
+	_sound->selectAudioResourceSet(kMusicIngame);
 	// The track map is exactly the same
 	// for FM-TOWNS and DOS
 	_trackMap = _dosTrackMap;

engines/kyra/kyra_hof.h
@@ -57,7 +57,6 @@ public:
 	GUI *gui() const { return _gui; }
 	virtual TextDisplayer *text() { return _text; }
 	int language() const { return _lang; }
-	const AudioDataStruct *soundData(int index) { return &_soundData[index]; }
 
 protected:
 	static const EngineDesc _hofEngineDesc;
@@ -86,8 +85,6 @@ protected:
 	static const int _gmSfxMapSize;
 	static const int8 _pcSpkSfxMap[];
 	static const int _pcSpkSfxMapSize;
-
-	AudioDataStruct _soundData[3];
 
 protected:
 	// game initialization

engines/kyra/kyra_lok.cpp
@@ -98,8 +98,6 @@ KyraEngine_LoK::KyraEngine_LoK(OSystem *system, const GameFlags &flags)
 
 	_malcolmFrame = 0;
 	_malcolmTimer1 = _malcolmTimer2 = 0;
-
-	_soundFiles = 0;
 }
 
 KyraEngine_LoK::~KyraEngine_LoK() {
@@ -122,8 +120,6 @@ KyraEngine_LoK::~KyraEngine_LoK() {
 	delete _sprites;
 	delete _animator;
 	delete _seq;
-
-	delete[] _soundFiles;
 
 	delete[] _characterList;
 
@@ -194,7 +190,7 @@ Common::Error KyraEngine_LoK::init() {
 
 	initStaticResource();
 
-	_sound->setSoundList(&_soundData[kMusicIntro]);
+	_sound->selectAudioResourceSet(kMusicIntro);
 
 	if (_flags.platform == Common::kPlatformAmiga) {
 		_trackMap = _amigaTrackMap;
@@ -349,7 +345,7 @@ void KyraEngine_LoK::startup() {
 	static const uint8 colorMap[] = { 0, 0, 0, 0, 12, 12, 12, 0, 0, 0, 0, 0 };
 	_screen->setTextColorMap(colorMap);
 
-	_sound->setSoundList(&_soundData[kMusicIngame]);
+	_sound->selectAudioResourceSet(kMusicIngame);
 	if (_flags.platform == Common::kPlatformPC98)
 		_sound->loadSoundFile("SE.DAT");
 	else

engines/kyra/kyra_lok.h
@@ -134,8 +134,6 @@ public:
 	const uint8 * const *palTable1() { return &_specialPalettes[0]; }
 	const uint8 * const *palTable2() { return &_specialPalettes[29]; }
 
-	const AudioDataStruct *soundData(int index) { return &_soundData[index]; }
-
 protected:
 	virtual Common::Error go();
 	virtual Common::Error init();
@@ -644,14 +642,6 @@ protected:
 	const uint8 *_amuleteAnim;
 
 	const uint8 * const *_specialPalettes;
-
-	const char * const *_soundFiles;
-	int _soundFilesSize;
-	const char * const *_soundFilesIntro;
-	int _soundFilesIntroSize;
-	const int32 *_cdaTrackTable;
-	int _cdaTrackTableSize;
-	AudioDataStruct _soundData[3];
 
 	// positions of the inventory
 	static const uint16 _itemPosX[];

engines/kyra/kyra_v1.h
@@ -140,14 +140,6 @@ enum {
 	GI_EOB2 = 6
 };
 
-struct AudioDataStruct {
-	const char *const *fileList;
-	int fileListLen;
-	const void *cdaTracks;
-	int cdaNumTracks;
-	int extraOffset;
-};
-
 // TODO: this is just the start of makeing the debug output of the kyra engine a bit more useable
 // in the future we maybe merge some flags  and/or create new ones
 enum DebugLevels {
@@ -164,7 +156,7 @@ enum DebugLevels {
 	kDebugLevelTimer       = 1 << 10  ///< debug level for "TimerManager" functions
 };
 
-enum MusicDataID {
+enum AudioResourceSet {
 	kMusicIntro = 0,
 	kMusicIngame,
 	kMusicFinale
@@ -242,8 +234,6 @@ public:
 	virtual void snd_playVoiceFile(int id) = 0;
 	virtual bool snd_voiceIsPlaying();
 	virtual void snd_stopVoice();
-
-	virtual const AudioDataStruct *soundData(int index) { return 0; }
 
 	// delay functionallity
 	virtual void delayUntil(uint32 timestamp, bool updateGameTimers = false, bool update = false, bool isMainLoop = false);

engines/kyra/lol.cpp
@@ -534,7 +534,7 @@ Common::Error LoLEngine::go() {
 	if (_flags.platform == Common::kPlatformPC98)
 		_sound->loadSoundFile("sound.dat");
 
-	_sound->setSoundList(&_soundData[kMusicIngame]);
+	_sound->selectAudioResourceSet(kMusicIngame);
 	if (_flags.platform != Common::kPlatformPC)
 		_sound->loadSoundFile(0);
 

engines/kyra/lol.h
@@ -275,8 +275,6 @@ public:
 	Screen *screen();
 	GUI *gui() const;
 
-	const AudioDataStruct *soundData(int index) { return &_soundData[index]; }
-
 private:
 	Screen_LoL *_screen;
 	GUI_LoL *_gui;
@@ -471,8 +469,6 @@ private:
 	int _ingameMT32SoundIndexSize;
 	const uint8 *_ingamePCSpeakerSoundIndex;
 	int _ingamePCSpeakerSoundIndexSize;
-
-	AudioDataStruct _soundData[3];
 
 	// gui
 	void gui_drawPlayField();

commit 42c38bdd2cc8efbe3aa6ec6de2ffb689146aac7e

KYRA: Fix missing music in Kyra 1 DOS intro.
This is a regression from 9a8fcf199277a7e00a9b10c24ccf9581c2041efc.
Thanks to eriktorbjorn to noticing that the music is missing.

engines/kyra/staticres.cpp
@@ -818,7 +818,7 @@ void KyraEngine_LoK::initStaticResource() {
 		for (int i = 0; i < soundFilesSize; i++)
 			soundFiles[i] = (i < size1) ? soundfiles1[i] : soundfiles2[i - size1];
 	}
-	const char *const *soundFilesIntro = _staticres->loadStrings(k1AudioTracksIntro, temp);
+	const char *const *soundFilesIntro = _staticres->loadStrings(k1AudioTracksIntro, soundFilesIntroSize);
 	const int32 *cdaTable = (const int32 *)_staticres->loadRawData(k1TownsCDATable, cdaTableSize);
 
 	// FIXME: It seems Kyra1 MAC CD includes AdLib and MIDI music and sfx, thus we enable