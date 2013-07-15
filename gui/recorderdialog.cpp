sev-/scummvm-new

commit 247a78403329fc2dafc3c591a0fb90d927021032

RECORDER: Fix CID 1046887. Missing break in switch

gui/recorderdialog.cpp
@@ -191,6 +191,7 @@ void RecorderDialog::handleCommand(CommandSender *sender, uint32 cmd, uint32 dat
 		break;
 	case kCloseCmd:
 		setResult(kRecordDialogClose);
+		break;
 	default:
 		Dialog::handleCommand(sender, cmd, data);
 		}

commit 9f6a51daa03a4c16295ccd0e1c5629a1c1f529a3

GUI: Allow the event recorder dialog to be closed.
This is a regression from 247a78403329fc2dafc3c591a0fb90d927021032.

setResult does not automatically close the dialog. However,
Dialog::handleCommand will close the dialog when kCloseCmd is received.

gui/recorderdialog.cpp
@@ -191,7 +191,7 @@ void RecorderDialog::handleCommand(CommandSender *sender, uint32 cmd, uint32 dat
 		break;
 	case kCloseCmd:
 		setResult(kRecordDialogClose);
-		break;
+		// Fall through
 	default:
 		Dialog::handleCommand(sender, cmd, data);
 	}