sev-/scummvm-new

commit dec13e428ab4e0b935eb8673b8452cf13931f3cf

PEGASUS: Reset _lastMillis when changing rate in the timer

engines/pegasus/timers.cpp
@@ -93,6 +93,7 @@ TimeValue TimeBase::getTime(const TimeScale scale) {
 
 void TimeBase::setRate(const Common::Rational rate) {
 	_rate = rate;
+	_lastMillis = 0;
 
 	if (_rate == 0)
 		_paused = false;

commit b30eed001697c82bc7fda755ca8c7236101f4f80

PEGASUS: Fix possible timer "skip" when resuming
pause/resume need to treat things a bit differently from the normal setRate function. This caused a jump in a timer when resuming from the pause menu in certain cases, though not all the time.

Regression from dec13e428ab4e0b935eb8673b8452cf13931f3cf, which was part of the fix for the lid animations.

engines/pegasus/timers.cpp
@@ -115,7 +115,7 @@ void TimeBase::stop() {
 void TimeBase::pause() {
 	if (isRunning() && !_paused) {
 		_pausedRate = getRate();
-		stop();
+		_rate = 0;
 		_paused = true;
 		_pauseStart = g_system->getMillis();
 	}
@@ -123,7 +123,7 @@ void TimeBase::pause() {
 
 void TimeBase::resume() {
 	if (_paused) {
-		setRate(_pausedRate);
+		_rate = _pausedRate;
 		_paused = false;
 
 		if (isRunning())