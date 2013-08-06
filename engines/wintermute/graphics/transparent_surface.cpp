sev-/scummvm-new

commit bc3d9c1fecd188fe1f04d2af03e0684560fed49a

WINTERMUTE: Prefer getBasePtr over direct Surface::pixels access.

engines/wintermute/graphics/transparent_surface.cpp
@@ -158,7 +158,10 @@ TransparentSurface::TransparentSurface(const Surface &surf, bool copyData) : Sur
 		h = surf.h;
 		pitch = surf.pitch;
 		format = surf.format;
-		pixels = surf.pixels;
+		// We need to cast the const qualifier away here because 'pixels'
+		// always needs to be writable. 'surf' however is a constant Surface,
+		// thus getBasePtr will always return const pixel data.
+		pixels = const_cast<void *>(surf.getBasePtr(0, 0));
 	}
 }
 
@@ -307,7 +310,7 @@ Common::Rect TransparentSurface::blit(Graphics::Surface &target, int posX, int p
 	}
 
 	if (pPartRect) {
-		srcImage.pixels = &((char *)pixels)[pPartRect->top * srcImage.pitch + pPartRect->left * 4];
+		srcImage.pixels = getBasePtr(pPartRect->top, pPartRect->left);
 		srcImage.w = pPartRect->width();
 		srcImage.h = pPartRect->height();
 
@@ -336,7 +339,7 @@ Common::Rect TransparentSurface::blit(Graphics::Surface &target, int posX, int p
 	if ((width != srcImage.w) || (height != srcImage.h)) {
 		// Scale the image
 		img = imgScaled = srcImage.scale(width, height);
-		savedPixels = (byte *)img->pixels;
+		savedPixels = (byte *)img->getBasePtr(0, 0);
 	} else {
 		img = &srcImage;
 	}
@@ -344,13 +347,13 @@ Common::Rect TransparentSurface::blit(Graphics::Surface &target, int posX, int p
 	// Handle off-screen clipping
 	if (posY < 0) {
 		img->h = MAX(0, (int)img->h - -posY);
-		img->pixels = (byte *)img->pixels + img->pitch * -posY;
+		img->pixels = (byte *)img->getBasePtr(0, -posY);
 		posY = 0;
 	}
 
 	if (posX < 0) {
 		img->w = MAX(0, (int)img->w - -posX);
-		img->pixels = (byte *)img->pixels + (-posX * 4);
+		img->pixels = (byte *)img->getBasePtr(-posX, 0);
 		posX = 0;
 	}
 

engines/wintermute/base/gfx/osystem/base_render_osystem.cpp
@@ -187,9 +187,9 @@ bool BaseRenderOSystem::flip() {
 	}
 	if (_needsFlip || _disableDirtyRects || _tempDisableDirtyRects) {
 		if (_disableDirtyRects || _tempDisableDirtyRects) {
-			g_system->copyRectToScreen((byte *)_renderSurface->pixels, _renderSurface->pitch, 0, 0, _renderSurface->w, _renderSurface->h);
+			g_system->copyRectToScreen((byte *)_renderSurface->getBasePtr(0, 0), _renderSurface->pitch, 0, 0, _renderSurface->w, _renderSurface->h);
 		}
-		//  g_system->copyRectToScreen((byte *)_renderSurface->pixels, _renderSurface->pitch, _dirtyRect->left, _dirtyRect->top, _dirtyRect->width(), _dirtyRect->height());
+		//  g_system->copyRectToScreen((byte *)_renderSurface->getBasePtr(0, 0), _renderSurface->pitch, _dirtyRect->left, _dirtyRect->top, _dirtyRect->width(), _dirtyRect->height());
 		delete _dirtyRect;
 		_dirtyRect = nullptr;
 		g_system->updateScreen();
@@ -682,7 +682,7 @@ void BaseRenderOSystem::endSaveLoad() {
 	_drawNum = 1;
 
 	_renderSurface->fillRect(Common::Rect(0, 0, _renderSurface->h, _renderSurface->w), _renderSurface->format.ARGBToColor(255, 0, 0, 0));
-	g_system->copyRectToScreen((byte *)_renderSurface->pixels, _renderSurface->pitch, 0, 0, _renderSurface->w, _renderSurface->h);
+	g_system->copyRectToScreen((byte *)_renderSurface->getBasePtr(0, 0), _renderSurface->pitch, 0, 0, _renderSurface->w, _renderSurface->h);
 	g_system->updateScreen();
 }
 

engines/wintermute/base/gfx/osystem/base_surface_osystem.cpp
@@ -234,7 +234,7 @@ uint32 BaseSurfaceOSystem::getPixelAt(Graphics::Surface *surface, int x, int y) 
 	warning("BaseSurfaceOSystem::GetPixel - Not ported yet");
 	int bpp = surface->format.bytesPerPixel;
 	/* Here p is the address to the pixel we want to retrieve */
-	uint8 *p = (uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+	uint8 *p = (uint8 *)surface->getBasePtr(x, y);
 
 	switch (bpp) {
 	case 1:

commit 37dfb7d78357e547e21ec5296f197cb42ccabc11

WINTERMUTE: Fix incorrect parameter order for getBasePtr.
This is a regression from bc3d9c1fecd188fe1f04d2af03e0684560fed49a. Thanks to
fuzzie for noticing.

engines/wintermute/graphics/transparent_surface.cpp
@@ -310,7 +310,7 @@ Common::Rect TransparentSurface::blit(Graphics::Surface &target, int posX, int p
 	}
 
 	if (pPartRect) {
-		srcImage.pixels = getBasePtr(pPartRect->top, pPartRect->left);
+		srcImage.pixels = getBasePtr(pPartRect->left, pPartRect->top);
 		srcImage.w = pPartRect->width();
 		srcImage.h = pPartRect->height();
 