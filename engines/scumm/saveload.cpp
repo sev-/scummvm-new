sev-/scummvm-new

commit 051686cd483d3619d2a14919b7b35d6d7b67bdd1

Apply patch #2984508 - "GSoC: SCUMM stopped audio from playing while saving"
This is a fix for bug #2090846 - "SCUMM: saving a game will play music before
finishing"

svn-id: r48630

engines/scumm/saveload.cpp
@@ -184,6 +184,8 @@ bool ScummEngine::saveState(int slot, bool compat) {
 	Common::String filename;
 	Common::OutSaveFile *out;
 
+	pauseEngine(true);
+
 	if (_saveLoadSlot == 255) {
 		// Allow custom filenames for save game system in HE Games
 		filename = _saveLoadFileName;
@@ -207,6 +209,9 @@ bool ScummEngine::saveState(int slot, bool compat) {
 		return false;
 	}
 	debug(1, "State saved as '%s'", filename.c_str());
+
+	pauseEngine(false);
+
 	return true;
 }
 

commit 17f70a4054a5c874c7a2a4a1440760ab8eca4ce3

SCUMM: Fix bug #6009 "DC: FT/Dig - Sound disappears when VMU save fails"
This was introduced by 051686cd483d3619d2a14919b7b35d6d7b67bdd1:
Apply patch #2984508 - "GSoC: SCUMM stopped audio from playing while
saving"

This was not quite correct as if the save fails, the function exits
without unpausing the engine, which resulted in sound and music
remaining muted. This corrects the logic to unpause in all cases.

engines/scumm/saveload.cpp
@@ -188,32 +188,31 @@ bool ScummEngine::saveState(Common::WriteStream *out, bool writeHeader) {
 }
 
 bool ScummEngine::saveState(int slot, bool compat, Common::String &filename) {
-	bool saveFailed;
+	bool saveFailed = false;
 
 	pauseEngine(true);
 
 	Common::WriteStream *out = openSaveFileForWriting(slot, compat, filename);
-	if (!out)
-		return false;
-
-	saveFailed = false;
-	if (!saveState(out))
+	if (!out) {
 		saveFailed = true;
+	} else {
+		if (!saveState(out))
+			saveFailed = true;
 
-	out->finalize();
-	if (out->err())
-		saveFailed = true;
-	delete out;
-
-	if (saveFailed) {
-		debug(1, "State save as '%s' FAILED", filename.c_str());
-		return false;
+		out->finalize();
+		if (out->err())
+			saveFailed = true;
+		delete out;
 	}
-	debug(1, "State saved as '%s'", filename.c_str());
+
+	if (saveFailed)
+		debug(1, "State save as '%s' FAILED", filename.c_str());
+	else
+		debug(1, "State saved as '%s'", filename.c_str());
 
 	pauseEngine(false);
 
-	return true;
+	return !saveFailed;
 }
 
 