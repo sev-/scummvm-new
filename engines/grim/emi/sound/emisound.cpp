Akz-/residual

EMI: Pause sounds in draw mode.

engines/grim/emi/sound/emisound.cpp
@@ -463,6 +463,27 @@ void EMISound::flushStack() {
 	}
 }
 
+void EMISound::pause(bool pause) {
+	Common::StackLock lock(_mutex);
+
+	for (int i = 0; i < NUM_CHANNELS; i++) {
+		SoundTrack *track = _channels[i];
+		if (track == nullptr)
+			continue;
+
+		if (pause && track->isPaused())
+			continue;
+		if (!pause && !track->isPaused())
+			continue;
+
+		// Do not pause music.
+		if (i == _musicChannel)
+			continue;
+
+		track->pause();
+	}
+}
+
 void EMISound::callback() {
 	Common::StackLock lock(_mutex);
 

engines/grim/emi/sound/emisound.h
@@ -23,6 +23,7 @@
 #ifndef GRIM_MSS_H
 #define GRIM_MSS_H
 
+#include "audio/mixer.h"
 #include "common/str.h"
 #include "common/stack.h"
 #include "common/mutex.h"
@@ -30,6 +31,7 @@
 namespace Grim {
 
 class SoundTrack;
+class SaveGame;
 
 struct MusicEntry {
 	int _x;
@@ -87,6 +89,7 @@ public:
 	void pushStateToStack();
 	void popStateFromStack();
 	void flushStack();
+	void pause(bool pause);
 
 	void callback();
 	void updateTrack(SoundTrack *track);

sev-/scummvm-new

engines/grim/emi/sound/emisound.cpp
@@ -462,6 +462,27 @@ void EMISound::flushStack() {
 	}
 }
 
+void EMISound::pause(bool paused) {
+	Common::StackLock lock(_mutex);
+
+	for (int i = 0; i < NUM_CHANNELS; i++) {
+		SoundTrack *track = _channels[i];
+		if (track == nullptr)
+			continue;
+
+		if (paused && track->isPaused())
+			continue;
+		if (!paused && !track->isPaused())
+			continue;
+
+		// Do not pause music.
+		if (i == _musicChannel)
+			continue;
+
+		track->pause();
+	}
+}
+
 void EMISound::callback() {
 	Common::StackLock lock(_mutex);
 

engines/grim/emi/sound/emisound.h
@@ -23,6 +23,7 @@
 #ifndef GRIM_MSS_H
 #define GRIM_MSS_H
 
+#include "audio/mixer.h"
 #include "common/str.h"
 #include "common/stack.h"
 #include "common/mutex.h"
@@ -30,6 +31,7 @@
 namespace Grim {
 
 class SoundTrack;
+class SaveGame;
 
 struct MusicEntry {
 	int _x;
@@ -87,6 +89,7 @@ public:
 	void pushStateToStack();
 	void popStateFromStack();
 	void flushStack();
+	void pause(bool paused);
 
 	void callback();
 	void updateTrack(SoundTrack *track);