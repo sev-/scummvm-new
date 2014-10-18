sev-/scummvm-new

commit e9cf6ff853bdba81c42c0efaeba86e2f30233ee0

SCI: Allow alt key combinations in all SCI games
Such key combos are used for the game debugger in SCI1.1 and GK1

engines/sci/event.cpp
@@ -253,14 +253,11 @@ SciEvent EventManager::getScummVMEvent() {
 	if ((modifiers & Common::KBD_ALT) && input.character > 0 && input.character < 27)
 		input.character += 96; // 0x01 -> 'a'
 
-	if (getSciVersion() <= SCI_VERSION_1_MIDDLE) {
-		// TODO: find out if altify is also not needed for sci1late+, couldnt find any game that uses those keys
-		// Scancodify if appropriate
-		if (modifiers & Common::KBD_ALT)
-			input.character = altify(input.character);
-		else if ((modifiers & Common::KBD_CTRL) && input.character > 0 && input.character < 27)
-			input.character += 96; // 0x01 -> 'a'
-	}
+	// Scancodify if appropriate
+	if (modifiers & Common::KBD_ALT)
+		input.character = altify(input.character);
+	else if ((modifiers & Common::KBD_CTRL) && input.character > 0 && input.character < 27)
+		input.character += 96; // 0x01 -> 'a'
 
 	// If no actual key was pressed (e.g. if only a modifier key was pressed),
 	// ignore the event

commit 80df6face85c0d146ca8306fd0fac2d983fa27c3

SCI: Fix bug #6703: "SCI: QFG1 VGA - Can't CTRL+S to access statistics"
This actually affected all Control+key combinations. A regression from
e9cf6ff853bdba81c42c0efaeba86e2f30233ee0

engines/sci/event.cpp
@@ -262,7 +262,7 @@ SciEvent EventManager::getScummVMEvent() {
 	// Scancodify if appropriate
 	if (modifiers & Common::KBD_ALT)
 		input.character = altify(input.character);
-	else if ((modifiers & Common::KBD_CTRL) && input.character > 0 && input.character < 27)
+	if (getSciVersion() <= SCI_VERSION_1_MIDDLE && (modifiers & Common::KBD_CTRL) && input.character > 0 && input.character < 27)
 		input.character += 96; // 0x01 -> 'a'
 
 	// If no actual key was pressed (e.g. if only a modifier key was pressed),