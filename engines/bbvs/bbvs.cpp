sev-/scummvm-new

commit ba23c42762787d8fe01f9ed2b36713357243514e

BBVS: Since we're using GUI for menus, switch iGUI to the relevant language

engines/bbvs/bbvs.cpp
@@ -39,6 +39,8 @@
 #include "common/error.h"
 #include "common/fs.h"
 #include "common/timer.h"
+#include "common/translation.h"
+#include "engines/advancedDetector.h"
 #include "engines/util.h"
 #include "graphics/cursorman.h"
 #include "graphics/font.h"
@@ -116,9 +118,15 @@ BbvsEngine::BbvsEngine(OSystem *syst, const ADGameDescription *gd) :
 
 	Engine::syncSoundSettings();
 
+	_oldGUILanguage	= TransMan.getCurrentLanguage();
+
+	if (gd->flags & GF_GUILANGSWITCH)
+		TransMan.setLanguage(getLanguageLocale(gd->language));
 }
 
 BbvsEngine::~BbvsEngine() {
+	if (TransMan.getCurrentLanguage() != _oldGUILanguage)
+		TransMan.setLanguage(_oldGUILanguage);
 
 	delete _random;
 

engines/bbvs/bbvs.h
@@ -63,6 +63,10 @@ class SoundMan;
 #define BBVS_SAVEGAME_VERSION 0
 
 enum {
+	GF_GUILANGSWITCH =    (1 << 0) // If GUI language switch is required for menus
+};
+
+enum {
 	kVerbLook      = 0,
 	kVerbUse       = 1,
 	kVerbTalk      = 2,
@@ -227,8 +231,11 @@ public:
 	void setNewSceneNum(int newSceneNum);
 	const Common::String getTargetName() { return _targetName; }
 	const ADGameDescription *_gameDescription;
+
 private:
 	Graphics::PixelFormat _pixelFormat;
+	Common::String _oldGUILanguage;
+
 public:
 	Common::RandomSource *_random;
 

engines/bbvs/detection.cpp
@@ -52,7 +52,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("vspr0001.vnm", "91c76b1048f93208cd7b1a05ebccb408", 1176976),
 		Common::RU_RUS,
 		Common::kPlatformWindows,
-		ADGF_NO_FLAGS,
+		GF_GUILANGSWITCH,
 		GUIO0()
 	},
 

commit f80169b58fdfe49ed2a5f483d20c09633ca56823

BBVS: Fix compilation without translation enabled
A regression from ba23c42

engines/bbvs/bbvs.cpp
@@ -118,15 +118,19 @@ BbvsEngine::BbvsEngine(OSystem *syst, const ADGameDescription *gd) :
 
 	Engine::syncSoundSettings();
 
+#ifdef USE_TRANSLATION
 	_oldGUILanguage	= TransMan.getCurrentLanguage();
 
 	if (gd->flags & GF_GUILANGSWITCH)
 		TransMan.setLanguage(getLanguageLocale(gd->language));
+#endif
 }
 
 BbvsEngine::~BbvsEngine() {
+#ifdef USE_TRANSLATION
 	if (TransMan.getCurrentLanguage() != _oldGUILanguage)
 		TransMan.setLanguage(_oldGUILanguage);
+#endif
 
 	delete _random;
 

engines/bbvs/bbvs.h
@@ -234,7 +234,10 @@ public:
 
 private:
 	Graphics::PixelFormat _pixelFormat;
+
+#ifdef USE_TRANSLATION
 	Common::String _oldGUILanguage;
+#endif
 
 public:
 	Common::RandomSource *_random;