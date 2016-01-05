sev-/scummvm-new

commit c25aff1ee768ecfaf5bd4b53bfb1c1046a4ea3b1

LAB: Reorder mouse position check in processMonitor in order to avoid duplicate checks

engines/lab/special.cpp
@@ -435,19 +435,9 @@ void LabEngine::processMonitor(char *ntext, TextFont *monitorFont, bool isIntera
 				return;
 			else if ((msgClass == MOUSEBUTTONS) && (IEQUALIFIER_LEFTBUTTON & qualifier)) {
 				if ((mouseY >= _utils->vgaScaleY(171)) && (mouseY <= _utils->vgaScaleY(200))) {
-					if ((mouseX >= _utils->vgaScaleX(259)) && (mouseX <= _utils->vgaScaleX(289))) {
-						if (!_lastPage) {
-							_monitorPage += 1;
-							drawMonText(ntext, monitorFont, x1, y1, x2, y2, isInteractive);
-						}
-					} else if ((mouseX >= _utils->vgaScaleX(0)) && (mouseX <= _utils->vgaScaleX(31))) {
+					if (mouseX <= _utils->vgaScaleX(31)) {
 						return;
-					} else if ((mouseX >= _utils->vgaScaleX(290)) && (mouseX <= _utils->vgaScaleX(320))) {
-						if (_monitorPage >= 1) {
-							_monitorPage -= 1;
-							drawMonText(ntext, monitorFont, x1, y1, x2, y2, isInteractive);
-						}
-					} else if ((mouseX >= _utils->vgaScaleX(31)) && (mouseX <= _utils->vgaScaleX(59))) {
+					} else if (mouseX <= _utils->vgaScaleX(59)) {
 						if (isInteractive) {
 							_monitorPage = 0;
 
@@ -459,6 +449,17 @@ void LabEngine::processMonitor(char *ntext, TextFont *monitorFont, bool isIntera
 							_monitorPage = 0;
 							drawMonText(ntext, monitorFont, x1, y1, x2, y2, isInteractive);
 						}
+					} else if (mouseX < _utils->vgaScaleX(259)) {
+						return;
+					} else if (mouseX <= _utils->vgaScaleX(289)) {
+						if (!_lastPage) {
+							_monitorPage += 1;
+							drawMonText(ntext, monitorFont, x1, y1, x2, y2, isInteractive);
+						}
+					} else if (_monitorPage >= 1) {
+						// mouseX between 290 and 320 (scaled)
+						_monitorPage -= 1;
+						drawMonText(ntext, monitorFont, x1, y1, x2, y2, isInteractive);
 					}
 				} else if (isInteractive) {
 					CloseDataPtr tmpClosePtr = _closeDataPtr;

commit b182afc2ca8cfe8108f62ffaca3c5d39786fa749

LAB: Ignore clicks in empty region in monitor
Regression from c25aff1ee768ecfaf5bd4b53bfb1c1046a4ea3b1

engines/lab/special.cpp
@@ -387,11 +387,15 @@ void LabEngine::processMonitor(const char *ntext, TextFont *monitorFont, bool is
 				int16 mouseX = msg->_mouse.x;
 				int16 mouseY = msg->_mouse.y;
 
+				// Check if mouse was in button bar
 				if ((mouseY >= _utils->vgaScaleY(171)) && (mouseY <= _utils->vgaScaleY(200))) {
-					if (mouseX <= _utils->vgaScaleX(31))
+					if (mouseX <= _utils->vgaScaleX(31)) {
+						// Exit button
 						return;
+					}
 					
 					if (mouseX <= _utils->vgaScaleX(59)) {
+						// Back button
 						if (isInteractive) {
 							_monitorPage = 0;
 
@@ -404,14 +408,15 @@ void LabEngine::processMonitor(const char *ntext, TextFont *monitorFont, bool is
 							drawMonText(ntext, monitorFont, textRect, isInteractive);
 						}
 					} else if (mouseX < _utils->vgaScaleX(259)) {
-						return;
+						// empty region; ignore
 					} else if (mouseX <= _utils->vgaScaleX(289)) {
+						// Page down button
 						if (!_lastPage) {
 							_monitorPage += 1;
 							drawMonText(ntext, monitorFont, textRect, isInteractive);
 						}
 					} else if (_monitorPage >= 1) {
-						// mouseX is greater than 290 (scaled)
+						// Page up button
 						_monitorPage -= 1;
 						drawMonText(ntext, monitorFont, textRect, isInteractive);
 					}