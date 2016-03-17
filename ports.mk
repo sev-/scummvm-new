sev-/scummvm-new

commit d7d2fe7787d5118158f598068aa21ed4c97c2cac

BUILD: Add OpenGL framework in static build link command on OS X
This should be provided by sdl-config --static-libs, but when using
SDL2 it is missing for me.

ports.mk
@@ -223,6 +223,8 @@ ifneq ($(BACKEND), iphone)
 ifneq ($(BACKEND), ios7)
 # Static libaries, used for the scummvm-static and iphone targets
 OSX_STATIC_LIBS := `$(SDLCONFIG) --static-libs`
+# With sdl2-config we don't always get the OpenGL framework
+OSX_STATIC_LIBS := -framework OpenGL
 endif
 endif
 

commit 1decd0301987c0bee650d6277c1a8ff99c1bc415

BUILD: Fix static linking for OS X.
Regression from d7d2fe7787d5118158f598068aa21ed4c97c2cac.

ports.mk
@@ -224,7 +224,7 @@ ifneq ($(BACKEND), ios7)
 # Static libaries, used for the scummvm-static and iphone targets
 OSX_STATIC_LIBS := `$(SDLCONFIG) --static-libs`
 # With sdl2-config we don't always get the OpenGL framework
-OSX_STATIC_LIBS := -framework OpenGL
+OSX_STATIC_LIBS += -framework OpenGL
 endif
 endif
 