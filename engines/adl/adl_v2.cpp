sev-/scummvm-new

commit 8b05db9797c1ec0685f6dcecb668ab0dd1a4b880

ADL: Fix hires6 canSaveGameStateCurrently()

engines/adl/adl_v2.cpp
@@ -611,6 +611,16 @@ int AdlEngine_v2::o2_initDisk(ScriptEnv &e) {
 	return 0;
 }
 
+bool AdlEngine_v2::canSaveGameStateCurrently() {
+	// Back up first visit flag as it may be changed by this test
+	const bool isFirstTime = getCurRoom().isFirstTime;
+	const bool retval = AdlEngine::canSaveGameStateCurrently();
+
+	getCurRoom().isFirstTime = isFirstTime;
+
+	return retval;
+}
+
 int AdlEngine_v2::askForSlot(const Common::String &question) {
 	while (1) {
 		_display->printString(question);

engines/adl/hires6.cpp
@@ -58,6 +58,9 @@ private:
 	// AdlEngine_v2
 	void printString(const Common::String &str);
 
+	// Engine
+	bool canSaveGameStateCurrently();
+
 	template <Direction D>
 	int o_goDirection(ScriptEnv &e);
 	int o_fluteSound(ScriptEnv &e);
@@ -201,6 +204,22 @@ int HiRes6Engine::o_fluteSound(ScriptEnv &e) {
 
 	return 0;
 }
+
+bool HiRes6Engine::canSaveGameStateCurrently() {
+	// Back up variables that may be changed by this test
+	const byte var2 = getVar(2);
+	const byte var24 = getVar(24);
+	const bool abortScript = _abortScript;
+
+	const bool retval = AdlEngine_v5::canSaveGameStateCurrently();
+
+	setVar(2, var2);
+	setVar(24, var24);
+	_abortScript = abortScript;
+
+	return retval;
+}
+
 
 #define SECTORS_PER_TRACK 16
 #define BYTES_PER_SECTOR 256

engines/adl/adl.cpp
@@ -891,6 +891,12 @@ bool AdlEngine::canSaveGameStateCurrently() {
 	// Here we check whether or not the game currently accepts the command
 	// "SAVE GAME". This prevents saving via the GMM in situations where
 	// it wouldn't otherwise be possible to do so.
+	for (cmd = _roomData.commands.begin(); cmd != _roomData.commands.end(); ++cmd) {
+		ScriptEnv env(*cmd, _state.room, _saveVerb, _saveNoun);
+		if (matchCommand(env))
+			return env.op() == IDO_ACT_SAVE;
+	}
+
 	for (cmd = _roomCommands.begin(); cmd != _roomCommands.end(); ++cmd) {
 		ScriptEnv env(*cmd, _state.room, _saveVerb, _saveNoun);
 		if (matchCommand(env))

engines/adl/adl.h
@@ -239,6 +239,7 @@ protected:
 	// Engine
 	Common::Error loadGameState(int slot);
 	Common::Error saveGameState(int slot, const Common::String &desc);
+	bool canSaveGameStateCurrently();
 
 	virtual void gameLoop();
 	virtual void loadState(Common::ReadStream &stream);
@@ -411,7 +412,6 @@ private:
 	Common::Error run();
 	bool hasFeature(EngineFeature f) const;
 	bool canLoadGameStateCurrently();
-	bool canSaveGameStateCurrently();
 
 	// Text input
 	byte convertKey(uint16 ascii) const;

engines/adl/adl_v2.h
@@ -51,6 +51,9 @@ protected:
 	virtual void showRoom();
 	void takeItem(byte noun);
 
+	// Engine
+	bool canSaveGameStateCurrently();
+
 	virtual DataBlockPtr readDataBlockPtr(Common::ReadStream &f) const;
 	virtual void adjustDataBlockPtr(byte &track, byte &sector, byte &offset, byte &size) const { }
 	void loadItems(Common::ReadStream &stream);

commit bb8d0264b5698504b6524d0533165b557f906c8e

ADL: Fix error() when opening GMM during intro
This is a regression from 8b05db9

engines/adl/adl_v2.cpp
@@ -634,6 +634,9 @@ int AdlEngine_v2::o2_initDisk(ScriptEnv &e) {
 }
 
 bool AdlEngine_v2::canSaveGameStateCurrently() {
+	if (!_canSaveNow)
+		return false;
+
 	// Back up first visit flag as it may be changed by this test
 	const bool isFirstTime = getCurRoom().isFirstTime;
 	const bool retval = AdlEngine::canSaveGameStateCurrently();

engines/adl/hires6.cpp
@@ -206,6 +206,9 @@ int HiRes6Engine::o_fluteSound(ScriptEnv &e) {
 }
 
 bool HiRes6Engine::canSaveGameStateCurrently() {
+	if (!_canSaveNow)
+		return false;
+
 	// Back up variables that may be changed by this test
 	const byte var2 = getVar(2);
 	const byte var24 = getVar(24);