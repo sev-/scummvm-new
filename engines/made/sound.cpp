sev-/scummvm-new

commit 6993fca7b52f9904696a009cfb411c98439ee879

MADE: Cleanup

engines/made/sound.cpp
@@ -248,6 +248,9 @@ void decompressSound(byte *source, byte *dest, uint16 chunkSize, uint16 chunkCou
 			// soundBuffer.
 			soundBuffer[workChunkSize] = soundBuffer[workChunkSize - 1];
 
+			for (i = 0; i < chunkSize; i++)
+				soundBuffer[i] = 0;
+
 			if (deltaType == 1) {
 				for (i = 0; i < chunkSize - 1; i += 2) {
 					l = i / 2;

engines/made/database.cpp
@@ -40,6 +40,7 @@ namespace Made {
 */
 
 Object::Object() : _objData(NULL), _freeData(false) {
+	_objSize = 0;
 }
 
 Object::~Object() {

engines/made/pmvplayer.cpp
@@ -223,7 +223,10 @@ bool PmvPlayer::play(const char *filename) {
 
 	//delete _audioStream;
 	delete _fd;
-	_surface->free();
+
+	if(_surface)
+		_surface->free();
+
 	delete _surface;
 
 	return !_aborted;

engines/made/redreader.cpp
@@ -102,7 +102,7 @@ int LzhDecompressor::decompress(Common::SeekableReadStream &source, byte *dest, 
 	int bufsize;
 	byte* buffer;
 
-	buffer = (byte *) malloc(DICSIZ);
+	buffer = (byte *)calloc(DICSIZ, 1);
 
 	_source = &source;
 	_compSize = sourceLen;

engines/made/resource.cpp
@@ -43,6 +43,7 @@ Resource::~Resource() {
 
 PictureResource::PictureResource() : _picture(NULL), _picturePalette(NULL) {
 	_hasPalette = false;
+	_paletteColorCount = 0;
 }
 
 PictureResource::~PictureResource() {
@@ -182,6 +183,9 @@ void PictureResource::loadChunked(byte *source, int size) {
 /* AnimationResource */
 
 AnimationResource::AnimationResource() {
+	_flags = 0;
+	_width = 0;
+	_height = 0;
 }
 
 AnimationResource::~AnimationResource() {

engines/made/screen.cpp
@@ -91,6 +91,8 @@ Screen::Screen(MadeEngine *vm) : _vm(vm) {
 	_currentFontNum = 0;
 	_fontDrawCtx.clipRect = Common::Rect(320, 200);
 	_fontDrawCtx.destSurface = _backgroundScreen;
+	_outlineColor = 0;
+	_dropShadowColor = 0;
 
 	clearChannels();
 }

commit 887bebdb73ec0b73ed3ae373b8672cff04e31116

MADE: Fix static noise when playing sounds
This was a regression introduced in commit 6993fca.
This fixes bug #9753.

engines/made/sound.cpp
@@ -249,9 +249,6 @@ void decompressSound(byte *source, byte *dest, uint16 chunkSize, uint16 chunkCou
 			// soundBuffer.
 			soundBuffer[workChunkSize] = soundBuffer[workChunkSize - 1];
 
-			for (i = 0; i < chunkSize; i++)
-				soundBuffer[i] = 0;
-
 			if (deltaType == 1) {
 				for (i = 0; i < chunkSize - 1; i += 2) {
 					l = i / 2;