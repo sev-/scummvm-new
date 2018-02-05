sev-/scummvm-new

commit b87fbc69e28262f223214faa16055f15056e17c2

MOHAWK: RIVEN: Rewind videos that are played until their end
When videos are skipped, they would sometimes not quite be seeked to the
last frame. In that case, when they are played again without being
restarted, they would end immediatly.

Now videos are automatically rewinded when they end or are skipped so
that they always start from the beginning when played again.

Fixes #10389.

engines/mohawk/riven_video.cpp
@@ -255,6 +255,7 @@ void RivenVideo::playBlocking(int32 endTime) {
 	if (playTillEnd) {
 		disable();
 		stop();
+		seek(0);
 	}
 
 	// Execute the stored opcode

commit f28596b3f7dd3f7ca8558f8f5d225f8f1ab019d7

MOHAWK: RIVEN: Execute the stored opcode before resetting movies
Otherwise the movie position is zero which leads the comparison with the
stored opcode position to always be false.
Fixes #10426, a regression from b87fbc69e28262f2.

engines/mohawk/riven_video.cpp
@@ -252,17 +252,17 @@ void RivenVideo::playBlocking(int32 endTime) {
 		}
 	}
 
-	if (playTillEnd) {
-		disable();
-		stop();
-		seek(0);
-	}
-
 	// Execute the stored opcode
 	uint16 storedOpcodeMovieSlot = _vm->_scriptMan->getStoredMovieOpcodeSlot();
 	uint32 storedOpcodeTime = _vm->_scriptMan->getStoredMovieOpcodeTime();
 	if (_slot == storedOpcodeMovieSlot && getTime() >= storedOpcodeTime) { // CHECKME: Suspicious use of time units
 		_vm->_scriptMan->runStoredMovieOpcode();
+	}
+
+	if (playTillEnd) {
+		disable();
+		stop();
+		seek(0);
 	}
 
 	_vm->_cursor->showCursor();