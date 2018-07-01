residualvm/residualvm

MYST3: Add RoomID enum to help working with Rooms
No behavior changes.

Not all rooms were given useful names, but the most used
were. More useful names for the more obscure rooms can be added
later.

engines/myst3/database.cpp
@@ -372,70 +372,70 @@ NodeWalker::~NodeWalker() {
 }
 
 static const RoomData roomsXXXX[] = {
-		{ 101, "XXXX" }
+		{ kInit, "XXXX" }
 };
 
 static const RoomData roomsINTR[] = {
-		{ 201, "INTR" }
+		{ kIntro, "INTR" }
 };
 
 static const RoomData roomsTOHO[] = {
-		{ 301, "TOHO" }
+		{ kTomahnaStart, "TOHO" }
 };
 
 static const RoomData roomsTOHB[] = {
-		{ 401, "TOHB" }
+		{ kTomahnaReturn, "TOHB" }
 };
 
 static const RoomData roomsLE[] = {
-		{ 501, "LEIS" },
-		{ 502, "LEOS" },
-		{ 503, "LEET" },
-		{ 504, "LELT" },
-		{ 505, "LEMT" },
-		{ 506, "LEOF" }
+		{ kJnaninStart, "LEIS" },
+		{ kLeos, "LEOS" },
+		{ kLeet, "LEET" },
+		{ kLelt, "LELT" },
+		{ kLemt, "LEMT" },
+		{ kLeof, "LEOF" }
 };
 
 static const RoomData roomsLI[] = {
-		{ 601, "LIDR" },
-		{ 602, "LISW" },
-		{ 603, "LIFO" },
-		{ 604, "LISP" },
-		{ 605, "LINE" }
+		{ kEdannaStart, "LIDR" },
+		{ kLisw, "LISW" },
+		{ kLifo, "LIFO" },
+		{ kLisp, "LISP" },
+		{ kLine, "LINE" }
 };
 
 static const RoomData roomsEN[] = {
-		{ 701, "ENSI" },
-		{ 703, "ENPP" },
-		{ 704, "ENEM" },
-		{ 705, "ENLC" },
-		{ 706, "ENDD" },
-		{ 707, "ENCH" },
-		{ 708, "ENLI" }
+		{ kVoltaicStart, "ENSI" },
+		{ kEnpp, "ENPP" },
+		{ kEnem, "ENEM" },
+		{ kEnlc, "ENLC" },
+		{ kEndd, "ENDD" },
+		{ kEnch, "ENCH" },
+		{ kEnli, "ENLI" }
 };
 
 static const RoomData roomsNA[] = {
-		{ 801, "NACH" }
+		{ kNarayan, "NACH" }
 };
 
 static const RoomData roomsMENU[] = {
-		{ 901, "MENU" },
-		{ 902, "JRNL" },
-		{ 903, "DEMO" },
-		{ 904, "ATIX" }
+		{ kMenuRoom, "MENU" },
+		{ kJrnl, "JRNL" },
+		{ kDemo, "DEMO" },
+		{ kAtix, "ATIX" }
 };
 
 static const RoomData roomsMA[] = {
-		{ 1001, "MACA" },
-		{ 1002, "MAIS" },
-		{ 1003, "MALL" },
-		{ 1004, "MASS" },
-		{ 1005, "MAWW" },
-		{ 1006, "MATO" }
+		{ kAmateriaStart, "MACA" },
+		{ kMais, "MAIS" },
+		{ kMall, "MALL" },
+		{ kMass, "MASS" },
+		{ kMaww, "MAWW" },
+		{ kMato, "MATO" }
 };
 
 static const RoomData roomsLOGO[] = {
-		{ 1101, "LOGO" }
+		{ kLogo, "LOGO" }
 };
 
 const AgeData Database::_ages[] = {
@@ -687,7 +687,7 @@ void Database::patchNodeScripts(const RoomData *room, Common::Array<NodePtr> &no
 }
 
 bool Database::isCommonRoom(uint32 roomID, uint32 ageID) const {
-	return roomID == 101 || roomID == 901 || roomID == 902;
+	return roomID == kInit || roomID == kMenuRoom || roomID == kJrnl;
 }
 
 void Database::cacheRoom(uint32 roomID, uint32 ageID) {
@@ -723,7 +723,7 @@ RoomKey Database::getRoomKey(const char *name) {
 			}
 		}
 
-	return RoomKey(0, 0);
+	return RoomKey(kNoRoom, 0);
 }
 
 uint32 Database::getAgeLabelId(uint32 ageID) {
@@ -864,7 +864,7 @@ void Database::patchLanguageMenu() {
 	//	op 194, runPuzzle1 ( 18 )
 	//	op 194, runPuzzle1 ( 19 )
 
-	NodePtr languageMenu = getNodeData(530, 901, 9);
+	NodePtr languageMenu = getNodeData(530, kMenuRoom, 9);
 	languageMenu->hotspots[5].script[1].args[1] = getGameLanguageCode();
 }
 

engines/myst3/menu.cpp
@@ -267,7 +267,7 @@ void Menu::updateMainMenu(uint16 action) {
 	case 4:
 		// Settings
 		_vm->_state->setMenuOptionsBack(1);
-		_vm->runScriptsFromNode(599, 0, 0);
+		_vm->runScriptsFromNode(599, kNoRoom, 0);
 		break;
 	case 5: {
 			// Asked to quit
@@ -305,7 +305,7 @@ Graphics::Surface *Menu::captureThumbnail() {
 }
 
 void Menu::goToNode(uint16 node) {
-	if (_vm->_state->getMenuSavedAge() == 0 && _vm->_state->getLocationRoom() != 901) {
+	if (_vm->_state->getMenuSavedAge() == 0 && _vm->_state->getLocationRoom() != kMenuRoom) {
 		// Entering menu, save current location ...
 		_vm->_state->setMenuSavedAge(_vm->_state->getLocationAge());
 		_vm->_state->setMenuSavedRoom(_vm->_state->getLocationRoom());
@@ -336,7 +336,7 @@ void Menu::goToNode(uint16 node) {
 	}
 
 	_vm->_state->setLocationNextAge(9);
-	_vm->_state->setLocationNextRoom(901);
+	_vm->_state->setLocationNextRoom(kMenuRoom);
 	_vm->goToNode(node, kTransitionNone);
 }
 
@@ -392,7 +392,7 @@ uint16 Menu::dialogSaveValue() {
 Common::String Menu::getAgeLabel(GameState *gameState) {
 	uint32 age = 0;
 	uint32 room = gameState->getLocationRoom();
-	if (room == 901)
+	if (room == kMenuRoom)
 		age = gameState->getMenuSavedAge();
 	else
 		age = gameState->getLocationAge();
@@ -441,7 +441,7 @@ void Menu::setSaveLoadSpotItem(uint16 id, SpotItemFace *spotItem) {
 }
 
 bool Menu::isOpen() const {
-	return _vm->_state->getLocationAge() == 9 && _vm->_state->getLocationRoom() == 901;
+	return _vm->_state->getLocationAge() == 9 && _vm->_state->getLocationRoom() == kMenuRoom;
 }
 
 void Menu::generateSaveThumbnail() {
@@ -677,7 +677,7 @@ void PagingMenu::draw() {
 	uint16 room = _vm->_state->getLocationRoom();
 	uint16 age = _vm->_state->getLocationAge();
 
-	if (room != 901 || !(node == 200 || node == 300))
+	if (room != kMenuRoom || !(node == 200 || node == 300))
 		return;
 
 	int16 page = _vm->_state->getMenuSaveLoadCurrentPage();
@@ -727,7 +727,7 @@ bool PagingMenu::handleInput(const Common::KeyState &e) {
 	uint16 room = _vm->_state->getLocationRoom();
 	uint16 item = _vm->_state->getMenuSaveLoadSelectedItem();
 
-	if (room != 901 || node != 300 || item != 7)
+	if (room != kMenuRoom || node != 300 || item != 7)
 		return false;
 
 	Common::String display = prepareSaveNameForDisplay(_saveName);
@@ -790,7 +790,7 @@ void AlbumMenu::draw() {
 	uint16 room = _vm->_state->getLocationRoom();
 
 	// Load and save menus only
-	if (room != 901 || !(node == 200 || node == 300))
+	if (room != kMenuRoom || !(node == 200 || node == 300))
 		return;
 
 	if (!_saveLoadAgeName.empty()) {
@@ -899,7 +899,7 @@ void AlbumMenu::loadMenuSelect() {
 	uint16 room = _vm->_state->getLocationRoom();
 
 	// Details are only updated on the load menu
-	if (room != 901 || node != 200)
+	if (room != kMenuRoom || node != 200)
 		return;
 
 	int32 selectedSave = _vm->_state->getMenuSelectedSave();

sev-/scummvm-new

engines/myst3/database.cpp
@@ -372,70 +372,70 @@ NodeWalker::~NodeWalker() {
 }
 
 static const RoomData roomsXXXX[] = {
-		{ 101, "XXXX" }
+		{ kRoomShared,        "XXXX" }
 };
 
 static const RoomData roomsINTR[] = {
-		{ 201, "INTR" }
+		{ kRoomIntro,         "INTR" }
 };
 
 static const RoomData roomsTOHO[] = {
-		{ 301, "TOHO" }
+		{ kRoomTomahnaStart,  "TOHO" }
 };
 
 static const RoomData roomsTOHB[] = {
-		{ 401, "TOHB" }
+		{ kRoomTomahnaReturn, "TOHB" }
 };
 
 static const RoomData roomsLE[] = {
-		{ 501, "LEIS" },
-		{ 502, "LEOS" },
-		{ 503, "LEET" },
-		{ 504, "LELT" },
-		{ 505, "LEMT" },
-		{ 506, "LEOF" }
+		{ kJnaninStart,       "LEIS" },
+		{ kRoomLeos,          "LEOS" },
+		{ kRoomLeet,          "LEET" },
+		{ kRoomLelt,          "LELT" },
+		{ kRoomLemt,          "LEMT" },
+		{ kRoomLeof,          "LEOF" }
 };
 
 static const RoomData roomsLI[] = {
-		{ 601, "LIDR" },
-		{ 602, "LISW" },
-		{ 603, "LIFO" },
-		{ 604, "LISP" },
-		{ 605, "LINE" }
+		{ kRoomEdannaStart,   "LIDR" },
+		{ kRoomLisw,          "LISW" },
+		{ kRoomLifo,          "LIFO" },
+		{ kRoomLisp,          "LISP" },
+		{ kRoomLine,          "LINE" }
 };
 
 static const RoomData roomsEN[] = {
-		{ 701, "ENSI" },
-		{ 703, "ENPP" },
-		{ 704, "ENEM" },
-		{ 705, "ENLC" },
-		{ 706, "ENDD" },
-		{ 707, "ENCH" },
-		{ 708, "ENLI" }
+		{ kRoomVoltaicStart,  "ENSI" },
+		{ kRoomEnpp,          "ENPP" },
+		{ kRoomEnem,          "ENEM" },
+		{ kRoomEnlc,          "ENLC" },
+		{ kRoomEndd,          "ENDD" },
+		{ kRoomEnch,          "ENCH" },
+		{ kRoomEnli,          "ENLI" }
 };
 
 static const RoomData roomsNA[] = {
-		{ 801, "NACH" }
+		{ kRoomNarayan,       "NACH" }
 };
 
 static const RoomData roomsMENU[] = {
-		{ 901, "MENU" },
-		{ 902, "JRNL" },
-		{ 903, "DEMO" },
-		{ 904, "ATIX" }
+		{ kRoomMenu,          "MENU" },
+		{ kRoomJournals,      "JRNL" },
+		{ kRoomDemo,          "DEMO" },
+		{ kRoomAtix,          "ATIX" }
 };
 
 static const RoomData roomsMA[] = {
-		{ 1001, "MACA" },
-		{ 1002, "MAIS" },
-		{ 1003, "MALL" },
-		{ 1004, "MASS" },
-		{ 1005, "MAWW" },
-		{ 1006, "MATO" }
+		{ kRoomAmateriaStart, "MACA" },
+		{ kRoomMais,          "MAIS" },
+		{ kRoomMall,          "MALL" },
+		{ kRoomMass,          "MASS" },
+		{ kRoomMaww,          "MAWW" },
+		{ kRoomMato,          "MATO" }
 };
 
 static const RoomData roomsLOGO[] = {
-		{ 1101, "LOGO" }
+		{ kLogo,              "LOGO" }
 };
 
 const AgeData Database::_ages[] = {
@@ -687,7 +687,7 @@ void Database::patchNodeScripts(const RoomData *room, Common::Array<NodePtr> &no
 }
 
 bool Database::isCommonRoom(uint32 roomID, uint32 ageID) const {
-	return roomID == 101 || roomID == 901 || roomID == 902;
+	return roomID == kRoomShared || roomID == kRoomMenu || roomID == kRoomJournals;
 }
 
 void Database::cacheRoom(uint32 roomID, uint32 ageID) {
@@ -864,7 +864,7 @@ void Database::patchLanguageMenu() {
 	//	op 194, runPuzzle1 ( 18 )
 	//	op 194, runPuzzle1 ( 19 )
 
-	NodePtr languageMenu = getNodeData(530, 901, 9);
+	NodePtr languageMenu = getNodeData(530, kRoomMenu, 9);
 	languageMenu->hotspots[5].script[1].args[1] = getGameLanguageCode();
 }
 

engines/myst3/menu.cpp
@@ -309,7 +309,7 @@ Graphics::Surface *Menu::captureThumbnail() {
 }
 
 void Menu::goToNode(uint16 node) {
-	if (_vm->_state->getMenuSavedAge() == 0 && _vm->_state->getLocationRoom() != 901) {
+	if (_vm->_state->getMenuSavedAge() == 0 && _vm->_state->getLocationRoom() != kRoomMenu) {
 		// Entering menu, save current location ...
 		_vm->_state->setMenuSavedAge(_vm->_state->getLocationAge());
 		_vm->_state->setMenuSavedRoom(_vm->_state->getLocationRoom());
@@ -340,7 +340,7 @@ void Menu::goToNode(uint16 node) {
 	}
 
 	_vm->_state->setLocationNextAge(9);
-	_vm->_state->setLocationNextRoom(901);
+	_vm->_state->setLocationNextRoom(kRoomMenu);
 	_vm->goToNode(node, kTransitionNone);
 }
 
@@ -396,7 +396,7 @@ uint16 Menu::dialogSaveValue() {
 Common::String Menu::getAgeLabel(GameState *gameState) {
 	uint32 age = 0;
 	uint32 room = gameState->getLocationRoom();
-	if (room == 901)
+	if (room == kRoomMenu)
 		age = gameState->getMenuSavedAge();
 	else
 		age = gameState->getLocationAge();
@@ -445,7 +445,7 @@ void Menu::setSaveLoadSpotItem(uint16 id, SpotItemFace *spotItem) {
 }
 
 bool Menu::isOpen() const {
-	return _vm->_state->getLocationAge() == 9 && _vm->_state->getLocationRoom() == 901;
+	return _vm->_state->getLocationAge() == 9 && _vm->_state->getLocationRoom() == kRoomMenu;
 }
 
 void Menu::generateSaveThumbnail() {
@@ -681,7 +681,7 @@ void PagingMenu::draw() {
 	uint16 room = _vm->_state->getLocationRoom();
 	uint16 age = _vm->_state->getLocationAge();
 
-	if (room != 901 || !(node == 200 || node == 300))
+	if (room != kRoomMenu || !(node == 200 || node == 300))
 		return;
 
 	int16 page = _vm->_state->getMenuSaveLoadCurrentPage();
@@ -731,7 +731,7 @@ bool PagingMenu::handleInput(const Common::KeyState &e) {
 	uint16 room = _vm->_state->getLocationRoom();
 	uint16 item = _vm->_state->getMenuSaveLoadSelectedItem();
 
-	if (room != 901 || node != 300 || item != 7)
+	if (room != kRoomMenu || node != 300 || item != 7)
 		return false;
 
 	Common::String display = prepareSaveNameForDisplay(_saveName);
@@ -794,7 +794,7 @@ void AlbumMenu::draw() {
 	uint16 room = _vm->_state->getLocationRoom();
 
 	// Load and save menus only
-	if (room != 901 || !(node == 200 || node == 300))
+	if (room != kRoomMenu || !(node == 200 || node == 300))
 		return;
 
 	if (!_saveLoadAgeName.empty()) {
@@ -903,7 +903,7 @@ void AlbumMenu::loadMenuSelect() {
 	uint16 room = _vm->_state->getLocationRoom();
 
 	// Details are only updated on the load menu
-	if (room != 901 || node != 200)
+	if (room != kRoomMenu || node != 200)
 		return;
 
 	int32 selectedSave = _vm->_state->getMenuSelectedSave();