clone2727/scummvm

STARTREK: Import my startrek engine
Now updated even

engines/startrek/graphics.cpp
@@ -1,0 +1,131 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL: https://scummvm-startrek.googlecode.com/svn/trunk/graphics.cpp $
+ * $Id: graphics.cpp 14 2010-05-26 15:44:12Z clone2727 $
+ *
+ */
+
+#include "startrek/graphics.h"
+
+#include "common/config-manager.h"
+
+namespace StarTrek {
+
+Graphics::Graphics(StarTrekEngine *vm) : _vm(vm), _egaMode(false) {
+	_font = 0;
+	_egaData = 0;
+
+	if (ConfMan.hasKey("render_mode"))
+		_egaMode = (Common::parseRenderMode(ConfMan.get("render_mode").c_str()) == Common::kRenderEGA) && (_vm->getGameType() != GType_STJR) && !(_vm->getFeatures() & GF_DEMO);
+
+	if (_vm->getGameType() == GType_ST25 && _vm->getPlatform() == Common::kPlatformPC)
+		_font = new Font(_vm);
+}
+
+Graphics::~Graphics() {
+	if (_egaData)
+		free(_egaData);
+
+	delete _font;
+}
+
+void Graphics::setPalette(const char *paletteFile) {
+	// Set the palette from a PAL file
+
+	Common::SeekableReadStream *palStream = _vm->openFile(paletteFile);
+	byte *palette = new byte[256 * 3];
+	palStream->read(palette, 256 * 3);
+
+	// Expand color components
+	if (_vm->getPlatform() == Common::kPlatformPC || _vm->getPlatform() == Common::kPlatformMacintosh)
+		for (uint16 i = 0; i < 256 * 3; i++)
+			palette[i] <<= 2;
+
+	_vm->_system->getPaletteManager()->setPalette(palette, 0, 256);
+
+	delete[] palette;
+	delete palStream;
+}
+
+void Graphics::loadEGAData(const char *filename) {
+	// Load EGA palette data
+	if (!_egaMode)
+		return;
+
+	if (!_egaData)
+		_egaData = (byte *)malloc(256);
+
+	Common::SeekableReadStream *egaStream = _vm->openFile(filename);
+	egaStream->read(_egaData, 256);
+	delete egaStream;
+}
+
+void Graphics::drawImage(const char *filename) {
+	// Draw a regular bitmap
+
+	Common::SeekableReadStream *imageStream = _vm->openFile(filename);
+	uint16 xoffset = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();
+	uint16 yoffset = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();
+	uint16 width = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();
+	uint16 height = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();	
+
+	byte *pixels = (byte *)malloc(width * height);
+
+	if (_egaMode && _egaData) {
+		// FIXME: This doesn't work right
+		for (uint32 i = 0; i < (uint32)(width * height); i++)
+			pixels[i] = _egaData[imageStream->readByte()];
+	} else {
+		imageStream->read(pixels, width * height);
+	}
+
+	_vm->_system->copyRectToScreen(pixels, width, xoffset, yoffset, width, height);
+	_vm->_system->updateScreen();
+}
+
+void Graphics::drawBackgroundImage(const char *filename) {
+	// Draw an stjr BGD image (palette built-in)
+
+	Common::SeekableReadStream *imageStream = _vm->openFile(filename);
+	byte *palette = new byte[256 * 3];
+	imageStream->read(palette, 256 * 3);
+
+	// Expand color components
+	for (uint16 i = 0; i < 256 * 3; i++)
+		palette[i] <<= 2;
+
+	uint16 xoffset = imageStream->readUint16LE();
+	uint16 yoffset = imageStream->readUint16LE();
+	uint16 width = imageStream->readUint16LE();
+	uint16 height = imageStream->readUint16LE();	
+
+	byte *pixels = (byte *)malloc(width * height);
+	imageStream->read(pixels, width * height);
+
+	_vm->_system->getPaletteManager()->setPalette(palette, 0, 256);
+	_vm->_system->copyRectToScreen(pixels, width, xoffset, yoffset, width, height);
+	_vm->_system->updateScreen();
+
+	delete[] palette;
+	delete imageStream;
+}
+
+}

engines/startrek/sound.cpp
@@ -1,0 +1,192 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL: https://scummvm-startrek.googlecode.com/svn/trunk/sound.cpp $
+ * $Id: sound.cpp 15 2010-06-27 06:13:42Z clone2727 $
+ *
+ */
+ 
+#include "startrek/sound.h"
+
+#include "common/file.h"
+#include "common/macresman.h"
+
+#include "audio/mods/protracker.h"
+#include "audio/decoders/raw.h"
+
+namespace StarTrek {
+
+// Main Sound Functions
+
+Sound::Sound(StarTrekEngine *vm) : _vm(vm) {
+	if (_vm->getPlatform() == Common::kPlatformPC || _vm->getPlatform() == Common::kPlatformMacintosh) {
+		// The main PC versions use XMIDI. ST25 Demo and Macintosh versions use SMF.
+		if ((_vm->getGameType() == GType_ST25 && _vm->getFeatures() & GF_DEMO) || _vm->getPlatform() == Common::kPlatformMacintosh)
+			_midiParser = MidiParser::createParser_SMF();
+		else
+			_midiParser = MidiParser::createParser_XMIDI();
+			
+		_midiDevice = MidiDriver::detectDevice(MDT_PCSPK|MDT_ADLIB|MDT_MIDI);
+		_midiDriver = MidiDriver::createMidi(_midiDevice);
+		_midiDriver->open();
+		_midiParser->setMidiDriver(_midiDriver);
+		_midiParser->setTimerRate(_midiDriver->getBaseTempo());
+	}
+
+	if (_vm->getPlatform() == Common::kPlatformMacintosh) {
+		_macAudioResFork = new Common::MacResManager();
+		if (!_macAudioResFork->open("Star Trek Audio"))
+			error("Could not open 'Star Trek Audio'");
+		assert(_macAudioResFork->hasResFork());
+	} else
+		_macAudioResFork = 0;
+
+	_soundHandle = new Audio::SoundHandle();
+}
+
+Sound::~Sound() {
+	delete _midiParser;
+	delete _midiDriver;
+	delete _soundHandle;
+	delete _macAudioResFork;
+}
+
+void Sound::playSound(const char *baseSoundName) {
+	if (_vm->getPlatform() == Common::kPlatformAmiga)
+		playAmigaSound(baseSoundName);
+	else if (_vm->getPlatform() == Common::kPlatformMacintosh)
+		playMacSMFSound(baseSoundName);
+	else if (_vm->getFeatures() & GF_DEMO)
+		playSMFSound(baseSoundName);
+	else
+		playXMIDISound(baseSoundName);
+}
+
+void Sound::playSoundEffect(const char *baseSoundName) {
+	if (_vm->getPlatform() == Common::kPlatformAmiga)
+		playAmigaSoundEffect(baseSoundName);
+	else if (_vm->getPlatform() == Common::kPlatformMacintosh)
+		playMacSoundEffect(baseSoundName);
+	else
+		error("PC Sound Effects Not Supported");
+}
+
+// PC Functions
+
+void Sound::playSMFSound(const char *baseSoundName) {
+	Common::String soundName = baseSoundName;
+	
+	soundName += '.';
+	
+	switch (MidiDriver::getMusicType(_midiDevice)) {
+		case MT_MT32:
+			soundName += "ROL";
+			break;
+		case MT_PCSPK:
+			return; // Not supported...
+		default:
+			soundName += "ADL";
+			break;
+	}
+	
+	debug(0, "Playing sound \'%s\'\n", soundName.c_str());
+	Common::SeekableReadStream *soundStream = _vm->openFile(soundName.c_str());
+	
+	byte *soundData = (byte *)malloc(soundStream->size());
+	soundStream->read(soundData, soundStream->size());
+	_midiParser->loadMusic(soundData, soundStream->size());
+	delete soundStream;
+	
+	_midiDriver->setTimerCallback(_midiParser, MidiParser::timerCallback);
+}
+
+void Sound::playXMIDISound(const char *baseSoundName) {
+	Common::String soundName = baseSoundName;
+	
+	soundName += '.';
+	
+	switch (MidiDriver::getMusicType(_midiDevice)) {
+		case MT_MT32:
+			soundName += "MT";
+			break;
+		case MT_PCSPK:
+			soundName += "PC";
+			break;
+		default:
+			soundName += "AD";
+			break;
+	}
+	
+	debug(0, "Playing sound \'%s\'\n", soundName.c_str());
+	Common::SeekableReadStream *soundStream = _vm->openFile(soundName.c_str());
+	
+	byte *soundData = (byte *)malloc(soundStream->size());
+	soundStream->read(soundData, soundStream->size());
+	_midiParser->loadMusic(soundData, soundStream->size());
+	delete soundStream;
+	
+	_midiDriver->setTimerCallback(_midiParser, MidiParser::timerCallback);
+}
+
+// Amiga Functions
+
+void Sound::playAmigaSound(const char *baseSoundName) {
+	// Nope, this is wrong... see http://protracker.de/files/amiga/formats/theplayer_41_format.txt
+#if 0
+	Common::String soundName = baseSoundName;
+	soundName += ".SNG";
+	if (_vm->_mixer->isSoundHandleActive(*_soundHandle))
+		_vm->_mixer->stopHandle(*_soundHandle);
+	_vm->_mixer->playInputStream(Audio::Mixer::kMusicSoundType, _soundHandle, Audio::makeProtrackerStream(_vm->openFile(soundName.c_str())));
+#endif
+}
+
+void Sound::playAmigaSoundEffect(const char *baseSoundName) {
+	Common::String soundName = baseSoundName;
+	soundName += ".SFX";
+
+	if (_vm->_mixer->isSoundHandleActive(*_soundHandle))
+		_vm->_mixer->stopHandle(*_soundHandle);
+
+	Audio::AudioStream *audStream = (Audio::AudioStream *)Audio::makeRawStream(_vm->openFile(soundName.c_str()), 11025, 0);
+	_vm->_mixer->playStream(Audio::Mixer::kSFXSoundType, _soundHandle, audStream);
+}
+
+// Macintosh Functions
+
+void Sound::playMacSMFSound(const char *baseSoundName) {
+	Common::SeekableReadStream *soundStream = _macAudioResFork->getResource(baseSoundName);
+	byte *soundData = (byte *)malloc(soundStream->size());
+	soundStream->read(soundData, soundStream->size());
+	_midiParser->loadMusic(soundData, soundStream->size());
+	delete soundStream;
+	
+	_midiDriver->setTimerCallback(_midiParser, MidiParser::timerCallback);
+}
+
+void Sound::playMacSoundEffect(const char *baseSoundName) {
+	if (_vm->_mixer->isSoundHandleActive(*_soundHandle))
+		_vm->_mixer->stopHandle(*_soundHandle);
+
+	Audio::AudioStream *audStream = (Audio::AudioStream *)Audio::makeRawStream(_macAudioResFork->getResource(baseSoundName), 11025, 0);
+	_vm->_mixer->playStream(Audio::Mixer::kSFXSoundType, _soundHandle, audStream);
+}
+
+} // End of namespace StarTrek

sev-/scummvm-new

engines/startrek/graphics.cpp
@@ -1,0 +1,133 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL: https://scummvm-startrek.googlecode.com/svn/trunk/graphics.cpp $
+ * $Id: graphics.cpp 14 2010-05-26 15:44:12Z clone2727 $
+ *
+ */
+
+#include "startrek/graphics.h"
+
+#include "common/config-manager.h"
+#include "common/rendermode.h"
+#include "graphics/palette.h"
+
+namespace StarTrek {
+
+Graphics::Graphics(StarTrekEngine *vm) : _vm(vm), _egaMode(false) {
+	_font = 0;
+	_egaData = 0;
+
+	if (ConfMan.hasKey("render_mode"))
+		_egaMode = (Common::parseRenderMode(ConfMan.get("render_mode").c_str()) == Common::kRenderEGA) && (_vm->getGameType() != GType_STJR) && !(_vm->getFeatures() & GF_DEMO);
+
+	if (_vm->getGameType() == GType_ST25 && _vm->getPlatform() == Common::kPlatformDOS)
+		_font = new Font(_vm);
+}
+
+Graphics::~Graphics() {
+	if (_egaData)
+		free(_egaData);
+
+	delete _font;
+}
+
+void Graphics::setPalette(const char *paletteFile) {
+	// Set the palette from a PAL file
+
+	Common::SeekableReadStream *palStream = _vm->openFile(paletteFile);
+	byte *palette = new byte[256 * 3];
+	palStream->read(palette, 256 * 3);
+
+	// Expand color components
+	if (_vm->getPlatform() == Common::kPlatformDOS || _vm->getPlatform() == Common::kPlatformMacintosh)
+		for (uint16 i = 0; i < 256 * 3; i++)
+			palette[i] <<= 2;
+
+	_vm->_system->getPaletteManager()->setPalette(palette, 0, 256);
+
+	delete[] palette;
+	delete palStream;
+}
+
+void Graphics::loadEGAData(const char *filename) {
+	// Load EGA palette data
+	if (!_egaMode)
+		return;
+
+	if (!_egaData)
+		_egaData = (byte *)malloc(256);
+
+	Common::SeekableReadStream *egaStream = _vm->openFile(filename);
+	egaStream->read(_egaData, 256);
+	delete egaStream;
+}
+
+void Graphics::drawImage(const char *filename) {
+	// Draw a regular bitmap
+
+	Common::SeekableReadStream *imageStream = _vm->openFile(filename);
+	uint16 xoffset = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();
+	uint16 yoffset = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();
+	uint16 width = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();
+	uint16 height = (_vm->getPlatform() == Common::kPlatformAmiga) ? imageStream->readUint16BE() : imageStream->readUint16LE();	
+
+	byte *pixels = (byte *)malloc(width * height);
+
+	if (_egaMode && _egaData) {
+		// FIXME: This doesn't work right
+		for (uint32 i = 0; i < (uint32)(width * height); i++)
+			pixels[i] = _egaData[imageStream->readByte()];
+	} else {
+		imageStream->read(pixels, width * height);
+	}
+
+	_vm->_system->copyRectToScreen(pixels, width, xoffset, yoffset, width, height);
+	_vm->_system->updateScreen();
+}
+
+void Graphics::drawBackgroundImage(const char *filename) {
+	// Draw an stjr BGD image (palette built-in)
+
+	Common::SeekableReadStream *imageStream = _vm->openFile(filename);
+	byte *palette = new byte[256 * 3];
+	imageStream->read(palette, 256 * 3);
+
+	// Expand color components
+	for (uint16 i = 0; i < 256 * 3; i++)
+		palette[i] <<= 2;
+
+	uint16 xoffset = imageStream->readUint16LE();
+	uint16 yoffset = imageStream->readUint16LE();
+	uint16 width = imageStream->readUint16LE();
+	uint16 height = imageStream->readUint16LE();	
+
+	byte *pixels = (byte *)malloc(width * height);
+	imageStream->read(pixels, width * height);
+
+	_vm->_system->getPaletteManager()->setPalette(palette, 0, 256);
+	_vm->_system->copyRectToScreen(pixels, width, xoffset, yoffset, width, height);
+	_vm->_system->updateScreen();
+
+	delete[] palette;
+	delete imageStream;
+}
+
+}

engines/startrek/sound.cpp
@@ -1,0 +1,192 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL: https://scummvm-startrek.googlecode.com/svn/trunk/sound.cpp $
+ * $Id: sound.cpp 15 2010-06-27 06:13:42Z clone2727 $
+ *
+ */
+ 
+#include "startrek/sound.h"
+
+#include "common/file.h"
+#include "common/macresman.h"
+
+#include "audio/mods/protracker.h"
+#include "audio/decoders/raw.h"
+
+namespace StarTrek {
+
+// Main Sound Functions
+
+Sound::Sound(StarTrekEngine *vm) : _vm(vm) {
+	if (_vm->getPlatform() == Common::kPlatformDOS || _vm->getPlatform() == Common::kPlatformMacintosh) {
+		// The main PC versions use XMIDI. ST25 Demo and Macintosh versions use SMF.
+		if ((_vm->getGameType() == GType_ST25 && _vm->getFeatures() & GF_DEMO) || _vm->getPlatform() == Common::kPlatformMacintosh)
+			_midiParser = MidiParser::createParser_SMF();
+		else
+			_midiParser = MidiParser::createParser_XMIDI();
+			
+		_midiDevice = MidiDriver::detectDevice(MDT_PCSPK|MDT_ADLIB|MDT_MIDI);
+		_midiDriver = MidiDriver::createMidi(_midiDevice);
+		_midiDriver->open();
+		_midiParser->setMidiDriver(_midiDriver);
+		_midiParser->setTimerRate(_midiDriver->getBaseTempo());
+	}
+
+	if (_vm->getPlatform() == Common::kPlatformMacintosh) {
+		_macAudioResFork = new Common::MacResManager();
+		if (!_macAudioResFork->open("Star Trek Audio"))
+			error("Could not open 'Star Trek Audio'");
+		assert(_macAudioResFork->hasResFork());
+	} else
+		_macAudioResFork = 0;
+
+	_soundHandle = new Audio::SoundHandle();
+}
+
+Sound::~Sound() {
+	delete _midiParser;
+	delete _midiDriver;
+	delete _soundHandle;
+	delete _macAudioResFork;
+}
+
+void Sound::playSound(const char *baseSoundName) {
+	if (_vm->getPlatform() == Common::kPlatformAmiga)
+		playAmigaSound(baseSoundName);
+	else if (_vm->getPlatform() == Common::kPlatformMacintosh)
+		playMacSMFSound(baseSoundName);
+	else if (_vm->getFeatures() & GF_DEMO)
+		playSMFSound(baseSoundName);
+	else
+		playXMIDISound(baseSoundName);
+}
+
+void Sound::playSoundEffect(const char *baseSoundName) {
+	if (_vm->getPlatform() == Common::kPlatformAmiga)
+		playAmigaSoundEffect(baseSoundName);
+	else if (_vm->getPlatform() == Common::kPlatformMacintosh)
+		playMacSoundEffect(baseSoundName);
+	else
+		error("PC Sound Effects Not Supported");
+}
+
+// PC Functions
+
+void Sound::playSMFSound(const char *baseSoundName) {
+	Common::String soundName = baseSoundName;
+	
+	soundName += '.';
+	
+	switch (MidiDriver::getMusicType(_midiDevice)) {
+		case MT_MT32:
+			soundName += "ROL";
+			break;
+		case MT_PCSPK:
+			return; // Not supported...
+		default:
+			soundName += "ADL";
+			break;
+	}
+	
+	debug(0, "Playing sound \'%s\'\n", soundName.c_str());
+	Common::SeekableReadStream *soundStream = _vm->openFile(soundName.c_str());
+	
+	byte *soundData = (byte *)malloc(soundStream->size());
+	soundStream->read(soundData, soundStream->size());
+	_midiParser->loadMusic(soundData, soundStream->size());
+	delete soundStream;
+	
+	_midiDriver->setTimerCallback(_midiParser, MidiParser::timerCallback);
+}
+
+void Sound::playXMIDISound(const char *baseSoundName) {
+	Common::String soundName = baseSoundName;
+	
+	soundName += '.';
+	
+	switch (MidiDriver::getMusicType(_midiDevice)) {
+		case MT_MT32:
+			soundName += "MT";
+			break;
+		case MT_PCSPK:
+			soundName += "PC";
+			break;
+		default:
+			soundName += "AD";
+			break;
+	}
+	
+	debug(0, "Playing sound \'%s\'\n", soundName.c_str());
+	Common::SeekableReadStream *soundStream = _vm->openFile(soundName.c_str());
+	
+	byte *soundData = (byte *)malloc(soundStream->size());
+	soundStream->read(soundData, soundStream->size());
+	_midiParser->loadMusic(soundData, soundStream->size());
+	delete soundStream;
+	
+	_midiDriver->setTimerCallback(_midiParser, MidiParser::timerCallback);
+}
+
+// Amiga Functions
+
+void Sound::playAmigaSound(const char *baseSoundName) {
+	// Nope, this is wrong... see http://protracker.de/files/amiga/formats/theplayer_41_format.txt
+#if 0
+	Common::String soundName = baseSoundName;
+	soundName += ".SNG";
+	if (_vm->_mixer->isSoundHandleActive(*_soundHandle))
+		_vm->_mixer->stopHandle(*_soundHandle);
+	_vm->_mixer->playInputStream(Audio::Mixer::kMusicSoundType, _soundHandle, Audio::makeProtrackerStream(_vm->openFile(soundName.c_str())));
+#endif
+}
+
+void Sound::playAmigaSoundEffect(const char *baseSoundName) {
+	Common::String soundName = baseSoundName;
+	soundName += ".SFX";
+
+	if (_vm->_mixer->isSoundHandleActive(*_soundHandle))
+		_vm->_mixer->stopHandle(*_soundHandle);
+
+	Audio::AudioStream *audStream = (Audio::AudioStream *)Audio::makeRawStream(_vm->openFile(soundName.c_str()), 11025, 0);
+	_vm->_mixer->playStream(Audio::Mixer::kSFXSoundType, _soundHandle, audStream);
+}
+
+// Macintosh Functions
+
+void Sound::playMacSMFSound(const char *baseSoundName) {
+	Common::SeekableReadStream *soundStream = _macAudioResFork->getResource(baseSoundName);
+	byte *soundData = (byte *)malloc(soundStream->size());
+	soundStream->read(soundData, soundStream->size());
+	_midiParser->loadMusic(soundData, soundStream->size());
+	delete soundStream;
+	
+	_midiDriver->setTimerCallback(_midiParser, MidiParser::timerCallback);
+}
+
+void Sound::playMacSoundEffect(const char *baseSoundName) {
+	if (_vm->_mixer->isSoundHandleActive(*_soundHandle))
+		_vm->_mixer->stopHandle(*_soundHandle);
+
+	Audio::AudioStream *audStream = (Audio::AudioStream *)Audio::makeRawStream(_macAudioResFork->getResource(baseSoundName), 11025, 0);
+	_vm->_mixer->playStream(Audio::Mixer::kSFXSoundType, _soundHandle, audStream);
+}
+
+} // End of namespace StarTrek