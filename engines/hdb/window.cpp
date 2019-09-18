sev-/scummvm-new

commit a389df141ccb8b93cbdcd70aa1197bc032a8fed4

HDB: Fix some more uninitialized variables

engines/hdb/window.cpp
@@ -119,6 +119,8 @@ Window::Window() {
 	_gfxTitleM = nullptr;
 	_gfxTitleR = nullptr;
 	_gGfxTL = nullptr;
+	_invItemSpaceX = 0;
+	_invItemSpaceY = 0;
 }
 
 Window::~Window() {

engines/hdb/ai-init.cpp
@@ -1020,6 +1020,32 @@ AI::AI() {
 		_cineBlitList[i] = nullptr;
 		_cineFreeList[i] = nullptr;
 	}
+	_targetDoor2S = 0;
+	_targetDoor2Nv = 0;
+	_targetDoor2Pv = 0;
+	_targetDoor2Sv = 0;
+	_target2DoorN = 0;
+	_target2DoorP = 0;
+	_target2DoorS = 0;
+	_target2DoorNv = 0;
+	_target2DoorPv = 0;
+	_target2DoorSv = 0;
+	_target3DoorN = 0;
+	_target3DoorP = 0;
+	_target3DoorS = 0;
+	_target3DoorNv = 0;
+	_target3DoorPv = 0;
+	_target3DoorSv = 0;
+	_targetBridgeU = 0;
+	_targetBridgeD = 0;
+	_targetBridgeL = 0;
+	_targetBridgeR = 0;
+	_targetBridgeMidLR = 0;
+	_targetBridgeMidUD = 0;
+	_touchplateOn = 0;
+	_touchplateOff = 0;
+	_templeTouchpOn = 0;
+	_templeTouchpOff = 0;
 }
 
 AI::~AI() {

commit ca7c93ef78850b348bb4be85820b9b7c9a9a7a88

HDB: Fix regression introduced in a389df1

engines/hdb/window.cpp
@@ -53,6 +53,9 @@ Window::Window() {
 		_tryRestartY = 240;	// (ok)
 		_panicXStop = g_hdb->_screenDrawWidth / 4;
 		_panicZoneFaceX = g_hdb->_screenDrawWidth - 32;
+		// TODO: Check those values!
+		_invItemSpaceX = 0;
+		_invItemSpaceY = 0;
 	} else {
 		_weaponX = (480 - 34);
 		_weaponY = 2;
@@ -119,8 +122,6 @@ Window::Window() {
 	_gfxTitleM = nullptr;
 	_gfxTitleR = nullptr;
 	_gGfxTL = nullptr;
-	_invItemSpaceX = 0;
-	_invItemSpaceY = 0;
 }
 
 Window::~Window() {