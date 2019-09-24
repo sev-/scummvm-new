sev-/scummvm-new

commit 5dae8ccaa7eb7e7578d575dccf3584933b22b9d7

SDL: Remove check for SDL2 in convertEncoding()
SDL_iconv_string() is available even with SDL1

backends/platform/sdl/sdl.cpp
@@ -769,15 +769,11 @@ int SDL_SetColorKey_replacement(SDL_Surface *surface, Uint32 flag, Uint32 key) {
 #endif
 
 char *OSystem_SDL::convertEncoding(const char *to, const char *from, const char *string, size_t length) {
-#if SDL_VERSION_ATLEAST(2, 0, 0)
 	int zeroBytes = 1;
 	if (Common::String(from).hasPrefixIgnoreCase("utf-16"))
 		zeroBytes = 2;
 	if (Common::String(from).hasPrefixIgnoreCase("utf-32"))
 		zeroBytes = 4;
 	return SDL_iconv_string(to, from, string, length + zeroBytes);
-#else
-	return nullptr;
-#endif // SDL_VERSION_ATLEAST(2, 0, 0)
 }
 

commit c0bdabc8cda02eef7da9897e8bb2d69a8801f5bf

SDL: Fix build for older SDL versions
This fixed the breakage caused by 5dae8ccaa7eb7e7578d575dccf3584933b22b9d7
in ancient SDL1 versions that don't have SDL_iconv_string()

Closes gh-1862

backends/platform/sdl/sdl.cpp
@@ -760,6 +760,7 @@ int SDL_SetColorKey_replacement(SDL_Surface *surface, Uint32 flag, Uint32 key) {
 #endif
 
 char *OSystem_SDL::convertEncoding(const char *to, const char *from, const char *string, size_t length) {
+#if SDL_VERSION_ATLEAST(1, 2, 10)
 	int zeroBytes = 1;
 	if (Common::String(from).hasPrefixIgnoreCase("utf-16"))
 		zeroBytes = 2;
@@ -776,7 +777,7 @@ char *OSystem_SDL::convertEncoding(const char *to, const char *from, const char 
 	memcpy(stringCopy, string, length);
 	result = SDL_iconv_string(to, from, stringCopy, length + zeroBytes);
 	free(stringCopy);
-#endif
+#endif // SDL_VERSION_ATLEAST(2, 0, 0)
 	if (result == nullptr)
 		return nullptr;
 
@@ -800,5 +801,8 @@ char *OSystem_SDL::convertEncoding(const char *to, const char *from, const char 
 	memcpy(finalResult, result, newLength + zeroBytes);
 	SDL_free(result);
 	return finalResult;
+#else
+	return ModularBackend::convertEncoding(to, from, string, length);
+#endif // SDL_VERSION_ATLEAST(1, 2, 10)
 }
 