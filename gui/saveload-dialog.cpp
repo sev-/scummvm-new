sev-/scummvm-new

commit 62cd4b886fb729f5413fb4e009ebec29e3de5893

GUI: Introduce dynamic layouts
Prior to this change, a GUI layout was only affected by the screen size.
Now, a layout can additionally be influenced by the GUI dialog and widgets
that uses it. This capability is leveraged to implement the following
features:

* Layout elements that are not bound to a GUI widget do not take space.
   This means that dialogs where the widgets shown depend on for example
   a feature being enabled at configure time no longer have blank spaces.
* Widgets can define a minimal required size for their contents not to be
   cut. For now this is only used for buttons so their width is always
   sufficient for their caption not to be cut. This mechanism could be
   applied to other widget types in the future.

gui/saveload-dialog.cpp
@@ -398,8 +398,7 @@ SaveLoadChooserSimple::SaveLoadChooserSimple(const String &title, const String &
 
 	_delSupport = _metaInfoSupport = _thumbnailSupport = false;
 
-	_container = new ContainerWidget(this, 0, 0, 10, 10);
-//	_container->setHints(THEME_HINT_USE_SHADOW);
+	_container = new ContainerWidget(this, "SaveLoadChooser.Thumbnail");
 }
 
 int SaveLoadChooserSimple::runIntern() {
@@ -472,6 +471,8 @@ void SaveLoadChooserSimple::handleCommand(CommandSender *sender, uint32 cmd, uin
 }
 
 void SaveLoadChooserSimple::reflowLayout() {
+	SaveLoadChooserDialog::reflowLayout();
+
 	if (g_gui.xmlEval()->getVar("Globals.SaveLoadChooser.ExtInfo.Visible") == 1 && (_thumbnailSupport || _saveDateSupport || _playTimeSupport)) {
 		int16 x, y;
 		uint16 w, h;
@@ -536,8 +537,6 @@ void SaveLoadChooserSimple::reflowLayout() {
 		_time->setVisible(false);
 		_playtime->setVisible(false);
 	}
-
-	SaveLoadChooserDialog::reflowLayout();
 }
 
 void SaveLoadChooserSimple::updateSelection(bool redraw) {
@@ -744,6 +743,10 @@ SaveLoadChooserGrid::SaveLoadChooserGrid(const Common::String &title, bool saveM
 	_backgroundType = ThemeEngine::kDialogBackgroundSpecial;
 
 	new StaticTextWidget(this, "SaveLoadChooser.Title", title);
+
+	// The list widget needs to be bound so it takes space in the layout
+	ContainerWidget *list = new ContainerWidget(this, "SaveLoadChooser.List");
+	list->setBackgroundType(ThemeEngine::kWidgetBackgroundNo);
 
 	// Buttons
 	new ButtonWidget(this, "SaveLoadChooser.Delete", _("Cancel"), 0, kCloseCmd);

backends/keymapper/remap-dialog.cpp
@@ -50,6 +50,9 @@ RemapDialog::RemapDialog()
 
 	_scrollBar = new GUI::ScrollBarWidget(this, 0, 0, 0, 0);
 
+	GUI::ContainerWidget *keymapArea = new GUI::ContainerWidget(this, "KeyMapper.KeymapArea");
+	keymapArea->setBackgroundType(GUI::ThemeEngine::kWidgetBackgroundNo);
+
 	new GUI::ButtonWidget(this, "KeyMapper.Close", _("Close"), 0, kCloseCmd);
 }
 

backends/keymapper/remap-dialog.h
@@ -77,8 +77,6 @@ protected:
 	Array<ActionInfo> _currentActions;
 	int _topAction;
 
-	Rect _keymapArea;
-
 	GUI::StaticTextWidget *_kmPopUpDesc;
 	GUI::PopUpWidget *_kmPopUp;
 	//GUI::ContainerWidget *_container;

engines/scumm/dialogs.cpp
@@ -287,6 +287,9 @@ HelpDialog::HelpDialog(const GameSettings &game)
 	new GUI::ButtonWidget(this, "ScummHelp.Close", _("~C~lose"), 0, GUI::kCloseCmd);
 	_prevButton->clearFlags(WIDGET_ENABLED);
 
+	GUI::ContainerWidget *placeHolder = new GUI::ContainerWidget(this, "ScummHelp.HelpText");
+	placeHolder->setBackgroundType(GUI::ThemeEngine::kWidgetBackgroundNo);
+
 	_numLines = HELP_NUM_LINES;
 
 	// Dummy entries

gui/EventRecorder.cpp
@@ -290,6 +290,7 @@ void EventRecorder::init(Common::String recordFileName, RecordMode mode) {
 	}
 	if (_recordMode != kPassthrough) {
 		_controlPanel = new GUI::OnScreenDialog(_recordMode == kRecorderRecord);
+		_controlPanel->reflowLayout();
 	}
 	if (_recordMode == kRecorderPlayback) {
 		applyPlaybackSettings();

gui/ThemeEngine.cpp
@@ -1165,6 +1165,9 @@ void ThemeEngine::drawWidgetBackground(const Common::Rect &r, uint16 hints, Widg
 		return;
 
 	switch (background) {
+	case kWidgetBackgroundNo:
+		break;
+
 	case kWidgetBackgroundBorderSmall:
 		drawDD(kDDWidgetBackgroundSmall, r);
 		break;

gui/ThemeEval.cpp
@@ -105,30 +105,18 @@ void ThemeEval::addWidget(const Common::String &name, int w, int h, const Common
 									typeAlign == Graphics::kTextAlignInvalid ? align : typeAlign);
 
 	_curLayout.top()->addChild(widget);
-	setVar(_curDialog + "." + name + ".Enabled", enabled ? 1 : 0);
+	setVar("Dialog." + _curDialog + "." + name + ".Enabled", enabled ? 1 : 0);
 }
 
 void ThemeEval::addDialog(const Common::String &name, const Common::String &overlays, bool enabled, int inset) {
-	int16 x, y;
-	uint16 w, h;
+	Common::String var = "Dialog." + name;
 
-	ThemeLayout *layout = 0;
+	ThemeLayout *layout = new ThemeLayoutMain(name, overlays, enabled, inset);
 
-	if (overlays == "screen") {
-		layout = new ThemeLayoutMain(inset, inset, g_system->getOverlayWidth() - 2 * inset, g_system->getOverlayHeight() - 2 * inset);
-	} else if (overlays == "screen_center") {
-		layout = new ThemeLayoutMain(-1, -1, -1, -1);
-	} else if (getWidgetData(overlays, x, y, w, h)) {
-		layout = new ThemeLayoutMain(x + inset, y + inset, w - 2 * inset, h - 2 * inset);
-	}
+	if (_layouts.contains(var))
+		delete _layouts[var];
 
-	if (!layout)
-		error("Error when loading dialog position for '%s'", overlays.c_str());
-
-	if (_layouts.contains(name))
-		delete _layouts[name];
-
-	_layouts[name] = layout;
+	_layouts[var] = layout;
 
 	layout->setPadding(
 		getVar("Globals.Padding.Left", 0),
@@ -139,7 +127,7 @@ void ThemeEval::addDialog(const Common::String &name, const Common::String &over
 
 	_curLayout.push(layout);
 	_curDialog = name;
-	setVar(name + ".Enabled", enabled ? 1 : 0);
+	setVar(var + ".Enabled", enabled ? 1 : 0);
 }
 
 void ThemeEval::addLayout(ThemeLayout::LayoutType type, int spacing, bool center) {
@@ -166,6 +154,15 @@ void ThemeEval::addLayout(ThemeLayout::LayoutType type, int spacing, bool center
 void ThemeEval::addSpace(int size) {
 	ThemeLayout *space = new ThemeLayoutSpacing(_curLayout.top(), size);
 	_curLayout.top()->addChild(space);
+}
+
+void ThemeEval::reflowDialogLayout(const Common::String &name, Widget *widgetChain) {
+	if (!_layouts.contains("Dialog." + name)) {
+		warning("No layout found for dialog '%s'", name.c_str());
+		return;
+	}
+
+	_layouts["Dialog." + name]->reflowLayout(widgetChain);
 }
 
 bool ThemeEval::addImportedLayout(const Common::String &name) {

gui/ThemeEval.h
@@ -83,8 +83,9 @@ public:
 	void addPadding(int16 l, int16 r, int16 t, int16 b) { _curLayout.top()->setPadding(l, r, t, b); }
 
 	void closeLayout() { _curLayout.pop(); }
-	void closeDialog() { _curLayout.pop()->reflowLayout(); _curDialog.clear(); }
+	void closeDialog() { _curLayout.pop(); _curDialog.clear(); }
 
+	void reflowDialogLayout(const Common::String &name, Widget *widgetChain);
 	bool getWidgetData(const Common::String &widget, int16 &x, int16 &y, uint16 &w, uint16 &h);
 
 	Graphics::TextAlign getWidgetTextHAlign(const Common::String &widget);

commit ff806737da50cb815b545304cd4a07b40aca6f97

GUI: Fix the SaveLoadDialog with the lowres layouts
The thumbnail layout element is not available in the lowres layouts.
Issue introduced by 62cd4b886fb729f5413fb4e009ebec29e3de5893.

Fixes #11304.

gui/saveload-dialog.cpp
@@ -372,7 +372,8 @@ enum {
 };
 
 SaveLoadChooserSimple::SaveLoadChooserSimple(const String &title, const String &buttonLabel, bool saveMode)
-	: SaveLoadChooserDialog("SaveLoadChooser", saveMode), _list(0), _chooseButton(0), _deleteButton(0), _gfxWidget(0)  {
+	: SaveLoadChooserDialog("SaveLoadChooser", saveMode), _list(0), _chooseButton(0), _deleteButton(0), _gfxWidget(0),
+	_container(0) {
 	_backgroundType = ThemeEngine::kDialogBackgroundSpecial;
 
 	new StaticTextWidget(this, "SaveLoadChooser.Title", title);
@@ -398,7 +399,18 @@ SaveLoadChooserSimple::SaveLoadChooserSimple(const String &title, const String &
 
 	_delSupport = _metaInfoSupport = _thumbnailSupport = false;
 
-	_container = new ContainerWidget(this, "SaveLoadChooser.Thumbnail");
+	addThumbnailContainer();
+}
+
+void SaveLoadChooserSimple::addThumbnailContainer() {
+	// When switching layouts, create / remove the thumbnail container as needed
+	if (g_gui.xmlEval()->getVar("Globals.SaveLoadChooser.ExtInfo.Visible") == 1 && !_container) {
+		_container = new ContainerWidget(this, "SaveLoadChooser.Thumbnail");
+	} else if (g_gui.xmlEval()->getVar("Globals.SaveLoadChooser.ExtInfo.Visible") == 0 && _container) {
+		removeWidget(_container);
+		delete _container;
+		_container = nullptr;
+	}
 }
 
 int SaveLoadChooserSimple::runIntern() {
@@ -471,6 +483,8 @@ void SaveLoadChooserSimple::handleCommand(CommandSender *sender, uint32 cmd, uin
 }
 
 void SaveLoadChooserSimple::reflowLayout() {
+	addThumbnailContainer();
+
 	SaveLoadChooserDialog::reflowLayout();
 
 	if (g_gui.xmlEval()->getVar("Globals.SaveLoadChooser.ExtInfo.Visible") == 1 && (_thumbnailSupport || _saveDateSupport || _playTimeSupport)) {
@@ -531,7 +545,7 @@ void SaveLoadChooserSimple::reflowLayout() {
 
 		updateSelection(false);
 	} else {
-		_container->setVisible(false);
+		if (_container) _container->setVisible(false);
 		_gfxWidget->setVisible(false);
 		_date->setVisible(false);
 		_time->setVisible(false);

gui/saveload-dialog.h
@@ -164,6 +164,7 @@ private:
 
 	String					_resultString;
 
+	void addThumbnailContainer();
 	void updateSelection(bool redraw);
 };
 