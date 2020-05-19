sev-/scummvm-new

commit 1527d754c4eff0f6f5765e72cfabea95e2d06870

QUEEN: Disable load/save until game is properly initialised
Co-authored-by: jdgleaver <jdgleaver@users.noreply.github.com>

engines/queen/queen.cpp
@@ -50,7 +50,7 @@
 namespace Queen {
 
 QueenEngine::QueenEngine(OSystem *syst)
-	: Engine(syst), _debugger(0), randomizer("queen") {
+	: Engine(syst), _gameStarted(false), _debugger(0), randomizer("queen") {
 }
 
 QueenEngine::~QueenEngine() {
@@ -173,7 +173,7 @@ void QueenEngine::update(bool checkPlayerInput) {
 }
 
 bool QueenEngine::canLoadOrSave() const {
-	return !_input->cutawayRunning() && !(_resource->isDemo() || _resource->isInterview());
+	return !_input->cutawayRunning() && !(_resource->isDemo() || _resource->isInterview()) && _gameStarted;
 }
 
 bool QueenEngine::canLoadGameStateCurrently() {
@@ -368,6 +368,9 @@ Common::Error QueenEngine::run() {
 			_logic->currentRoom(_logic->newRoom());
 			_logic->changeRoom();
 			_display->fullscreen(false);
+			// From this point onwards it is safe to use the load/save
+			// menu, so consider game to be 'started'
+			_gameStarted = true;
 			if (_logic->currentRoom() == _logic->newRoom()) {
 				_logic->newRoom(0);
 			}

engines/queen/queen.h
@@ -129,6 +129,7 @@ protected:
 	bool _subtitles;
 	uint32 _lastSaveTime;
 	uint32 _lastUpdateTime;
+	bool _gameStarted;
 
 	BamScene *_bam;
 	BankManager *_bankMan;

commit 64c98ef90fba7e4f9c98efa5b1b5bc5a8a722c3c

QUEEN: Fix loading savegame from Launcher
Loading a savegame from the Launcher for FOTAQ was broken by
commit 1527d75 (QUEEN: Disable load/save until game is properly
initialized). The patch comes from libretro to fix an issue
described as "ScummVM allows you to load a previous save before
the game is properly initialised. At best, this leads to
undefined behaviour. Most of the time it causes a segfault."

This commit allows loading save game earlier than the original
patch so that loading savegames from the launcher works again.
I don't know the details of the issue that was seen in libretro,
but I tested this change with both address-sanitizer and valgrind
on both Linux and macOS, and neither tool report any issue when
loading a savegame from the launcher.

This fixes bug #11327 (FOTAQ: Loading save game from the launcher
doesn't work).

engines/queen/queen.cpp
@@ -352,6 +352,7 @@ Common::Error QueenEngine::run() {
 	syncSoundSettings();
 
 	_logic->start();
+	_gameStarted = true;
 	if (ConfMan.hasKey("save_slot") && canLoadOrSave()) {
 		loadGameState(ConfMan.getInt("save_slot"));
 	}
@@ -364,9 +365,6 @@ Common::Error QueenEngine::run() {
 			_logic->currentRoom(_logic->newRoom());
 			_logic->changeRoom();
 			_display->fullscreen(false);
-			// From this point onwards it is safe to use the load/save
-			// menu, so consider game to be 'started'
-			_gameStarted = true;
 			if (_logic->currentRoom() == _logic->newRoom()) {
 				_logic->newRoom(0);
 			}