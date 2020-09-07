sev-/scummvm-new

commit 8054cbaa5d3816d02ba1a00f5a03e72a00aa5ba9

MOHAWK: Fix mess with stream pointers casting

engines/mohawk/livingbooks_graphics.cpp
@@ -32,18 +32,25 @@
 namespace Mohawk {
 
 LBGraphics::LBGraphics(MohawkEngine_LivingBooks *vm, uint16 width, uint16 height) : GraphicsManager(), _vm(vm) {
-	_bmpDecoder = _vm->isPreMohawk() ? new LivingBooksBitmap_v1() : new MohawkBitmap();
+	_bmpDecoder = nullptr;
+	_bmpDecoderLB = nullptr;
+
+	if (_vm->isPreMohawk())
+		_bmpDecoderLB = new LivingBooksBitmap_v1();
+	else
+		_bmpDecoder =  new MohawkBitmap();
 
 	initGraphics(width, height);
 }
 
 LBGraphics::~LBGraphics() {
 	delete _bmpDecoder;
+	delete _bmpDecoderLB;
 }
 
 MohawkSurface *LBGraphics::decodeImage(uint16 id) {
 	if (_vm->isPreMohawk())
-		return _bmpDecoder->decodeImage(_vm->wrapStreamEndian(ID_BMAP, id));
+		return _bmpDecoderLB->decodeImage(_vm->wrapStreamEndian(ID_BMAP, id));
 
 	return _bmpDecoder->decodeImage(_vm->getResource(ID_TBMP, id));
 }

engines/mohawk/bitmap.cpp
@@ -677,9 +677,7 @@ MohawkSurface *MystBitmap::decodeImage(Common::SeekableReadStream *stream) {
 
 #endif
 
-MohawkSurface *LivingBooksBitmap_v1::decodeImage(Common::SeekableReadStream *stream) {
-	Common::SeekableSubReadStreamEndian *endianStream = (Common::SeekableSubReadStreamEndian *)stream;
-
+MohawkSurface *LivingBooksBitmap_v1::decodeImageLB(Common::SeekableReadStreamEndian *endianStream) {
 	// 12 bytes header for the image
 	_header.format = endianStream->readUint16();
 	_header.bytesPerRow = endianStream->readUint16();
@@ -709,7 +707,7 @@ MohawkSurface *LivingBooksBitmap_v1::decodeImage(Common::SeekableReadStream *str
 		if (lengthBits != LEN_BITS)
 			error("Length bits modified to %d", lengthBits);
 
-		_data = decompressLZ(stream, uncompressedSize);
+		_data = decompressLZ(endianStream, uncompressedSize);
 
 		if (endianStream->pos() != endianStream->size())
 			error("LivingBooksBitmap_v1 decompression failed");
@@ -728,8 +726,8 @@ MohawkSurface *LivingBooksBitmap_v1::decodeImage(Common::SeekableReadStream *str
 		if (!endianStream->isBE())
 			leRLE8 = true;
 
-		_data = stream;
-		stream = nullptr;
+		_data = endianStream;
+		endianStream = nullptr;
 	}
 
 	Graphics::Surface *surface = createSurface(_header.width, _header.height);
@@ -740,7 +738,7 @@ MohawkSurface *LivingBooksBitmap_v1::decodeImage(Common::SeekableReadStream *str
 		drawRaw(surface);
 
 	delete _data;
-	delete stream;
+	delete endianStream;
 
 	MohawkSurface *mhkSurface = new MohawkSurface(surface);
 	mhkSurface->setOffsetX(offsetX);

engines/mohawk/bitmap.h
@@ -167,7 +167,7 @@ public:
 	LivingBooksBitmap_v1() : MohawkBitmap() {}
 	~LivingBooksBitmap_v1() override {}
 
-	MohawkSurface *decodeImage(Common::SeekableReadStream *stream) override;
+	MohawkSurface *decodeImageLB(Common::SeekableReadStreamEndian *stream);
 
 protected:
 	byte getBitsPerPixel() override { return 8; }

engines/mohawk/livingbooks_graphics.h
@@ -44,6 +44,7 @@ protected:
 
 private:
 	MohawkBitmap *_bmpDecoder;
+	LivingBooksBitmap_v1 *_bmpDecoderLB;
 	MohawkEngine_LivingBooks *_vm;
 };
 

commit 93358539ea9cf6305584a38dcd3f6ab0ceda53a3

MOHAWK: Fix crash starting Living Book games
This was a regression from commit 8054cba MOHAWK: Fix mess with
stream pointers casting.

engines/mohawk/livingbooks_graphics.cpp
@@ -50,7 +50,7 @@ LBGraphics::~LBGraphics() {
 
 MohawkSurface *LBGraphics::decodeImage(uint16 id) {
 	if (_vm->isPreMohawk())
-		return _bmpDecoderLB->decodeImage(_vm->wrapStreamEndian(ID_BMAP, id));
+		return _bmpDecoderLB->decodeImageLB(_vm->wrapStreamEndian(ID_BMAP, id));
 
 	return _bmpDecoder->decodeImage(_vm->getResource(ID_TBMP, id));
 }