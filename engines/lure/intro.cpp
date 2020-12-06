NMIError/scummvm

LURE: Fix music/SFX distinction
Distinction between music and SFX was made based on the most significant bit in
the sound number byte. This does not accurately reflect which MIDI sequences
are music or a sound effect.

Instead, I've added a flag which can be passed to
SoundManager::musicInterface_Play to indicate whether the sound is music or a
sound effect. All music is started from hard-coded animation sequences, so this
flag can be passed from ScummVM code when appropriate.

engines/lure/intro.cpp
@@ -233,7 +233,7 @@ void Introduction::playMusic(uint8 soundNumber, bool fadeOut) {
 		Sound.musicInterface_KillAll();
 
 		_currentSound = soundNumber;
-		Sound.musicInterface_Play(_currentSound, 0);
+		Sound.musicInterface_Play(_currentSound, 0, true);
 	}
 }
 

sev-/scummvm-new

engines/lure/intro.cpp
@@ -132,7 +132,7 @@ bool Introduction::show() {
 				Sound.musicInterface_KillAll();
 
 			currentSound = curr_anim->soundNumber;
-			Sound.musicInterface_Play(currentSound, 0);
+			Sound.musicInterface_Play(currentSound, 0, true);
 		}
 
 		bool fadeIn = curr_anim == anim_screens;