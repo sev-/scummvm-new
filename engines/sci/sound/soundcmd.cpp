sev-/scummvm-new

commit e19cbdadd9820f8ef5b27d0747a9d7c4505cf624

SCI: Fully implement kDoAudio(13) for PHARKAS CD
Now that we match SSCI behavior when it comes to interrupting samples
when new samples play, we need to implement kDoAudio(13) so that the CD
version of Freddy Pharkas can detect these interruptions. Otherwise,
the game will softlock if the points sound effect or narration is
interrupted.

engines/sci/sound/soundcmd.cpp
@@ -240,6 +240,12 @@ void SoundCommandParser::processPlaySound(reg_t obj, bool playBed, bool restorin
 
 	_music->soundPlay(musicSlot, restoring);
 
+	// Increment the sample play counter used by Pharkas CD.
+	// SSCI calls kDoAudio(Play) which did this. See kDoAudio(13).
+	if (musicSlot->isSample) {
+		_audio->incrementPlayCounter();
+	}
+
 	// Reset any left-over signals
 	musicSlot->signal = 0;
 	musicSlot->fadeStep = 0;

engines/sci/engine/ksound.cpp
@@ -196,6 +196,10 @@ reg_t kDoAudio(EngineState *s, int argc, reg_t *argv) {
 			return NULL_REG;
 		}
 
+		if (argv[0].toUint16() == kSciAudioPlay) {
+			g_sci->_audio->incrementPlayCounter();
+		}
+
 		debugC(kDebugLevelSound, "kDoAudio: play sample %d, module %d", number, module);
 
 		// return sample length in ticks
@@ -287,19 +291,23 @@ reg_t kDoAudio(EngineState *s, int argc, reg_t *argv) {
 
 		return make_reg(0, 1);
 	case 13:
-		// SSCI returns a serial number for the played audio
-		// here, used in the PointsSound class. The reason is severalfold:
-
-		// 1. SSCI does not support multiple wave effects at once
-		// 2. FPFP may disable its icon bar during the points sound.
-		// 3. Each new sound preempts any sound already playing.
-		// 4. If the points sound is interrupted before completion,
-		// the icon bar could remain disabled.
-
-		// Since points (1) and (3) do not apply to us, we can simply
-		// return a constant here. This is equivalent to the
-		// old behavior, as above.
-		return make_reg(0, 1);
+		// SSCI returns a counter that increments each time a sample
+		//  is played. This is used by the PointsSound and Narrator
+		//  classes to detect if their sound was interrupted by
+		//  another sample playing, since only one can play at a time.
+		//  The counter is incremented on each kDoAudio(Play) and on
+		//  each kDoSound(Play) when the sound is a sample, since SSCI
+		//  just passes those on to kDoAudio.
+		//
+		// When awarding points, the icon bar is often disabled, and
+		//  PointsSound:check polls its signal to detect when the
+		//  sound is completed so that it can re-enable the icon bar.
+		//  If the sound is interrupted by another sample then the icon
+		//  bar could remain permanently disabled, so the play counter
+		//  is stored before playing and PointsSound:check polls for
+		//  a change. The Narrator class also does this to detect
+		//  if speech was interrupted so that the game can continue.
+		return make_reg(0, g_sci->_audio->getPlayCounter());
 	case 17:
 		// Seems to be some sort of audio sync, used in SQ6. Silenced the
 		// warning due to the high level of spam it produces. (takes no params)

engines/sci/sound/audio.cpp
@@ -44,7 +44,7 @@
 namespace Sci {
 
 AudioPlayer::AudioPlayer(ResourceManager *resMan) : _resMan(resMan), _audioRate(11025),
-		_audioCdStart(0), _initCD(false) {
+		_audioCdStart(0), _initCD(false), _playCounter(0) {
 
 	_mixer = g_system->getMixer();
 	_wPlayFlag = false;
@@ -565,6 +565,14 @@ int AudioPlayer::audioCdPosition() {
 
 	// Return the position otherwise (in ticks).
 	return (g_system->getMillis() - _audioCdStart) * 60 / 1000;
+}
+
+void AudioPlayer::incrementPlayCounter() {
+	_playCounter++;
+}
+
+uint16 AudioPlayer::getPlayCounter() {
+	return _playCounter;
 }
 
 } // End of namespace Sci

engines/sci/sound/audio.h
@@ -77,6 +77,9 @@ public:
 
 	void stopAllAudio();
 
+	void incrementPlayCounter();
+	uint16 getPlayCounter();
+
 private:
 	ResourceManager *_resMan;
 	uint16 _audioRate;
@@ -85,6 +88,7 @@ private:
 	uint32 _audioCdStart;
 	bool _wPlayFlag;
 	bool _initCD;
+	uint16 _playCounter;
 };
 
 } // End of namespace Sci

commit cbf17f72877436cc74b1eb87026ca9f96b9b1ef2

SCI32: Fix kDoAudioPlay regression
Fixes kDoSoundPlay crash when called from an SCI32 game, introduced in
e19cbdadd9820f8ef5b27d0747a9d7c4505cf624

engines/sci/sound/soundcmd.cpp
@@ -242,7 +242,7 @@ void SoundCommandParser::processPlaySound(reg_t obj, bool playBed, bool restorin
 
 	// Increment the sample play counter used by Pharkas CD.
 	// SSCI calls kDoAudio(Play) which did this. See kDoAudio(13).
-	if (musicSlot->isSample) {
+	if (_audio != nullptr && musicSlot->isSample) {
 		_audio->incrementPlayCounter();
 	}
 