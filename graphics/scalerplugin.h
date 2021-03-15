sev-/scummvm

GRAPHICS: Add possibility to search for a scaler via its name.

graphics/scalerplugin.h
@@ -217,6 +217,11 @@ public:
 	 * to surfaces.
 	 */
 	uint getMaxExtraPixels() const;
+
+	/**
+	 * Search the scaler plugins for a special plugin based on its name.
+	 */
+	ScalerPlugin *findScalerPlugin(const char *name) const;
 };
 
 /** Convenience shortcut for accessing singleton */

sev-/scummvm-new

graphics/scalerplugin.h
@@ -205,6 +205,11 @@ public:
 	 * to surfaces.
 	 */
 	uint getMaxExtraPixels() const;
+
+	/**
+	 * Search the scaler plugins for a special plugin based on its name.
+	 */
+	Plugin *findScalerPlugin(const char *name) const;
 };
 
 /** Convenience shortcut for accessing singleton */