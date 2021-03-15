sev-/scummvm-new

commit 57ee1f504d3defa4dd2a038fcc3f540e4a78de0d

TWINE: removed shadeAngleTab3 pointer

engines/twine/scene/movements.cpp
@@ -24,6 +24,7 @@
 #include "common/textconsole.h"
 #include "twine/input.h"
 #include "twine/renderer/renderer.h"
+#include "twine/renderer/shadeangletab.h"
 #include "twine/scene/actor.h"
 #include "twine/scene/animations.h"
 #include "twine/scene/collision.h"
@@ -112,15 +113,15 @@ int32 Movements::getAngleAndSetTargetActorDistance(int32 x1, int32 z1, int32 x2,
 
 	const int32 destAngle = (difZ * SCENE_SIZE_HALF) / targetActorDistance;
 
-	int32 startAngle = ANGLE_0;
+	int32 startAngle = ANGLE_135;
 	//	stopAngle  = 0x100;
 
-	while (_engine->_renderer->shadeAngleTab3[startAngle] > destAngle) {
+	while (shadeAngleTable[startAngle] > destAngle) {
 		startAngle++;
 	}
 
-	if (_engine->_renderer->shadeAngleTab3[startAngle] != destAngle) {
-		if ((_engine->_renderer->shadeAngleTab3[startAngle - 1] + _engine->_renderer->shadeAngleTab3[startAngle]) / 2 <= destAngle) {
+	if (shadeAngleTable[startAngle] != destAngle) {
+		if ((shadeAngleTable[startAngle - 1] + shadeAngleTable[startAngle]) / 2 <= destAngle) {
 			startAngle--;
 		}
 	}

engines/twine/renderer/renderer.cpp
@@ -42,7 +42,7 @@ namespace TwinE {
 #define RENDERTYPE_DRAWPOLYGON 1
 #define RENDERTYPE_DRAWSPHERE 2
 
-Renderer::Renderer(TwinEEngine *engine) : _engine(engine), shadeAngleTab3(&shadeAngleTable[384]) {
+Renderer::Renderer(TwinEEngine *engine) : _engine(engine) {
 }
 
 Renderer::~Renderer() {

engines/twine/renderer/renderer.h
@@ -405,8 +405,6 @@ public:
 	Vec3 destPos;
 	Vec3 getHolomapRotation(const int32 angleX, const int32 angleY, const int32 angleZ) const;
 
-	const int16 *const shadeAngleTab3;
-
 	void setLightVector(int32 angleX, int32 angleY, int32 angleZ);
 	void getBaseRotationPosition(int32 x, int32 y, int32 z);
 

engines/twine/renderer/shadeangletab.h
@@ -28,6 +28,9 @@
 
 namespace TwinE {
 
+/**
+ * @brief Caches sin cos table for all possible angles (0-1024 = 0-360 degree)
+ */
 const int16 shadeAngleTable[ANGLE_360] = {
     0, // tab1
     101,

commit 96a87f933e5382a1d647be92e868bace304130dc

TWINE: fixed regression in actor movement
introduced in 57ee1f504d3defa4dd2a038fcc3f540e4a78de0d

engines/twine/scene/movements.cpp
@@ -113,15 +113,15 @@ int32 Movements::getAngleAndSetTargetActorDistance(int32 x1, int32 z1, int32 x2,
 
 	const int32 destAngle = (difZ * SCENE_SIZE_HALF) / targetActorDistance;
 
-	int32 startAngle = ANGLE_135;
-	//	stopAngle  = 0x100;
-
-	while (shadeAngleTable[startAngle] > destAngle) {
+	int32 startAngle = ANGLE_0;
+	//	stopAngle  = ANGLE_90;
+	const int16 *shadeAngleTab3(&shadeAngleTable[ANGLE_135]);
+	while (shadeAngleTab3[startAngle] > destAngle) {
 		startAngle++;
 	}
 
-	if (shadeAngleTable[startAngle] != destAngle) {
-		if ((shadeAngleTable[startAngle - 1] + shadeAngleTable[startAngle]) / 2 <= destAngle) {
+	if (shadeAngleTab3[startAngle] != destAngle) {
+		if ((shadeAngleTab3[startAngle - 1] + shadeAngleTab3[startAngle]) / 2 <= destAngle) {
 			startAngle--;
 		}
 	}