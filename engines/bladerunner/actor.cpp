sev-/scummvm-new

commit b83139a1fc53e4847c9589bee5c63fa9a713b19b

BLADERUNNER: Clean up and synch screenRectangle and bbox
For actors and their corresponding screenObjects

This should fix the issue with Sadik's remnant hitbox in the middle of the bridge at UG18

engines/bladerunner/actor.cpp
@@ -809,6 +809,37 @@ bool Actor::tick(bool forceDraw, Common::Rect *screenRect) {
 		}
 	}
 
+#if !BLADERUNNER_ORIGINAL_BUGS
+	// For consistency we need to init the screen rectangle and bbox for the actor's *scene object*
+	// in a new scene (since we also reset the screen rectangle at Scene::open())
+	// for the case of the actor not moving
+	if (_vm->_scene->getSetId() == _setId
+	    && !_isInvisible
+	    && _vm->_sceneObjects->findById(_id + kSceneObjectOffsetActors) != -1
+	    && (_vm->_sceneObjects->isEmptyScreenRectangle(_id + kSceneObjectOffsetActors)
+	        || _vm->_sceneObjects->compareScreenRectangle(_id + kSceneObjectOffsetActors, _screenRectangle) != 0)
+	) {
+		if (isVisible) {
+			Vector3 pos = getPosition();
+			int facing = getFacing();
+			setAtXYZ(pos, facing, false, _isMoving, false);
+		} else {
+			resetScreenRectangleAndBbox();
+			_vm->_sceneObjects->resetScreenRectangleAndBbox(_id + kSceneObjectOffsetActors);
+		}
+	}
+
+	if ((_vm->_scene->getSetId() != _setId || _isInvisible || !isVisible)
+	    && !_screenRectangle.isEmpty()
+	) {
+		resetScreenRectangleAndBbox();
+		if (_vm->_sceneObjects->findById(_id + kSceneObjectOffsetActors) != -1
+		    && !_vm->_sceneObjects->isEmptyScreenRectangle(_id + kSceneObjectOffsetActors)) {
+			_vm->_sceneObjects->resetScreenRectangleAndBbox(_id + kSceneObjectOffsetActors);
+		}
+	}
+#endif
+
 	if (needsUpdate) {
 		// timeLeft is supposed to be negative or 0 here in the original!
 		int32 nextFrameTime = timeLeft + _frameMs;
@@ -951,6 +982,14 @@ void Actor::setBoundingBox(const Vector3 &position, bool retired) {
 		             position.y + 72.0f,
 		             position.z + 12.0f);
 	}
+}
+
+void Actor::resetScreenRectangleAndBbox() {
+	_screenRectangle.left   = -1;
+	_screenRectangle.top    = -1;
+	_screenRectangle.right  = -1;
+	_screenRectangle.bottom = -1;
+	_bbox.setXYZ(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
 }
 
 float Actor::distanceFromView(View *view) const{

engines/bladerunner/actor.h
@@ -169,6 +169,8 @@ public:
 	void tickCombat();
 	bool draw(Common::Rect *screenRect);
 
+	void resetScreenRectangleAndBbox();
+
 	int getSetId()  const;
 	void setSetId(int setId);
 	const BoundingBox &getBoundingBox() const { return _bbox; }

engines/bladerunner/debugger.cpp
@@ -2288,8 +2288,11 @@ void Debugger::drawSceneObjects() {
 				) {
 					color = _vm->_surfaceFront.format.RGBToColor(255, 0, 0);
 					drawBBox(a, b, _vm->_view, &_vm->_surfaceFront, color);
+					Actor *actor = _vm->_actors[sceneObject->id - kSceneObjectOffsetActors];
+					//const Common::Rect &screenRect = actor->getScreenRectangle();
+					//_vm->_surfaceFront.frameRect(screenRect, color);
 					_vm->_surfaceFront.frameRect(sceneObject->screenRectangle, color);
-					_vm->_mainFont->drawString(&_vm->_surfaceFront, _vm->_textActorNames->getText(sceneObject->id - kSceneObjectOffsetActors), pos.x, pos.y, _vm->_surfaceFront.w, color);
+					_vm->_mainFont->drawString(&_vm->_surfaceFront, _vm->_textActorNames->getText(actor->getId()), pos.x, pos.y, _vm->_surfaceFront.w, color);
 				}
 				break;
 			case kSceneObjectTypeItem:

engines/bladerunner/scene_objects.cpp
@@ -341,6 +341,38 @@ void SceneObjects::updateObstacles() {
 	_vm->_obstacles->backup();
 }
 
+bool SceneObjects::isEmptyScreenRectangle(int sceneObjectId) {
+	int index = findById(sceneObjectId);
+	if (index != -1) {
+		const SceneObject *sceneObject = &_sceneObjects[index];
+		return sceneObject->screenRectangle.isEmpty();
+	}
+	return true;
+}
+
+int SceneObjects::compareScreenRectangle(int sceneObjectId, const Common::Rect &rectangle) {
+	int index = findById(sceneObjectId);
+	if (index != -1) {
+		const SceneObject *sceneObject = &_sceneObjects[index];
+		if (sceneObject->screenRectangle == rectangle) {
+			return 0;
+		}
+	}
+	return -1;
+}
+
+void SceneObjects::resetScreenRectangleAndBbox(int sceneObjectId) {
+	int index = findById(sceneObjectId);
+	if (index != -1) {
+		SceneObject *sceneObject = &_sceneObjects[index];
+		sceneObject->screenRectangle.left   = -1;
+		sceneObject->screenRectangle.top    = -1;
+		sceneObject->screenRectangle.right  = -1;
+		sceneObject->screenRectangle.bottom = -1;
+		sceneObject->boundingBox.setXYZ(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	}
+}
+
 void SceneObjects::save(SaveFileWriteStream &f) {
 	f.writeInt(_count);
 	for (int i = 0; i < kSceneObjectCount; ++i) {

engines/bladerunner/scene_objects.h
@@ -87,12 +87,17 @@ public:
 	void setIsObstacle(int sceneObjectId, bool isObstacle);
 	void setIsTarget(int sceneObjectId, bool isTarget);
 	void updateObstacles();
+	int getCount() { return _count; }
+
+	int findById(int sceneObjectId) const;
+	bool isEmptyScreenRectangle(int sceneObjectId);
+	int compareScreenRectangle(int sceneObjectId, const Common::Rect &rectangle);
+	void resetScreenRectangleAndBbox(int sceneObjectId);
 
 	void save(SaveFileWriteStream &f);
 	void load(SaveFileReadStream &f);
 
 private:
-	int findById(int sceneObjectId) const;
 	bool addSceneObject(int sceneObjectId, SceneObjectType sceneObjectType, const BoundingBox &boundingBox, const Common::Rect &screenRectangle, bool isClickable, bool isObstacle, uint8 unknown1, bool isTarget, bool isMoving, bool isRetired);
 	int findEmpty() const;
 };

engines/bladerunner/scene.cpp
@@ -179,6 +179,11 @@ bool Scene::open(int setId, int sceneId, bool isLoadingGame) {
 	for (int i = 0; i != actorCount; ++i) {
 		Actor *actor = _vm->_actors[i];
 		if (actor->getSetId() == setId) {
+			//debug("Actor added: %d", i);
+#if !BLADERUNNER_ORIGINAL_BUGS
+			// ensure that actors' "hotspot" areas from previous scene are cleared up
+			actor->resetScreenRectangleAndBbox();
+#endif
 			_vm->_sceneObjects->addActor(
 				i + kSceneObjectOffsetActors,
 				actor->getBoundingBox(),

commit 5fe829a7246bdae47b16ae7e0029ad5f20d8a1d9

BLADERUNNER: Fix bad boundbox for corpses
Some of these issues were caused by commit b83139a1fc53e4847c9589bee5c63fa9a713b19b

Others were original bugs (mutants 2 and 3), and both rats.
This commit includes a fix for Grayford's death animation when shot (in Act 4)

engines/bladerunner/actor.cpp
@@ -202,6 +202,10 @@ void Actor::changeAnimationMode(int animationMode, bool force) {
 	}
 }
 
+int Actor::getFPS() const {
+	return _fps;
+}
+
 void Actor::setFPS(int fps) {
 	_fps = fps;
 
@@ -398,6 +402,7 @@ void Actor::movementTrackNext(bool omitAiScript) {
 			}
 		} else {
 			setSetId(waypointSetId);
+
 			setAtXYZ(waypointPosition, angle, true, false, false);
 
 			if (!delay) {
@@ -682,6 +687,13 @@ bool Actor::tick(bool forceDraw, Common::Rect *screenRect) {
 	int32 timeLeft = 0;
 	bool needsUpdate = false;
 	if (_fps > 0) {
+		// Note that when (some?) actors are retired (eg. Zuben)
+		// their _fps is still > 0 so they will periodically set needsUpdate to true in their tick() (here)
+		// Also, the moment an actor is retired does not necessarily means their death animation finished playing
+		// Their death animation may finish a while later.
+		// Thus, until it finished, their screen rectangle will be likely changing at the draw() operation.
+		// Typically at the end of a death animation, the actor keeps updating for the same frame
+		// (ie the last of the death animation). At that point their screen rectangle won't change at the draw() operation.
 		timerUpdate(kActorTimerAnimationFrame);
 		timeLeft = timerLeft(kActorTimerAnimationFrame);
 		needsUpdate = (timeLeft <= 0);
@@ -803,6 +815,9 @@ bool Actor::tick(bool forceDraw, Common::Rect *screenRect) {
 
 	bool isVisible = false;
 	if (!_isInvisible) {
+		// draw() will set the new screenRect for the actor
+		// based on the current animation frame
+		// the new screenRect may be empty, in which case draw returns false (thus isVisible will be false then).
 		isVisible = draw(screenRect);
 		if (isVisible) {
 			_screenRectangle = *screenRect;
@@ -813,19 +828,28 @@ bool Actor::tick(bool forceDraw, Common::Rect *screenRect) {
 	// For consistency we need to init the screen rectangle and bbox for the actor's *scene object*
 	// in a new scene (since we also reset the screen rectangle at Scene::open())
 	// for the case of the actor not moving
+
 	if (_vm->_scene->getSetId() == _setId
 	    && !_isInvisible
-	    && _vm->_sceneObjects->findById(_id + kSceneObjectOffsetActors) != -1
-	    && (_vm->_sceneObjects->isEmptyScreenRectangle(_id + kSceneObjectOffsetActors)
-	        || _vm->_sceneObjects->compareScreenRectangle(_id + kSceneObjectOffsetActors, _screenRectangle) != 0)
-	) {
-		if (isVisible) {
-			Vector3 pos = getPosition();
-			int facing = getFacing();
-			setAtXYZ(pos, facing, false, _isMoving, false);
-		} else {
-			resetScreenRectangleAndBbox();
-			_vm->_sceneObjects->resetScreenRectangleAndBbox(_id + kSceneObjectOffsetActors);
+	    && _vm->_sceneObjects->findById(_id + kSceneObjectOffsetActors) != -1) {
+		if (_vm->_sceneObjects->isEmptyScreenRectangle(_id + kSceneObjectOffsetActors)) {
+			if (isVisible) {
+				Vector3 pos = getPosition();
+				int facing = getFacing();
+				setAtXYZ(pos, facing, true, _isMoving, _isRetired);
+			} else {
+				resetScreenRectangleAndBbox();
+				_vm->_sceneObjects->resetScreenRectangleAndBbox(_id + kSceneObjectOffsetActors);
+			}
+		} else if (_vm->_sceneObjects->compareScreenRectangle(_id + kSceneObjectOffsetActors, _screenRectangle) != 0) {
+			if (isVisible) {
+				// keep actor's _screenRectangle synched with sceneObject's actor's screen rectange
+				// don't do a setAtXYZ here though
+				_vm->_sceneObjects->synchScreenRectangle(_id + kSceneObjectOffsetActors, _screenRectangle);
+			} else {
+				resetScreenRectangleAndBbox();
+				_vm->_sceneObjects->resetScreenRectangleAndBbox(_id + kSceneObjectOffsetActors);
+			}
 		}
 	}
 
@@ -965,7 +989,7 @@ void Actor::setFacing(int facing, bool halfOrSet) {
 }
 
 void Actor::setBoundingBox(const Vector3 &position, bool retired) {
-	if (retired) {
+	if (retired || _isRetired) {
 		_bbox.setXYZ(position.x - (_retiredWidth / 2.0f),
 		             position.y,
 		             position.z - (_retiredWidth / 2.0f),

engines/bladerunner/actor.h
@@ -140,6 +140,7 @@ public:
 	Vector3 getPosition() const { return _position; }
 
 	void changeAnimationMode(int animationMode, bool force = false);
+	int  getFPS() const;
 	void setFPS(int fps);
 	void increaseFPS();
 

engines/bladerunner/boundingbox.cpp
@@ -26,6 +26,11 @@
 
 namespace BladeRunner {
 
+// TO VERIFY
+// First vertex is typically the bottom left point (x0, y0, z0)
+// and second vertex is the top right (x1, y1, z1)
+// Hence, we also assume that x0 < x1, y0 < y1 and z0 < z1 (ie. see how the inside() method makes its calculation)
+// TODO Maybe add a check here to catch any exceptions?
 BoundingBox::BoundingBox(float x0, float y0, float z0, float x1, float y1, float z1) {
 	_vertices[0].x = x0;
 	_vertices[0].y = y0;

engines/bladerunner/debugger.cpp
@@ -2056,11 +2056,12 @@ bool Debugger::cmdList(int argc, const char **argv) {
 						             sceneObject->isPresent?   "T" : "F",
 						             sceneObject->isObstacle?  "T" : "F",
 						             sceneObject->isMoving?    "T" : "F");
-						debugPrintf("    Goal: %d, Set: %d, Anim mode: %d id:%d showDmg: %s inCombat: %s\n",
+						debugPrintf("    Goal: %d, Set: %d, Anim mode: %d id:%d fps: %d showDmg: %s inCombat: %s\n",
 						             actor->getGoal(),
 						             actor->getSetId(),
 						             actor->getAnimationMode(),
 						             actor->getAnimationId(),
+						             actor->getFPS(),
 						             actor->getFlagDamageAnimIfMoving()? "T" : "F",
 						             actor->inCombat()? "T" : "F");
 						debugPrintf("    Pos(%02.2f,%02.2f,%02.2f)\n",

engines/bladerunner/game_constants.h
@@ -3496,17 +3496,19 @@ enum GoalFreeSlotA { // Rat A
 	kGoalFreeSlotAAct5Default        = 400,
 	kGoalFreeSlotAAct5Prepare        = 405,
 	kGoalFreeSlotAAct5KP02Attack     = 406,
+	kGoalFreeSlotAGoneIntermediate   = 411,
 	kGoalFreeSlotAGone               = 599
 };
 
 enum GoalFreeSlotB { // Rat B
-	kGoalFreeSlotBAct4Default     = 300,
-	kGoalFreeSlotBAct4WalkAround  = 301,
-	kGoalFreeSlotBAct4AttackMcCoy = 302,
-	kGoalFreeSlotBAct5Default     = 400,
-	kGoalFreeSlotBAct5Prepare     = 405,
-	kGoalFreeSlotBAct5KP02Attack  = 406,
-	kGoalFreeSlotBGone            = 599
+	kGoalFreeSlotBAct4Default      = 300,
+	kGoalFreeSlotBAct4WalkAround   = 301,
+	kGoalFreeSlotBAct4AttackMcCoy  = 302,
+	kGoalFreeSlotBAct5Default      = 400,
+	kGoalFreeSlotBAct5Prepare      = 405,
+	kGoalFreeSlotBAct5KP02Attack   = 406,
+	kGoalFreeSlotBGoneIntermediate = 411,
+	kGoalFreeSlotBGone             = 599
 };
 
 enum GoalMaggie {

engines/bladerunner/scene_objects.cpp
@@ -373,6 +373,17 @@ void SceneObjects::resetScreenRectangleAndBbox(int sceneObjectId) {
 	}
 }
 
+void SceneObjects::synchScreenRectangle(int sceneObjectId, const Common::Rect &targetScreenRect) {
+	int index = findById(sceneObjectId);
+	if (index != -1) {
+		SceneObject *sceneObject = &_sceneObjects[index];
+		sceneObject->screenRectangle.left   = targetScreenRect.left;
+		sceneObject->screenRectangle.top    = targetScreenRect.top;
+		sceneObject->screenRectangle.right  = targetScreenRect.right;
+		sceneObject->screenRectangle.bottom = targetScreenRect.bottom;
+	}
+}
+
 void SceneObjects::save(SaveFileWriteStream &f) {
 	f.writeInt(_count);
 	for (int i = 0; i < kSceneObjectCount; ++i) {

engines/bladerunner/scene_objects.h
@@ -93,6 +93,7 @@ public:
 	bool isEmptyScreenRectangle(int sceneObjectId);
 	int compareScreenRectangle(int sceneObjectId, const Common::Rect &rectangle);
 	void resetScreenRectangleAndBbox(int sceneObjectId);
+	void synchScreenRectangle(int sceneObjectId, const Common::Rect &targetScreenRect);
 
 	void save(SaveFileWriteStream &f);
 	void load(SaveFileReadStream &f);

engines/bladerunner/script/ai/free_slot_a.cpp
@@ -106,7 +106,11 @@ bool AIScriptFreeSlotA::Update() {
 			if (Actor_Query_Which_Set_In(kActorFreeSlotA) != Player_Query_Current_Set()) {
 				Game_Flag_Reset(kFlagRatWalkingAround);
 				Game_Flag_Reset(kFlagUG15BridgeWillBreak);
+#if BLADERUNNER_ORIGINAL_BUGS
 				Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotADefault);
+#else
+				Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotAGoneIntermediate);
+#endif
 			}
 			break;
 
@@ -143,7 +147,11 @@ bool AIScriptFreeSlotA::Update() {
 				if (Actor_Query_Goal_Number(kActorFreeSlotA) == kGoalFreeSlotAGone) {
 					if (Actor_Query_Which_Set_In(kActorFreeSlotA) != Player_Query_Current_Set()) {
 						Non_Player_Actor_Combat_Mode_Off(kActorFreeSlotA);
+#if BLADERUNNER_ORIGINAL_BUGS
 						Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotAAct5Default);
+#else
+						Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotAGoneIntermediate);
+#endif
 						return true;
 					}
 				}
@@ -244,11 +252,10 @@ void AIScriptFreeSlotA::OtherAgentExitedThisSet(int otherActorId) {
 		if (Global_Variable_Query(kVariableChapter) == 4) {
 			Game_Flag_Reset(kFlagRatWalkingAround);
 			Game_Flag_Reset(kFlagUG15BridgeWillBreak);
-			Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotADefault);
 		} else if (Global_Variable_Query(kVariableChapter) == 5) {
 			Non_Player_Actor_Combat_Mode_Off(kActorFreeSlotA);
-			Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotAAct5Default);
 		}
+		Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotAGoneIntermediate);
 	}
 #endif // BLADERUNNER_ORIGINAL_BUGS
 	// return false;
@@ -267,6 +274,12 @@ void AIScriptFreeSlotA::ShotAtAndMissed() {
 bool AIScriptFreeSlotA::ShotAtAndHit() {
 	if (Actor_Query_In_Set(kActorFreeSlotA, kSetUG15)) {
 		checkIfOnBridge();
+		// This goal "kGoalFreeSlotAUG15Die" circumvents the proper process
+		// a proper combat with the rat and it dying when its health reaches <= 0
+		// ie. being "retired".
+		// Thus, since the rat never actually 'dies' from being shot,
+		// its Actor::retire() is not called in this case (UG15 bridge rat),
+		// and so its bounding box is not affected. Thus, the rat corpse remains clickable.
 		Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotAUG15Die);
 		return true;
 	}
@@ -385,8 +398,23 @@ bool AIScriptFreeSlotA::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 		AI_Movement_Track_Repeat(kActorFreeSlotA);
 		break;
 
-	case kGoalFreeSlotAGone:
+#if BLADERUNNER_ORIGINAL_BUGS
+#else
+	case kGoalFreeSlotAGoneIntermediate:
 		Actor_Set_Health(kActorFreeSlotA, 20, 20);
+		if (Global_Variable_Query(kVariableChapter) == 4) {
+			Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotADefault);
+		} else if (Global_Variable_Query(kVariableChapter) == 5) {
+			Actor_Set_Goal_Number(kActorFreeSlotA, kGoalFreeSlotAAct5Default);
+		}
+		break;
+#endif
+
+	case kGoalFreeSlotAGone:
+#if BLADERUNNER_ORIGINAL_BUGS
+		Actor_Set_Health(kActorFreeSlotA, 20, 20);
+#endif
+		// A bug? What does this friendliness affect?
 		Actor_Set_Friendliness_To_Other(kActorFreeSlotA, kActorMcCoy, 40);
 		break;
 

engines/bladerunner/script/ai/free_slot_b.cpp
@@ -81,7 +81,11 @@ bool AIScriptFreeSlotB::Update() {
 
 		case kGoalFreeSlotBGone:
 			if (Actor_Query_Which_Set_In(kActorFreeSlotB) != Player_Query_Current_Set()) {
+#if BLADERUNNER_ORIGINAL_BUGS
 				Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBAct4Default);
+#else
+				Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBGoneIntermediate);
+#endif
 			}
 			break;
 
@@ -104,7 +108,11 @@ bool AIScriptFreeSlotB::Update() {
 			if (Actor_Query_Goal_Number(kActorFreeSlotB) == kGoalFreeSlotBGone) {
 				if (Actor_Query_Which_Set_In(kActorFreeSlotB) != Player_Query_Current_Set()) {
 					Non_Player_Actor_Combat_Mode_Off(kActorFreeSlotB);
+#if BLADERUNNER_ORIGINAL_BUGS
 					Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBAct5Default);
+#else
+					Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBGoneIntermediate);
+#endif
 					return true;
 				}
 			}
@@ -184,12 +192,10 @@ void AIScriptFreeSlotB::OtherAgentEnteredThisSet(int otherActorId) {
 void AIScriptFreeSlotB::OtherAgentExitedThisSet(int otherActorId) {
 #if !BLADERUNNER_ORIGINAL_BUGS
 	if (otherActorId == kActorMcCoy && Actor_Query_Goal_Number(kActorFreeSlotB) == kGoalFreeSlotBGone) {
-		if (Global_Variable_Query(kVariableChapter) == 4) {
-			Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBAct4Default);
-		} else if (Global_Variable_Query(kVariableChapter) == 5) {
+		if (Global_Variable_Query(kVariableChapter) == 5) {
 			Non_Player_Actor_Combat_Mode_Off(kActorFreeSlotB);
-			Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBAct5Default);
 		}
+		Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBGoneIntermediate);
 	}
 
 #endif // BLADERUNNER_ORIGINAL_BUGS
@@ -250,8 +256,22 @@ bool AIScriptFreeSlotB::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 		AI_Movement_Track_Repeat(kActorFreeSlotB);
 		break;
 
-	case kGoalFreeSlotBGone:
+#if BLADERUNNER_ORIGINAL_BUGS
+#else
+	case kGoalFreeSlotBGoneIntermediate:
 		Actor_Set_Health(kActorFreeSlotB, 20, 20);
+		if (Global_Variable_Query(kVariableChapter) == 4) {
+			Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBAct4Default);
+		} else if (Global_Variable_Query(kVariableChapter) == 5) {
+			Actor_Set_Goal_Number(kActorFreeSlotB, kGoalFreeSlotBAct5Default);
+		}
+		break;
+#endif
+
+	case kGoalFreeSlotBGone:
+#if BLADERUNNER_ORIGINAL_BUGS
+		Actor_Set_Health(kActorFreeSlotB, 20, 20);
+#endif
 		break;
 
 	default:

engines/bladerunner/script/ai/mutant1.cpp
@@ -372,6 +372,9 @@ bool AIScriptMutant1::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 			Actor_Set_Friendliness_To_Other(kActorMutant1, kActorMcCoy, 45);
 		}
 
+		// code repeated also in case 599 which precedes this one
+		// redundant?
+		// results in additional reduction in friendliness and increase of aggressiveness for the other two mutants
 		Actor_Modify_Friendliness_To_Other(kActorMutant2, kActorMcCoy, -10);
 		Actor_Modify_Friendliness_To_Other(kActorMutant3, kActorMcCoy, -20);
 		Actor_Modify_Combat_Aggressiveness(kActorMutant2, 10);
@@ -387,7 +390,8 @@ bool AIScriptMutant1::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 
 	case 599:
 		AI_Movement_Track_Flush(kActorMutant1);
-		Actor_Change_Animation_Mode(kActorMutant1, 48);
+		Actor_Change_Animation_Mode(kActorMutant1, kAnimationModeDie);
+		// results in additional reduction in friendlinees and increase of aggressiveness for the other two mutants
 		Actor_Modify_Friendliness_To_Other(kActorMutant2, kActorMcCoy, -10);
 		Actor_Modify_Friendliness_To_Other(kActorMutant3, kActorMcCoy, -20);
 		Actor_Modify_Combat_Aggressiveness(kActorMutant2, 10);

engines/bladerunner/script/ai/mutant2.cpp
@@ -71,7 +71,12 @@ bool AIScriptMutant2::Update() {
 
 		case 599:
 			if (Actor_Query_Which_Set_In(kActorMutant2) != Player_Query_Current_Set()) {
+#if BLADERUNNER_ORIGINAL_BUGS
 				Actor_Set_Goal_Number(kActorMutant2, 403);
+#else
+				// intermediate goal to set new Health (revive for reuse)
+				Actor_Set_Goal_Number(kActorMutant2, 411);
+#endif // BLADERUNNER_ORIGINAL_BUGS
 			}
 			break;
 		}
@@ -337,17 +342,18 @@ bool AIScriptMutant2::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 		}
 		return true;
 
-	case 590:
+#if BLADERUNNER_ORIGINAL_BUGS
+#else
+	case 411:
+		// We need the additional intermediate goal 411 (as mutant1 has)
+		// so that we set the health here, instead of the when the goal is set to 599 (dying)
+		// Setting the health "revives" the mutant, which would result in their bound box being reduced to a point
+		// (see Actor::setHealth() call to retire(false, 0, 0, -1))
+		// and thus their corpse being unclickable after McCoy shot them.
+		// Goal 411 does this, but is set only when McCoy is no longer present in the scene/set.
 		AI_Movement_Track_Flush(kActorMutant2);
-		AI_Movement_Track_Append(kActorMutant2, 39, 100);
-		AI_Movement_Track_Repeat(kActorMutant2);
-		return true;
-
-	case 599:
-		AI_Movement_Track_Flush(kActorMutant2);
-		Actor_Change_Animation_Mode(kActorMutant2, 48);
 		Actor_Set_Intelligence(kActorMutant2, 20);
-		Actor_Set_Health(71, 10 * Query_Difficulty_Level() + 50, 10 * Query_Difficulty_Level() + 50);
+		Actor_Set_Health(kActorMutant2, 10 * Query_Difficulty_Level() + 50, 10 * Query_Difficulty_Level() + 50);
 
 		if (Game_Flag_Query(kFlagCT04HomelessKilledByMcCoy)) {
 			Actor_Set_Combat_Aggressiveness(kActorMutant2, 60);
@@ -357,6 +363,39 @@ bool AIScriptMutant2::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 			Actor_Set_Friendliness_To_Other(kActorMutant2, kActorMcCoy, 50);
 		}
 
+		// code repeated also in case 599 which precedes this one
+		// redundant? 
+		// results in additional reduction in friendliness and increase of aggressiveness for the other two mutants
+		Actor_Modify_Friendliness_To_Other(kActorMutant1, kActorMcCoy, -15);
+		Actor_Modify_Friendliness_To_Other(kActorMutant3, kActorMcCoy, -20);
+		Actor_Modify_Combat_Aggressiveness(kActorMutant1, 10);
+		Actor_Modify_Combat_Aggressiveness(kActorMutant3, 15);
+		Actor_Set_Goal_Number(kActorMutant2, 403);
+		return true;
+#endif // BLADERUNNER_ORIGINAL_BUGS
+
+	case 590:
+		AI_Movement_Track_Flush(kActorMutant2);
+		AI_Movement_Track_Append(kActorMutant2, 39, 100);
+		AI_Movement_Track_Repeat(kActorMutant2);
+		return true;
+
+	case 599:
+		AI_Movement_Track_Flush(kActorMutant2);
+		Actor_Change_Animation_Mode(kActorMutant2, kAnimationModeDie);
+#if BLADERUNNER_ORIGINAL_BUGS
+		Actor_Set_Intelligence(kActorMutant2, 20);
+		Actor_Set_Health(kActorMutant2, 10 * Query_Difficulty_Level() + 50, 10 * Query_Difficulty_Level() + 50);
+
+		if (Game_Flag_Query(kFlagCT04HomelessKilledByMcCoy)) {
+			Actor_Set_Combat_Aggressiveness(kActorMutant2, 60);
+			Actor_Set_Friendliness_To_Other(kActorMutant2, kActorMcCoy, 30);
+		} else {
+			Actor_Set_Combat_Aggressiveness(kActorMutant2, 40);
+			Actor_Set_Friendliness_To_Other(kActorMutant2, kActorMcCoy, 50);
+		}
+#endif // BLADERUNNER_ORIGINAL_BUGS
+		// results in additional reduction in friendliness and increase of aggressiveness for the other two mutants
 		Actor_Modify_Friendliness_To_Other(kActorMutant1, kActorMcCoy, -15);
 		Actor_Modify_Friendliness_To_Other(kActorMutant3, kActorMcCoy, -20);
 		Actor_Modify_Combat_Aggressiveness(kActorMutant1, 10);

engines/bladerunner/script/ai/mutant3.cpp
@@ -71,7 +71,12 @@ bool AIScriptMutant3::Update() {
 
 		case 599:
 			if (Actor_Query_Which_Set_In(kActorMutant3) != Player_Query_Current_Set()) {
+#if BLADERUNNER_ORIGINAL_BUGS
 				Actor_Set_Goal_Number(kActorMutant3, 403);
+#else
+				// intermediate goal to set new Health (revive for reuse)
+				Actor_Set_Goal_Number(kActorMutant3, 411);
+#endif // BLADERUNNER_ORIGINAL_BUGS
 			}
 			break;
 		}
@@ -348,15 +353,16 @@ bool AIScriptMutant3::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 		}
 		break;
 
-	case 590:
+#if BLADERUNNER_ORIGINAL_BUGS
+#else
+	case 411:
+		// We need the additional intermediate goal 411 (as mutant1 has)
+		// so that we set the health here, instead of the when the goal is set to 599 (dying)
+		// Setting the health "revives" the mutant, which would result in their bound box being reduced to a point
+		// (see Actor::setHealth() call to retire(false, 0, 0, -1))
+		// and thus their corpse being unclickable after McCoy shot them.
+		// Goal 411 does this, but is set only when McCoy is no longer present in the scene/set.
 		AI_Movement_Track_Flush(kActorMutant3);
-		AI_Movement_Track_Append(kActorMutant3, 39, 100);
-		AI_Movement_Track_Repeat(kActorMutant3);
-		break;
-
-	case 599:
-		AI_Movement_Track_Flush(kActorMutant3);
-		Actor_Change_Animation_Mode(kActorMutant3, 48);
 		Actor_Set_Intelligence(kActorMutant3, 40);
 		Actor_Set_Health(kActorMutant3, 10 * Query_Difficulty_Level() + 50, 10 * Query_Difficulty_Level() + 50);
 
@@ -368,6 +374,39 @@ bool AIScriptMutant3::GoalChanged(int currentGoalNumber, int newGoalNumber) {
 			Actor_Set_Friendliness_To_Other(kActorMutant3, kActorMcCoy, 40);
 		}
 
+		// code repeated also in case 599 which precedes this one
+		// redundant? 
+		// results in additional reduction in friendliness and increase of aggressiveness for the other two mutants
+		Actor_Modify_Friendliness_To_Other(kActorMutant1, kActorMcCoy, 20);
+		Actor_Modify_Friendliness_To_Other(kActorMutant2, kActorMcCoy, 15);
+		Actor_Modify_Combat_Aggressiveness(kActorMutant1, 10);
+		Actor_Modify_Combat_Aggressiveness(kActorMutant2, 10);
+		Actor_Set_Goal_Number(kActorMutant3, 403);
+		return true;
+#endif // BLADERUNNER_ORIGINAL_BUGS
+
+	case 590:
+		AI_Movement_Track_Flush(kActorMutant3);
+		AI_Movement_Track_Append(kActorMutant3, 39, 100);
+		AI_Movement_Track_Repeat(kActorMutant3);
+		break;
+
+	case 599:
+		AI_Movement_Track_Flush(kActorMutant3);
+		Actor_Change_Animation_Mode(kActorMutant3, kAnimationModeDie);
+#if BLADERUNNER_ORIGINAL_BUGS
+		Actor_Set_Intelligence(kActorMutant3, 40);
+		Actor_Set_Health(kActorMutant3, 10 * Query_Difficulty_Level() + 50, 10 * Query_Difficulty_Level() + 50);
+
+		if (Game_Flag_Query(kFlagCT04HomelessKilledByMcCoy)) {
+			Actor_Set_Combat_Aggressiveness(kActorMutant3, 80);
+			Actor_Set_Friendliness_To_Other(kActorMutant3, kActorMcCoy, 20);
+		} else {
+			Actor_Set_Combat_Aggressiveness(kActorMutant3, 50);
+			Actor_Set_Friendliness_To_Other(kActorMutant3, kActorMcCoy, 40);
+		}
+#endif // BLADERUNNER_ORIGINAL_BUGS
+		// results in additional reduction in friendliness and increase of aggressiveness for the other two mutants
 		Actor_Modify_Friendliness_To_Other(kActorMutant1, kActorMcCoy, 20);
 		Actor_Modify_Friendliness_To_Other(kActorMutant2, kActorMcCoy, 15);
 		Actor_Modify_Combat_Aggressiveness(kActorMutant1, 10);

engines/bladerunner/script/ai/officer_grayford.cpp
@@ -914,10 +914,22 @@ bool AIScriptOfficerGrayford::GoalChanged(int currentGoalNumber, int newGoalNumb
 		return true;
 
 	case kGoalOfficerGrayfordDead:
+#if BLADERUNNER_ORIGINAL_BUGS
 		_animationState = 32;
 		_animationFrame = Slice_Animation_Query_Number_Of_Frames(kModelAnimationOfficerGrayfordShotDead) - 1;
-		return true;
-
+#else
+		// The original code sets Grayford flat "dead" immediately when he is "retired"
+		// His full death animation will not play, only the last frame.
+		// This is useful for Act5, where he should lie dying down (underground railway UG05),
+		// but looks bad in Act4 if McCoy shoots a Grayford cop hunting him
+		if (Game_Flag_Query(kFlagHF07Hole)
+		    && Actor_Query_In_Set(kActorOfficerGrayford, kSetUG05)) {
+			_animationState = 32;
+			_animationFrame = Slice_Animation_Query_Number_Of_Frames(kModelAnimationOfficerGrayfordShotDead) - 1;
+			return true;
+		}
+		return false;
+#endif
 	}
 	return false;
 }
@@ -1549,6 +1561,7 @@ bool AIScriptOfficerGrayford::ChangeAnimationMode(int mode) {
 		break;
 
 	case kAnimationModeDie:
+#if BLADERUNNER_ORIGINAL_BUGS
 		switch (_animationState) {
 		case 0:
 			// fall through
@@ -1575,6 +1588,44 @@ bool AIScriptOfficerGrayford::ChangeAnimationMode(int mode) {
 			_animationFrame = 0;
 			break;
 		}
+#else
+		switch (_animationState) {
+		case 5:
+			// fall through
+		case 6:
+			// fall through
+		case 18:
+			// fall through
+		case 19:
+			// fall through
+		case 20:
+			// fall through
+		case 21:
+			// fall through
+		case 22:
+			// fall through
+		case 23:
+			// fall through
+		case 24:
+			// fall through
+		case 27:
+			// fall through
+		case 28:
+			// fall through
+		case 29:
+			// fall through
+		case 30:
+			// For all the states using combat animation (incl. holster/unholster gun)
+			_animationState = 31; // combat shot dead
+			_animationFrame = 0;
+			break;
+
+		default:
+			_animationState = 32; // shot dead
+			_animationFrame = 0;
+			break;
+		}
+#endif // BLADERUNNER_ORIGINAL_BUGS
 		break;
 
 	case 58: