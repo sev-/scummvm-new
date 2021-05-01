sev-/scummvm-new

commit 5d2fe11c9e8a8db63c83aa37c1a69884409c5f87

KYRA: Clear program queue (non-trivial type) without memset

engines/kyra/sound/drivers/adlib.cpp
@@ -451,7 +451,7 @@ void AdLibDriver::setSoundData(uint8 *data, uint32 size) {
 	// Drop all tracks that are still queued. These would point to the old
 	// sound data.
 	_programQueueStart = _programQueueEnd = 0;
-	memset(_programQueue, 0, sizeof(_programQueue));
+	_programQueue[0] = QueueEntry();
 
 	_sfxPointer = nullptr;
 

commit 80c987ed2001a1984d065950ae5e5b4b2ba5a385

KYRA: fix bug no. 12507 (Background music suddenly turns off in Legend of Kyrandia 2)
(regression from 5d2fe11c)

engines/kyra/sound/drivers/adlib.cpp
@@ -433,7 +433,8 @@ void AdLibDriver::setSoundData(uint8 *data, uint32 size) {
 	// Drop all tracks that are still queued. These would point to the old
 	// sound data.
 	_programQueueStart = _programQueueEnd = 0;
-	_programQueue[0] = QueueEntry();
+	for (int i = 0; i < ARRAYSIZE(_programQueue); ++i)
+		_programQueue[i] = QueueEntry();
 
 	_sfxPointer = nullptr;
 