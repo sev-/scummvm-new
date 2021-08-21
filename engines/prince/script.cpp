sev-/scummvm-new

commit 2fb1162f0b8f024d301375ed74fe13c5a1961b60

PRINCE:  Fix display of multiple phrases (#3298)
When examining an object, if the description has multiple phrases, only the first one was displayed on the screen (but speech audio continues).
Thant's because multiple phrases are encoded in the same string using '\0' separator, so strcpy/strncpy should be avoided.

engines/prince/script.cpp
@@ -1025,7 +1025,11 @@ void Interpreter::O_GETMOBTEXT() {
 	int32 mob = readScriptFlagValue();
 	debugInterpreter("O_GETMOBTEXT mob %d", mob);
 	_currentString = _vm->_locationNr * 100 + mob + 60001;
-	strncpy((char *)_stringBuf, _vm->_mobList[mob]._examText.c_str(), 1023);
+	// Use memcpy() instead of strncpy() because the examination text can contain
+	// different phrases separated by '\0' characters, followed by an ID.
+	// The examination text ends for a mob if the ID is 0xFF.
+	// Strings are properly extracted by the interpreter in that format.
+	memcpy((char *)_stringBuf, _vm->_mobList[mob]._examText.c_str(), 1023);
 	_string = _stringBuf;
 }
 

commit 46a4d89e937023db9b23b4b21d9c3193a91fdc8d

PRINCE: Fix out of bound access when examining objects
The bug was introduced in commit 2fb1162f0 and was causing random
crashes.

engines/prince/script.cpp
@@ -1029,7 +1029,7 @@ void Interpreter::O_GETMOBTEXT() {
 	// different phrases separated by '\0' characters, followed by an ID.
 	// The examination text ends for a mob if the ID is 0xFF.
 	// Strings are properly extracted by the interpreter in that format.
-	memcpy((char *)_stringBuf, _vm->_mobList[mob]._examText.c_str(), 1023);
+	memcpy((char *)_stringBuf, _vm->_mobList[mob]._examText.c_str(), MIN<int>(_vm->_mobList[mob]._examText.size(), 1023));
 	_string = _stringBuf;
 }
 