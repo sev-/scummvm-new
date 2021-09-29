sev-/scummvm-new

commit 4177222b7c61cf5ab16b9c4f3b8d6e83dfd2f27f

SDL: Refactor OpenGLSdlGraphics3dManager to inherit from SdlGraphicsManager

backends/graphics/sdl/sdl-graphics.cpp
@@ -41,6 +41,8 @@ SdlGraphicsManager::SdlGraphicsManager(SdlEventSource *source, SdlWindow *window
 	, _allowWindowSizeReset(false), _hintedWidth(0), _hintedHeight(0), _lastFlags(0)
 #endif
 {
+	ConfMan.registerDefault("fullscreen_res", "desktop");
+
 	SDL_GetMouseState(&_cursorX, &_cursorY);
 }
 
@@ -98,6 +100,23 @@ bool SdlGraphicsManager::setState(const State &state) {
 	} else {
 		return true;
 	}
+}
+
+Common::Rect SdlGraphicsManager::getPreferredFullscreenResolution() {
+	// Default to the desktop resolution, unless the user has set a
+	// resolution in the configuration file
+	const Common::String &fsres = ConfMan.get("fullscreen_res");
+	if (fsres != "desktop") {
+		uint newW, newH;
+		int converted = sscanf(fsres.c_str(), "%ux%u", &newW, &newH);
+		if (converted == 2) {
+			return Common::Rect(newW, newH);
+		} else {
+			warning("Could not parse 'fullscreen_res' option: expected WWWxHHH, got %s", fsres.c_str());
+		}
+	}
+
+	return _window->getDesktopResolution();
 }
 
 bool SdlGraphicsManager::defaultGraphicsModeConfig() const {

backends/events/sdl/sdl-events.cpp
@@ -27,7 +27,6 @@
 #include "backends/events/sdl/sdl-events.h"
 #include "backends/platform/sdl/sdl.h"
 #include "backends/graphics/graphics.h"
-#include "backends/graphics3d/sdl/sdl-graphics3d.h"
 #include "common/config-manager.h"
 #include "common/textconsole.h"
 #include "common/fs.h"
@@ -179,11 +178,7 @@ bool SdlEventSource::processMouseEvent(Common::Event &event, int x, int y, int r
 	event.relMouse.y = rely;
 
 	if (_graphicsManager) {
-		if (dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)) {
-			return dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->notifyMousePosition(event.mouse);
-		} else if (dynamic_cast<SdlGraphicsManager *>(_graphicsManager)) {
-			return dynamic_cast<SdlGraphicsManager *>(_graphicsManager)->notifyMousePosition(event.mouse);
-		}
+		return _graphicsManager->notifyMousePosition(event.mouse);
 	}
 
 	return true;
@@ -504,12 +499,7 @@ bool SdlEventSource::dispatchSDLEvent(SDL_Event &ev, Common::Event &event) {
 	case SDL_WINDOWEVENT:
 		// We're only interested in events from the current display window
 		if (_graphicsManager) {
-			uint32 windowID = 0;
-			if (dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)) {
-				windowID = SDL_GetWindowID(dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->getWindow()->getSDLWindow());
-			} else if (dynamic_cast<SdlGraphicsManager *>(_graphicsManager)) {
-				windowID = SDL_GetWindowID(dynamic_cast<SdlGraphicsManager *>(_graphicsManager)->getWindow()->getSDLWindow());
-			}
+			uint32 windowID = SDL_GetWindowID(_graphicsManager->getWindow()->getSDLWindow());
 			if (windowID != ev.window.windowID) {
 				return false;
 			}
@@ -518,11 +508,7 @@ bool SdlEventSource::dispatchSDLEvent(SDL_Event &ev, Common::Event &event) {
 		switch (ev.window.event) {
 		case SDL_WINDOWEVENT_EXPOSED:
 			if (_graphicsManager) {
-				if (dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)) {
-					dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->notifyVideoExpose();
-				} else if (dynamic_cast<SdlGraphicsManager *>(_graphicsManager)) {
-					dynamic_cast<SdlGraphicsManager *>(_graphicsManager)->notifyVideoExpose();
-				}
+				_graphicsManager->notifyVideoExpose();
 			}
 			return false;
 
@@ -581,11 +567,7 @@ bool SdlEventSource::dispatchSDLEvent(SDL_Event &ev, Common::Event &event) {
 #else
 	case SDL_VIDEOEXPOSE:
 		if (_graphicsManager) {
-			if (dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)) {
-				dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->notifyVideoExpose();
-			} else if (dynamic_cast<SdlGraphicsManager *>(_graphicsManager)) {
-				dynamic_cast<SdlGraphicsManager *>(_graphicsManager)->notifyVideoExpose();
-			}
+			_graphicsManager->notifyVideoExpose();
 		}
 		return false;
 
@@ -979,11 +961,8 @@ void SdlEventSource::setEngineRunning(const bool value) {
 
 bool SdlEventSource::handleResizeEvent(Common::Event &event, int w, int h) {
 	if (_graphicsManager) {
-		if (dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)) {
-			dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->notifyResize(w, h);
-		} else if (dynamic_cast<SdlGraphicsManager *>(_graphicsManager)) {
-			dynamic_cast<SdlGraphicsManager *>(_graphicsManager)->notifyResize(w, h);
-		}
+		_graphicsManager->notifyResize(w, h);
+
 		// If the screen changed, send an Common::EVENT_SCREEN_CHANGED
 		int screenID = g_system->getScreenChangeID();
 		if (screenID != _lastScreenID) {

backends/events/sdl/sdl-events.h
@@ -43,7 +43,7 @@ public:
 	SdlEventSource();
 	virtual ~SdlEventSource();
 
-	void setGraphicsManager(GraphicsManager *gMan) { _graphicsManager = gMan; }
+	void setGraphicsManager(SdlGraphicsManager *gMan) { _graphicsManager = gMan; }
 
 	/**
 	 * Gets and processes SDL events.
@@ -85,7 +85,7 @@ protected:
 	/**
 	 * The associated graphics manager.
 	 */
-	GraphicsManager *_graphicsManager;
+	SdlGraphicsManager *_graphicsManager;
 
 	/**
 	 * Search for a game controller db file and load it.

backends/events/switchsdl/switchsdl-events.cpp
@@ -28,7 +28,6 @@
 
 #include "backends/platform/sdl/switch/switch.h"
 #include "backends/events/switchsdl/switchsdl-events.h"
-#include "backends/graphics3d/sdl/sdl-graphics3d.h"
 #include "backends/timer/sdl/sdl-timer.h"
 #include "backends/platform/sdl/sdl.h"
 #include "engines/engine.h"
@@ -221,15 +220,8 @@ void SwitchEventSource::preprocessFingerMotion(SDL_Event *event) {
 	if (numFingersDown >= 1) {
 		int x = _mouseX;
 		int y = _mouseY;
-		int xMax;
-		int yMax;
-		if (dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)) {
-			xMax = dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->getOverlayWidth() - 1;
-			yMax = dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->getOverlayHeight() - 1;
-		} else {
-			xMax = dynamic_cast<WindowedGraphicsManager *>(_graphicsManager)->getWindowWidth() - 1;
-			yMax = dynamic_cast<WindowedGraphicsManager *>(_graphicsManager)->getWindowHeight() - 1;
-		}
+		int xMax = _graphicsManager->getWindowWidth() - 1;
+		int yMax = _graphicsManager->getWindowHeight() - 1;
 
 		if (port == 0 && !ConfMan.getBool("touchpad_mouse_mode")) {
 			convertTouchXYToGameXY(event->tfinger.x, event->tfinger.y, &x, &y);
@@ -361,15 +353,8 @@ void SwitchEventSource::preprocessFingerMotion(SDL_Event *event) {
 }
 
 void SwitchEventSource::convertTouchXYToGameXY(float touchX, float touchY, int *gameX, int *gameY) {
-	int screenH, screenW;
-
-	if (dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)) {
-		screenH = dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->getOverlayHeight();
-		screenW = dynamic_cast<SdlGraphics3dManager *>(_graphicsManager)->getOverlayWidth();
-	} else {
-		screenH = dynamic_cast<WindowedGraphicsManager *>(_graphicsManager)->getWindowHeight();
-		screenW = dynamic_cast<WindowedGraphicsManager *>(_graphicsManager)->getWindowWidth();
-	}
+	int screenH = _graphicsManager->getWindowHeight();
+	int screenW = _graphicsManager->getWindowWidth();
 
 	const int dispW = TOUCHSCREEN_WIDTH;
 	const int dispH = TOUCHSCREEN_HEIGHT;

backends/graphics/sdl/sdl-graphics.h
@@ -151,6 +151,9 @@ protected:
 		kActionPreviousScaleFilter
 	};
 
+	/** Obtain the user configured fullscreen resolution, or default to the desktop resolution */
+	Common::Rect getPreferredFullscreenResolution();
+
 	virtual int getGraphicsModeScale(int mode) const = 0;
 
 	bool defaultGraphicsModeConfig() const;

backends/graphics3d/graphics3d.h
@@ -1,67 +1,0 @@
-/* ResidualVM - Graphic Adventure Engine
- *
- * ResidulVM is the legal property of its developers, whose names
- * are too numerous to list here. Please refer to the COPYRIGHT
- * file distributed with this source distribution.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- */
-
-#ifndef BACKENDS_GRAPHICS3D_ABSTRACT_H
-#define BACKENDS_GRAPHICS3D_ABSTRACT_H
-
-#include "common/system.h"
-#include "common/noncopyable.h"
-#include "common/keyboard.h"
-#include "common/config-manager.h"
-
-#include "graphics/mode.h"
-#include "graphics/palette.h"
-
-#include "backends/graphics/graphics.h"
-
-/**
- * Abstract class for graphics manager. Subclasses
- * implement the real functionality.
- */
-class Graphics3dManager : public GraphicsManager {
-public:
-	// Following methods are not used by 3D graphics managers
-#ifdef USE_RGB_COLOR
-	virtual Common::List<Graphics::PixelFormat> getSupportedFormats() const {
-		Common::List<Graphics::PixelFormat> supportedFormats;
-		return supportedFormats;
-	}
-#endif
-	virtual void setPalette(const byte *colors, uint start, uint num) {}
-	virtual void grabPalette(byte *colors, uint start, uint num) const {}
-	virtual void copyRectToScreen(const void *buf, int pitch, int x, int y, int w, int h) {}
-	virtual Graphics::Surface *lockScreen() { return nullptr; }
-	virtual void unlockScreen() {}
-	virtual void fillScreen(uint32 col) {}
-	virtual void setShakePos(int shakeXOffset, int shakeYOffset) {};
-	virtual void setFocusRectangle(const Common::Rect& rect) {}
-	virtual void clearFocusRectangle() {}
-	virtual void setMouseCursor(const void *buf, uint w, uint h, int hotspotX, int hotspotY, uint32 keycolor, bool dontScale = false, const Graphics::PixelFormat *format = NULL) {}
-	virtual void setCursorPalette(const byte *colors, uint start, uint num) {}
-
-	// Stubs for windowed gfx manager calls
-	int getWindowWidth() const { return 0; }
-	int getWindowHeight() const { return 0; }
-	virtual void unlockWindowSize() {}
-};
-
-#endif

backends/graphics3d/openglsdl/openglsdl-graphics3d.cpp
@@ -45,7 +45,7 @@
 #endif
 
 OpenGLSdlGraphics3dManager::OpenGLSdlGraphics3dManager(SdlEventSource *eventSource, SdlWindow *window, bool supportsFrameBuffer)
-	: SdlGraphics3dManager(eventSource, window),
+	: SdlGraphicsManager(eventSource, window),
 #if SDL_VERSION_ATLEAST(2, 0, 0)
 	_glContext(nullptr),
 #endif
@@ -282,6 +282,8 @@ void OpenGLSdlGraphics3dManager::createOrUpdateScreen() {
 	int obtainedWidth = effectiveWidth;
 	int obtainedHeight = effectiveHeight;
 #endif
+
+	handleResize(obtainedWidth, obtainedHeight);
 
 	// Compute the rectangle where to draw the game inside the effective screen
 	_gameRect = computeGameRect(renderToFrameBuffer, _engineRequestedWidth, _engineRequestedHeight,
@@ -669,6 +671,11 @@ int16 OpenGLSdlGraphics3dManager::getOverlayHeight() const {
 
 int16 OpenGLSdlGraphics3dManager::getOverlayWidth() const {
 	return _overlayScreen->getWidth();
+}
+
+bool OpenGLSdlGraphics3dManager::showMouse(bool visible) {
+	SDL_ShowCursor(visible);
+	return true;
 }
 
 void OpenGLSdlGraphics3dManager::warpMouse(int x, int y) {

backends/graphics3d/openglsdl/openglsdl-graphics3d.h
@@ -23,7 +23,9 @@
 #ifndef BACKENDS_GRAPHICS3D_OPENGLSDL_GRAPHICS3D_H
 #define BACKENDS_GRAPHICS3D_OPENGLSDL_GRAPHICS3D_H
 
-#include "backends/graphics3d/sdl/sdl-graphics3d.h"
+#include "backends/graphics/sdl/sdl-graphics.h"
+
+#include "math/rect2d.h"
 
 #if defined(USE_OPENGL_GAME) || defined(USE_OPENGL_SHADERS) || defined(USE_GLES2)
 
@@ -39,7 +41,7 @@ namespace OpenGL {
  *
  * Used when rendering games with OpenGL
  */
-class OpenGLSdlGraphics3dManager : public SdlGraphics3dManager {
+class OpenGLSdlGraphics3dManager : public SdlGraphicsManager {
 public:
 	OpenGLSdlGraphics3dManager(SdlEventSource *eventSource, SdlWindow *window, bool supportsFrameBuffer);
 	virtual ~OpenGLSdlGraphics3dManager();
@@ -60,6 +62,10 @@ public:
 	// GraphicsManager API - Graphics mode
 #ifdef USE_RGB_COLOR
 	virtual Graphics::PixelFormat getScreenFormat() const override { return _overlayFormat; }
+	virtual Common::List<Graphics::PixelFormat> getSupportedFormats() const override {
+		Common::List<Graphics::PixelFormat> supportedFormats;
+		return supportedFormats;
+	}
 #endif
 	virtual int getScreenChangeID() const override { return _screenChangeCount; }
 	virtual void initSize(uint w, uint h, const Graphics::PixelFormat *format) override;
@@ -68,6 +74,16 @@ public:
 
 	// GraphicsManager API - Draw methods
 	virtual void updateScreen() override;
+	// Following methods are not used by 3D graphics managers
+	virtual void setPalette(const byte *colors, uint start, uint num) override {}
+	virtual void grabPalette(byte *colors, uint start, uint num) const override {}
+	virtual void copyRectToScreen(const void *buf, int pitch, int x, int y, int w, int h) override {}
+	virtual Graphics::Surface *lockScreen() override { return nullptr; }
+	virtual void unlockScreen() override {}
+	virtual void fillScreen(uint32 col) override {}
+	virtual void setShakePos(int shakeXOffset, int shakeYOffset) override {};
+	virtual void setFocusRectangle(const Common::Rect& rect) override {}
+	virtual void clearFocusRectangle() override {}
 
 	// GraphicsManager API - Overlay
 	virtual void showOverlay() override;
@@ -81,12 +97,24 @@ public:
 	virtual bool isOverlayVisible() const override { return _overlayVisible; }
 
 	// GraphicsManager API - Mouse
+	virtual bool showMouse(bool visible) override;
 	virtual void warpMouse(int x, int y) override;
+	virtual void setMouseCursor(const void *buf, uint w, uint h, int hotspotX, int hotspotY, uint32 keycolor, bool dontScale = false, const Graphics::PixelFormat *format = NULL) override {}
+	virtual void setCursorPalette(const byte *colors, uint start, uint num) override {}
 
 	// SdlGraphicsManager API
-	virtual void transformMouseCoordinates(Common::Point &point) override;
+	virtual void notifyVideoExpose() override {};
+	virtual void notifyResize(const int width, const int height) override;
 
-	void notifyResize(const int width, const int height) override;
+	virtual bool gameNeedsAspectRatioCorrection() const override { return false; }
+	virtual int getGraphicsModeScale(int mode) const override { return 1; }
+
+	void transformMouseCoordinates(Common::Point &point);
+	virtual bool notifyMousePosition(Common::Point &mouse) override {
+		transformMouseCoordinates(mouse);
+
+		return true;
+	}
 
 protected:
 #if SDL_VERSION_ATLEAST(2, 0, 0)

commit ea94ca2bd48617bc9c585914ae9a1e063934e24c

SDL: Fix full screen toggling for 3D games
The previous SdlGraphics3dManager::toggleFullScreen() was not using
beginGFXTransaction / endGFXTransaction in toggleFullScreen().
Restore this logic for 3D games, as using transactions in such games
results in a black screen for 3D games when switching to full screen

A regression from 4177222b7

backends/graphics/sdl/sdl-graphics.cpp
@@ -401,9 +401,13 @@ void SdlGraphicsManager::toggleFullScreen() {
 		return;
 	}
 
-	beginGFXTransaction();
+	bool is3D = g_system->hasFeature(OSystem::kFeatureOpenGLForGame);
+
+	if (!is3D)
+		beginGFXTransaction();
 	setFeatureState(OSystem::kFeatureFullscreenMode, !getFeatureState(OSystem::kFeatureFullscreenMode));
-	endGFXTransaction();
+	if (!is3D)
+		endGFXTransaction();
 #ifdef USE_OSD
 	if (getFeatureState(OSystem::kFeatureFullscreenMode))
 		displayMessageOnOSD(_("Fullscreen mode"));