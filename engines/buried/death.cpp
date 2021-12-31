sev-/scummvm-new

commit dda578ec0d87c4d4b14e5e6f1628cd8fdca3b523

BURIED: Pass big parameters only by reference

engines/buried/death.cpp
@@ -62,7 +62,7 @@ enum {
 	if (_globalFlags.evcapBaseID[i] == flag) \
 		supportingEvidence++
 
-DeathWindow::DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags globalFlags, Common::Array<int> itemArray)
+DeathWindow::DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags &globalFlags, Common::Array<int> itemArray)
 		: Window(vm, parent), _deathSceneIndex(deathSceneIndex), _globalFlags(globalFlags), _itemArray(itemArray) {
 	_curButton = 0;
 	_deathFrameIndex = -1;

engines/buried/death.h
@@ -40,7 +40,7 @@ class AVIFrames;
 
 class DeathWindow : public Window {
 public:
-	DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags globalFlags, Common::Array<int> itemArray);
+	DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags &globalFlags, Common::Array<int> itemArray);
 	~DeathWindow();
 
 	void onPaint();

engines/buried/frame_window.cpp
@@ -263,7 +263,7 @@ bool FrameWindow::startNewGame(bool walkthrough, bool introMovie) {
 	return true;
 }
 
-bool FrameWindow::showDeathScene(int deathSceneIndex, GlobalFlags globalFlags, Common::Array<int> itemArray) {
+bool FrameWindow::showDeathScene(int deathSceneIndex, GlobalFlags &globalFlags, Common::Array<int> itemArray) {
 	_gameInProgress = false;
 	_atMainMenu = false;
 
@@ -277,7 +277,7 @@ bool FrameWindow::showDeathScene(int deathSceneIndex, GlobalFlags globalFlags, C
 	return true;
 }
 
-bool FrameWindow::showCompletionScene(GlobalFlags globalFlags) {
+bool FrameWindow::showCompletionScene(GlobalFlags &globalFlags) {
 	_gameInProgress = false;
 	_atMainMenu = false;
 
@@ -348,7 +348,7 @@ void FrameWindow::onKeyDown(const Common::KeyState &key, uint flags) {
 
 	if (key.keycode == Common::KEYCODE_ESCAPE) {
 		if (_gameInProgress || !_atMainMenu) {
-			// Ask if the player wants to return 
+			// Ask if the player wants to return
 			if (_vm->runQuitDialog())
 				showMainMenu();
 		} else {
@@ -383,7 +383,7 @@ void FrameWindow::setTransitionSpeed(int newSpeed) {
 	ConfMan.setInt(_vm->isDemo() ? "TransitionSpeed" : _vm->getString(IDS_INI_KEY_TRANS_SPEED), newSpeed);
 }
 
-void FrameWindow::loadFromState(const Location &location, GlobalFlags flags, Common::Array<int> inventoryItems) {
+void FrameWindow::loadFromState(const Location &location, GlobalFlags &flags, Common::Array<int> inventoryItems) {
 	if (!_gameInProgress) {
 		// Make the game in progress
 		_atMainMenu = false;

engines/buried/complete.cpp
@@ -53,7 +53,7 @@ namespace Buried {
 	if (_globalFlags.evcapBaseID[i] == flag) \
 		supportingEvidence++
 
-CompletionWindow::CompletionWindow(BuriedEngine *vm, Window *parent, GlobalFlags globalFlags) : Window(vm, parent), _globalFlags(globalFlags) {
+CompletionWindow::CompletionWindow(BuriedEngine *vm, Window *parent, GlobalFlags &globalFlags) : Window(vm, parent), _globalFlags(globalFlags) {
 	_vm->_sound->setAmbientSound();
 
 	_status = 0;
@@ -182,7 +182,7 @@ CompletionWindow::CompletionWindow(BuriedEngine *vm, Window *parent, GlobalFlags
 CompletionWindow::~CompletionWindow() {
 	delete _gageVideo;
 	killTimer(_timer);
-	
+
 	delete _textFontA;
 	delete _textFontB;
 

engines/buried/complete.h
@@ -40,7 +40,7 @@ class VideoWindow;
 
 class CompletionWindow : public Window {
 public:
-	CompletionWindow(BuriedEngine *vm, Window *parent, GlobalFlags globalFlags);
+	CompletionWindow(BuriedEngine *vm, Window *parent, GlobalFlags &globalFlags);
 	~CompletionWindow();
 
 	void onPaint();

engines/buried/frame_window.h
@@ -45,8 +45,8 @@ public:
 	bool showClosingScreen();
 	bool showFeaturesScreen();
 	bool startNewGame(bool walkthrough = false, bool introMovie = false);
-	bool showDeathScene(int deathSceneIndex, GlobalFlags globalFlags, Common::Array<int> itemArray);
-	bool showCompletionScene(GlobalFlags globalFlags);
+	bool showDeathScene(int deathSceneIndex, GlobalFlags &globalFlags, Common::Array<int> itemArray);
+	bool showCompletionScene(GlobalFlags &globalFlags);
 	bool showCredits();
 	bool showOverview();
 	bool notifyUserOfFrameCycling();
@@ -69,7 +69,7 @@ public:
 
 	bool isGameInProgress() const { return _gameInProgress; }
 	Window *getMainChildWindow() const { return _mainChildWindow; }
-	void loadFromState(const Location &location, GlobalFlags flags, Common::Array<int> inventoryItems);
+	void loadFromState(const Location &location, GlobalFlags &flags, Common::Array<int> inventoryItems);
 
 private:
 	Window *_mainChildWindow;

commit 43af8bf605cc06dce6149d7436b6173cd54ed4be

BURIED: Fix global flag corruption in death screens
A regression from dda578ec0d87c4d4b14e5e6f1628cd8fdca3b523

engines/buried/death.cpp
@@ -192,7 +192,7 @@ enum {
 	BUTTON_MAIN_MENU = 3
 };
 
-DeathWindow::DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags &globalFlags, Common::Array<int> itemArray)
+DeathWindow::DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags globalFlags, Common::Array<int> itemArray)
 		: Window(vm, parent), _deathSceneIndex(deathSceneIndex), _globalFlags(globalFlags), _itemArray(itemArray) {
 	_curButton = 0;
 	_deathFrameIndex = -1;

engines/buried/death.h
@@ -52,7 +52,7 @@ private:
 
 class DeathWindow : public Window {
 public:
-	DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags &globalFlags, Common::Array<int> itemArray);
+	DeathWindow(BuriedEngine *vm, Window *parent, int deathSceneIndex, GlobalFlags globalFlags, Common::Array<int> itemArray);
 	~DeathWindow();
 
 	void onPaint();

engines/buried/frame_window.cpp
@@ -268,8 +268,13 @@ bool FrameWindow::showDeathScene(int deathSceneIndex, GlobalFlags &globalFlags, 
 
 	_vm->removeMouseMessages(this);
 
+	// Pass globalFlags by value to DeathWindowhere, as they will be destroyed
+	// together with _mainChildWindow (a GameUIWindow, which contains the scene
+	// window, which holds the instance of the global flags)
+	DeathWindow *deathWindow = new DeathWindow(_vm, this, deathSceneIndex, globalFlags, itemArray);
+
 	delete _mainChildWindow;
-	_mainChildWindow = new DeathWindow(_vm, this, deathSceneIndex, globalFlags, itemArray);
+	_mainChildWindow = deathWindow;
 	_mainChildWindow->showWindow(kWindowShow);
 	_mainChildWindow->invalidateWindow(false);
 