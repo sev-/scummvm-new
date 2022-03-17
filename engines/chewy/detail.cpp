sev-/scummvm-new

commit 127c722cb722452e78d3d0713cb0e8bb631d7ee2

CHEWY: rename a bunch of 'Anz' variables

engines/chewy/detail.cpp
@@ -28,21 +28,21 @@ namespace Chewy {
 
 bool RoomInfo::load(Common::SeekableReadStream *src) {
 	_roomNr = src->readByte();
-	BildNr = src->readByte();
-	AutoMovAnz = src->readByte();
-	TafLoad = src->readByte();
-	src->read(TafName, 14);
+	_imageNr = src->readByte();
+	_autoMovNr = src->readByte();
+	_tafLoad = src->readByte();
+	src->read(_tafName, 14);
 	_zoomFactor = src->readByte();
-	Dummy = src->readByte();
+	_dummy = src->readByte();
 
 	return true;
 }
 
 bool RoomAutoMov::load(Common::SeekableReadStream *src) {
-	X = src->readSint16LE();
-	Y = src->readSint16LE();
-	SprNr = src->readByte();
-	dummy = src->readByte();
+	_x = src->readSint16LE();
+	_y = src->readSint16LE();
+	_sprNr = src->readByte();
+	_dummy = src->readByte();
 
 	return true;
 }
@@ -104,8 +104,8 @@ bool StaticDetailInfo::load(Common::SeekableReadStream *src) {
 bool RoomDetailInfo::load(Common::SeekableReadStream *src) {
 	int i;
 
-	StaticDetailAnz = src->readSint16LE();
-	AniDetailAnz = src->readSint16LE();
+	_staticDetailNr = src->readSint16LE();
+	_aniDetailNr = src->readSint16LE();
 	src->skip(4);	// dptr
 	for (i = 0; i < MAXDETAILS; ++i)
 		Ainfo[i].load(src);
@@ -127,7 +127,7 @@ bool RoomDetailInfo::load(Common::SeekableReadStream *src) {
 
 bool RdiDataHeader::load(Common::SeekableReadStream *src) {
 	src->read(_id, 4);
-	Anz = src->readSint16LE();
+	_nr = src->readSint16LE();
 
 	return true;
 }
@@ -248,11 +248,11 @@ void Detail::load_taf_tbl(TafInfo *fti) {
 TafInfo *Detail::init_taf_tbl(const char *fname_) {
 	TafInfo *Tt = nullptr;
 	SpriteResource *res = new SpriteResource(_tafName);
-	int16 anz = res->getChunkCount();
-	byte *tmp = (byte *)MALLOC((int32)anz * sizeof(byte *) + sizeof(TafInfo));
+	int16 nr = res->getChunkCount();
+	byte *tmp = (byte *)MALLOC((int32)nr * sizeof(byte *) + sizeof(TafInfo));
 
 	Tt = (TafInfo *)tmp;
-	Tt->_count = anz;
+	Tt->_count = nr;
 	Tt->_correction = (int16 *)MALLOC((int32)Tt->_count * 2 * sizeof(int16));
 	Tt->_image = (byte **)(tmp + sizeof(TafInfo));
 	memcpy(Tt->_correction, (byte *)res->getSpriteCorrectionsTable(), Tt->_count * 2 * sizeof(int16));
@@ -274,10 +274,10 @@ void Detail::del_taf_tbl(TafInfo *Tt) {
 	free((char *) Tt);
 }
 
-void Detail::del_taf_tbl(int16 start, int16 anz, TafInfo *Tt) {
+void Detail::del_taf_tbl(int16 start, int16 nr, TafInfo *Tt) {
 	if (!Tt)
 		Tt = _rdi.dptr;
-	for (int16 i = start; i < start + anz && i < Tt->_count; i++) {
+	for (int16 i = start; i < start + nr && i < Tt->_count; i++) {
 		free(Tt->_image[i]);
 		Tt->_image[i] = nullptr;
 	}
@@ -570,7 +570,7 @@ SprInfo Detail::plot_detail_sprite(int16 scrx, int16 scry, int16 det_nr, int16 s
 		spr_nr = adiptr->start_ani;
 	if (spr_nr > adiptr->end_ani)
 		spr_nr = adiptr->end_ani - 1;
-	int16 *Cxy = _rdi.dptr->_correction + (spr_nr << 1);
+	int16 *Cxy = &_rdi.dptr->_correction[spr_nr];
 	int16 *Xy = (int16 *)_rdi.dptr->_image[spr_nr];
 	_sprInfo._image = _rdi.dptr->_image[spr_nr];
 	_sprInfo._x = adiptr->x + Cxy[0] - scrx;

engines/chewy/atds.cpp
@@ -593,7 +593,7 @@ void Atdsys::print_ats(int16 x, int16 y, int16 scrx, int16 scry) {
 						_mousePush = true;
 						_atsv.DelayCount = 0;
 						KbdInfo *kbInfo = _G(in)->getPointer()->kbinfo;
-						kbInfo->scan_code = Common::KEYCODE_INVALID;
+						kbInfo->_scanCode = Common::KEYCODE_INVALID;
 						kbInfo->_keyCode = '\0';
 					}
 				}
@@ -940,7 +940,7 @@ void Atdsys::print_aad(int16 scrx, int16 scry) {
 						_mousePush = true;
 						_aadv._delayCount = 0;
 						KbdInfo *kbInfo = _G(in)->getPointer()->kbinfo;
-						kbInfo->scan_code = Common::KEYCODE_INVALID;
+						kbInfo->_scanCode = Common::KEYCODE_INVALID;
 						kbInfo->_keyCode = '\0';
 					}
 				}

engines/chewy/data.cpp
@@ -43,11 +43,11 @@ uint16 Data::select_pool_item(Common::Stream *stream, uint16 nr) {
 		if (!ph.load(rs))
 			error("select_pool_item error");
 
-		if (!strncmp(ph.id, "NGS", 3)) {
-			if (nr >= ph.PoolAnz)
-				nr = ph.PoolAnz - 1;
+		if (!strncmp(ph._id, "NGS", 3)) {
+			if (nr >= ph._poolNr)
+				nr = ph._poolNr - 1;
 
-			rs->seek(-(int)((ph.PoolAnz - nr) * sizeof(uint32)), SEEK_END);
+			rs->seek(-(int)((ph._poolNr - nr) * sizeof(uint32)), SEEK_END);
 			uint32 tmp1 = rs->readUint32LE();
 			rs->seek(tmp1, SEEK_SET);
 		}
@@ -76,7 +76,7 @@ uint32 Data::load_tmf(Common::Stream *handle, TmfHeader *song) {
 
 		size = ch.size + sizeof(TmfHeader);
 		byte *speicher = (byte *)song + sizeof(TmfHeader);
-		speicher += ((uint32)song->pattern_anz) * 1024l;
+		speicher += ((uint32)song->_patternNr) * 1024l;
 		for (int16 i = 0; i < 31; ++i) {
 			if (song->instrument[i].laenge) {
 				song->ipos[i] = speicher;
@@ -99,11 +99,11 @@ uint32 Data::get_poolsize(const char *fname, int16 chunk_start, int16 chunk_anz)
 	if (!Nph.load(&f))
 		error("get_poolsize error");
 
-	if (!strncmp(Nph.id, "NGS", 3)) {
+	if (!strncmp(Nph._id, "NGS", 3)) {
 		select_pool_item(&f, chunk_start);
 		f.seek(-ChunkHead::SIZE(), SEEK_CUR);
 
-		for (int16 i = chunk_start; (i < Nph.PoolAnz) && i < (chunk_start + chunk_anz); i++) {
+		for (int16 i = chunk_start; (i < Nph._poolNr) && i < (chunk_start + chunk_anz); i++) {
 			ChunkHead ch;
 			if (!ch.load(&f))
 				error("get_poolsize error");

engines/chewy/detail.h
@@ -35,12 +35,12 @@ namespace Chewy {
 
 struct RoomInfo {
 	uint8 _roomNr;
-	uint8 BildNr;
-	uint8 AutoMovAnz;
-	uint8 TafLoad;
-	char TafName[14];
+	uint8 _imageNr;
+	uint8 _autoMovNr;
+	uint8 _tafLoad;
+	char _tafName[14];
 	uint8 _zoomFactor;
-	uint8 Dummy;
+	uint8 _dummy;
 
 	bool load(Common::SeekableReadStream *src);
 	static constexpr int SIZE() {
@@ -49,10 +49,10 @@ struct RoomInfo {
 };
 
 struct RoomAutoMov {
-	int16 X;
-	int16 Y;
-	uint8 SprNr;
-	uint8 dummy;
+	int16 _x;
+	int16 _y;
+	uint8 _sprNr;
+	uint8 _dummy;
 
 	bool load(Common::SeekableReadStream *src);
 	static constexpr int SIZE() {
@@ -60,10 +60,8 @@ struct RoomAutoMov {
 	}
 };
 
-#define MAXTAF 10
 #define MAXDETAILS 32
 #define MAX_M_ITEMS 50
-#define MAX_RDI_ROOM 100
 
 struct SprInfo {
 	byte *_image = nullptr;
@@ -130,8 +128,8 @@ struct StaticDetailInfo {
 };
 
 struct RoomDetailInfo {
-	int16 StaticDetailAnz = 0;
-	int16 AniDetailAnz = 0;
+	int16 _staticDetailNr = 0;
+	int16 _aniDetailNr = 0;
 	TafInfo *dptr = nullptr;
 	AniDetailInfo Ainfo[MAXDETAILS];
 	StaticDetailInfo Sinfo[MAXDETAILS];
@@ -158,7 +156,7 @@ struct RoomDetailInfo {
 
 struct RdiDataHeader {
 	char _id[4];
-	int16 Anz;
+	int16 _nr;
 
 	bool load(Common::SeekableReadStream *src);
 };
@@ -233,7 +231,7 @@ public:
 
 	void del_taf_tbl(TafInfo *Tt);
 
-	void del_taf_tbl(int16 start, int16 anz, TafInfo *Tt);
+	void del_taf_tbl(int16 start, int16 nr, TafInfo *Tt);
 
 	void load_taf_seq(int16 sprNr, int16 sprCount, TafInfo *Tt);
 	void load_taf_tbl(TafInfo *fti);

engines/chewy/dialogs/cinema.cpp
@@ -61,7 +61,7 @@ void Cinema::execute() {
 	_G(room)->load_tgp(4, &_G(room_blk), 1, 0, GBOOK);
 	showCur();
 	EVENTS_CLEAR;
-	_G(kbinfo).scan_code = 0;
+	_G(kbinfo)._scanCode = 0;
 
 	for (bool endLoop = false; !endLoop;) {
 		_G(out)->setPointer(_G(workptr));
@@ -88,14 +88,14 @@ void Cinema::execute() {
 			flag = true;
 			switch (_G(in)->mouseVector(g_events->_mousePos.x, g_events->_mousePos.y, CINEMA_TBL, 3)) {
 			case 0:
-				_G(kbinfo).scan_code = Common::KEYCODE_UP;
+				_G(kbinfo)._scanCode = Common::KEYCODE_UP;
 				if (!endLoop)
 					endLoop = true;
 
 				break;
 
 			case 1:
-				_G(kbinfo).scan_code = Common::KEYCODE_DOWN;
+				_G(kbinfo)._scanCode = Common::KEYCODE_DOWN;
 				if (!endLoop)
 					endLoop = true;
 
@@ -106,7 +106,7 @@ void Cinema::execute() {
 				int selIndex = (g_events->_mousePos.y - 68) / 10 + topIndex;
 				if (selIndex < (int)cutscenes.size())
 					selected = selIndex;
-				_G(kbinfo).scan_code = Common::KEYCODE_RETURN;
+				_G(kbinfo)._scanCode = Common::KEYCODE_RETURN;
 				break;
 			}
 
@@ -114,7 +114,7 @@ void Cinema::execute() {
 				break;
 			}
 		} else if (_G(minfo)._button == 2 && !flag) {
-			_G(kbinfo).scan_code = Common::KEYCODE_ESCAPE;
+			_G(kbinfo)._scanCode = Common::KEYCODE_ESCAPE;
 			flag = true;
 		} else if (_G(minfo)._button != 1) {
 			flag = false;
@@ -125,10 +125,10 @@ void Cinema::execute() {
 				flag = false;
 		}
 
-		switch (_G(kbinfo).scan_code) {
+		switch (_G(kbinfo)._scanCode) {
 		case Common::KEYCODE_ESCAPE:
 			endLoop = true;
-			_G(kbinfo).scan_code = 0;
+			_G(kbinfo)._scanCode = 0;
 			break;
 
 		case Common::KEYCODE_UP:
@@ -138,7 +138,7 @@ void Cinema::execute() {
 			} else if (topIndex > 0) {
 				--topIndex;
 			}
-			_G(kbinfo).scan_code = 0;
+			_G(kbinfo)._scanCode = 0;
 			break;
 
 		case Common::KEYCODE_DOWN:
@@ -152,7 +152,7 @@ void Cinema::execute() {
 				if ((topIndex + newIndex) < (int)cutscenes.size())
 					++selected;
 			}
-			_G(kbinfo).scan_code = 0;
+			_G(kbinfo)._scanCode = 0;
 			break;
 		}
 
@@ -175,7 +175,7 @@ void Cinema::execute() {
 		}
 
 		// The below are hacks to get the dialog to work in ScummVM
-		_G(kbinfo).scan_code = 0;
+		_G(kbinfo)._scanCode = 0;
 		_G(minfo)._button = 0;
 		txt_anz = 0;
 

engines/chewy/dialogs/inventory.cpp
@@ -425,7 +425,7 @@ int16 Inventory::look(int16 invent_nr, int16 mode, int16 ats_nr) {
 		if (_G(minfo)._button) {
 			if (_G(minfo)._button == 2) {
 				if (!mouseFl)
-					_G(kbinfo).scan_code = Common::KEYCODE_ESCAPE;
+					_G(kbinfo)._scanCode = Common::KEYCODE_ESCAPE;
 			} else if (_G(minfo)._button == 1) {
 				if (!mouseFl) {
 					switch (rect) {
@@ -441,12 +441,12 @@ int16 Inventory::look(int16 invent_nr, int16 mode, int16 ats_nr) {
 
 					case 3:
 					case 6:
-						_G(kbinfo).scan_code = Common::KEYCODE_UP;
+						_G(kbinfo)._scanCode = Common::KEYCODE_UP;
 						break;
 
 					case 4:
 					case 7:
-						_G(kbinfo).scan_code = Common::KEYCODE_DOWN;
+						_G(kbinfo)._scanCode = Common::KEYCODE_DOWN;
 						break;
 
 					case 5:
@@ -465,7 +465,7 @@ int16 Inventory::look(int16 invent_nr, int16 mode, int16 ats_nr) {
 			mouseFl = false;
 		}
 
-		switch (_G(kbinfo).scan_code) {
+		switch (_G(kbinfo)._scanCode) {
 		case Common::KEYCODE_F1:
 			_G(in)->_hotkey = Common::KEYCODE_F1;
 			break;
@@ -492,7 +492,7 @@ int16 Inventory::look(int16 invent_nr, int16 mode, int16 ats_nr) {
 			break;
 		}
 
-		_G(kbinfo).scan_code = Common::KEYCODE_INVALID;
+		_G(kbinfo)._scanCode = Common::KEYCODE_INVALID;
 		setupScreen(NO_SETUP);
 		plot_menu();
 		_G(fontMgr)->setFont(_G(font8));

engines/chewy/dialogs/main_menu.cpp
@@ -201,7 +201,7 @@ void MainMenu::startGame() {
 	_G(room)->loadRoom(&_G(room_blk), 0, &_G(spieler));
 
 	_G(spieler_vector)[P_CHEWY].Phase = 6;
-	_G(spieler_vector)[P_CHEWY].PhAnz = _G(chewy_ph_anz)[6];
+	_G(spieler_vector)[P_CHEWY].PhAnz = _G(chewy_ph_nr)[6];
 	setPersonPos(160, 80, P_CHEWY, P_RIGHT);
 	_G(fx_blend) = BLEND3;
 	_G(spieler)._personHide[P_CHEWY] = false;
@@ -243,7 +243,7 @@ void MainMenu::playGame() {
 	_G(cur_display) = true;
 	_G(tmp_menu_item) = 0;
 	_G(mouseLeftClick) = false;
-	_G(kbinfo).scan_code = Common::KEYCODE_INVALID;
+	_G(kbinfo)._scanCode = Common::KEYCODE_INVALID;
 
 	_G(flags).mainMouseFlag = false;
 	_G(flags).MainInput = true;

engines/chewy/events.cpp
@@ -141,9 +141,9 @@ void EventsManager::handleKbdEvent(const Common::Event &event) {
 	if (_kbInfo) {
 		if (event.type == Common::EVENT_KEYDOWN) {
 			_kbInfo->_keyCode = event.kbd.ascii;
-			_kbInfo->scan_code = event.kbd.keycode;
+			_kbInfo->_scanCode = event.kbd.keycode;
 			if (event.kbd.flags & Common::KBD_ALT)
-				_kbInfo->scan_code |= ALT;
+				_kbInfo->_scanCode |= ALT;
 		} else if (event.type == Common::EVENT_KEYUP) {
 			if (event.kbd.ascii == _kbInfo->_keyCode)
 				_kbInfo->_keyCode = '\0';
@@ -163,7 +163,7 @@ void EventsManager::delay(size_t time) {
 
 void EventsManager::clearEvents() {
 	if (_kbInfo) {
-		_kbInfo->scan_code = Common::KEYCODE_INVALID;
+		_kbInfo->_scanCode = Common::KEYCODE_INVALID;
 		_kbInfo->_keyCode = '\0';
 	}
 
@@ -249,7 +249,7 @@ char getch() {
 	if (g_events->_kbInfo && g_events->_kbInfo->_keyCode) {
 		char c = g_events->_kbInfo->_keyCode;
 		g_events->_kbInfo->_keyCode = 0;
-		g_events->_kbInfo->scan_code = 0;
+		g_events->_kbInfo->_scanCode = 0;
 
 		return c;
 	}

commit 8758d7b0721f6a3f15c80887407a8a1f7a174ed6

CHEWY: Fix animation regression introduced in 127c722cb722452e78d3d0713cb0e8bb631d7ee2

engines/chewy/detail.cpp
@@ -568,7 +568,7 @@ SprInfo Detail::plot_detail_sprite(int16 scrx, int16 scry, int16 det_nr, int16 s
 		spr_nr = adiptr->start_ani;
 	if (spr_nr > adiptr->end_ani)
 		spr_nr = adiptr->end_ani - 1;
-	int16 *Cxy = &_rdi.dptr->_correction[spr_nr];
+	int16 *Cxy = &_rdi.dptr->_correction[spr_nr << 1];
 	int16 *Xy = (int16 *)_rdi.dptr->_image[spr_nr];
 	_sprInfo._image = _rdi.dptr->_image[spr_nr];
 	_sprInfo._x = adiptr->x + Cxy[0] - scrx;