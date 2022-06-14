sev-/scummvm-new

commit d1018fe47d0cc31bba55900ee4f819b8f7848f8e

TWINE: fixed invalid processActor reset
was the cause of this blocker bug: https://bugs.scummvm.org/ticket/13177

the _processCollision variable is overwritten in getBrickShape and the 'backup' is lost

engines/twine/scene/animations.cpp
@@ -668,7 +668,7 @@ void Animations::processActorAnimations(int32 actorIdx) {
 
 		_engine->_collision->_causeActorDamage = 0;
 
-		_engine->_collision->_processCollision = processActor;
+		const IVec3 processActorSave = processActor;
 
 		if (IS_HERO(actorIdx) && !actor->_staticFlags.bComputeLowCollision) {
 			// check hero collisions with bricks
@@ -683,6 +683,7 @@ void Animations::processActorAnimations(int32 actorIdx) {
 			_engine->_collision->checkActorCollisionWithBricks(actor->_boudingBox.maxs.x, actor->_boudingBox.mins.y, actor->_boudingBox.maxs.z, 4);
 			_engine->_collision->checkActorCollisionWithBricks(actor->_boudingBox.mins.x, actor->_boudingBox.mins.y, actor->_boudingBox.maxs.z, 8);
 		}
+		processActor = processActorSave;
 
 		// process wall hit while running
 		if (_engine->_collision->_causeActorDamage && !actor->_dynamicFlags.bIsFalling && IS_HERO(_currentlyProcessedActorIdx) && _engine->_actor->_heroBehaviour == HeroBehaviourType::kAthletic && actor->_anim == AnimationTypes::kForward) {

commit 7d2363476bbc9c96c6e5b0f62624869b9c577135

TWINE: fixed regression introduced in d1018fe47d
the modification of the processActor values from checkActorCollisionWithBricks and checkHeroCollisionWithBricks were
lost. The bug is that you could move crates into walls in the 'sokoban'-scene (scene 35).

See https://bugs.scummvm.org/ticket/13177

Once we remove the processActorSave hack, this would re-introduced the tank bug in scene 63 - that's why the fix
is non functional yet.

engines/twine/scene/animations.cpp
@@ -671,6 +671,7 @@ void Animations::doAnim(int32 actorIdx) {
 		collision->_causeActorDamage = 0;
 
 		const IVec3 processActorSave = processActor;
+		collision->setCollisionPos(processActor);
 
 		if (IS_HERO(actorIdx) && !actor->_staticFlags.bComputeLowCollision) {
 			// check hero collisions with bricks

engines/twine/scene/collision.cpp
@@ -293,6 +293,10 @@ int32 Collision::checkObjCol(int32 actorIdx) {
 	return actor->_collision;
 }
 
+void Collision::setCollisionPos(const IVec3 &pos) {
+	_processCollision = pos;
+}
+
 void Collision::doCornerReajustTwinkel(ActorStruct *actor, int32 x, int32 y, int32 z, int32 damageMask) {
 	IVec3 &processActor = actor->_processActor;
 	IVec3 &previousActor = actor->_previousActor;

engines/twine/scene/collision.h
@@ -68,6 +68,7 @@ public:
 	 */
 	int32 checkObjCol(int32 actorIdx);
 
+	void setCollisionPos(const IVec3 &pos);
 	/**
 	 * Check Hero collision with bricks
 	 * @param x Hero X coordinate