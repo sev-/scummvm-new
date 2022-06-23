sev-/scummvm-new

commit 8676baa4215b8bee8633f28e1b27eee16e00d1d9

DIRECTOR: Fix Palette loading wrong Palette index

engines/director/frame.cpp
@@ -167,9 +167,9 @@ void Frame::readChannels(Common::ReadStreamEndian *stream, uint16 version) {
 
 			stream->read(unk, 6);
 		} else {
-			stream->read(unk, 4);
+			stream->read(unk, 3);
 
-			_palette.paletteId = stream->readByte();
+			_palette.paletteId = stream->readUint16();
 			_palette.firstColor = stream->readByte(); // for cycles. note: these start at 0x80 (for pal entry 0)!
 			_palette.lastColor = stream->readByte();
 			_palette.flags = stream->readByte();

engines/director/score.cpp
@@ -256,7 +256,7 @@ void Score::startPlay() {
 	_playState = kPlayStarted;
 	_nextFrameTime = 0;
 
-	_lastPalette = _movie->getCast()->_defaultPalette;
+	_lastPalette = _frames[_currentFrame]->_palette.paletteId;
 	_vm->setPalette(resolvePaletteId(_lastPalette));
 
 	if (_frames.size() <= 1) {	// We added one empty sprite

commit 7583358f1ea56b7fee3b3cf5a5f71655c1a22646

DIRECTOR: fix palette regression
8676baa4215b8bee8633f28e1b27eee16e00d1d9 introduced a fix for
fetching the palette. However, it misinterpreted the number as an
unsigned int instead of a signed int, leading to incorrect palettes
on certain other games.

engines/director/frame.cpp
@@ -169,7 +169,7 @@ void Frame::readChannels(Common::ReadStreamEndian *stream, uint16 version) {
 		} else {
 			stream->read(unk, 3);
 
-			_palette.paletteId = stream->readUint16();
+			_palette.paletteId = stream->readSint16();
 			_palette.firstColor = stream->readByte(); // for cycles. note: these start at 0x80 (for pal entry 0)!
 			_palette.lastColor = stream->readByte();
 			_palette.flags = stream->readByte();