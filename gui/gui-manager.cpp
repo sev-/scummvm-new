sev-/scummvm-new

commit c55b1167874a26f0c979871fd304223f6bb96b6d

GUI: Change the mechanism to update the grid launcher after upding the icons
The icons download dialog was triggering the grid launcher update after
icons had been downloaded. But that means no update was done if the
dialog had been closed during the download.

Now the GUIManager triggers the update. This fixes the missing update
when hiding the download dialog while downloading an icon set.

gui/gui-manager.cpp
@@ -59,7 +59,7 @@ enum {
 };
 
 // Constructor
-GuiManager::GuiManager() : _redrawStatus(kRedrawDisabled), _stateIsSaved(false),
+GuiManager::GuiManager() : CommandSender(nullptr), _redrawStatus(kRedrawDisabled), _stateIsSaved(false),
 	_cursorAnimateCounter(0), _cursorAnimateTimer(0) {
 	_theme = nullptr;
 	_useStdCursor = false;
@@ -72,6 +72,8 @@ GuiManager::GuiManager() : _redrawStatus(kRedrawDisabled), _stateIsSaved(false),
 	_launched = false;
 
 	_useRTL = false;
+
+	_iconsSetChanged = false;
 
 	_topDialogLeftPadding = 0;
 	_topDialogRightPadding = 0;
@@ -170,6 +172,7 @@ void GuiManager::initIconsSet() {
 	}
 
 	_iconsSet.add(fname, dat);
+	_iconsSetChanged = true;
 
 	debug(2, "GUI: Loaded icon file: %s", fname);
 }
@@ -504,6 +507,18 @@ void GuiManager::runLoop() {
 			}
 
 			processEvent(event, activeDialog);
+		}
+
+		// If iconsSet was modified, notify dialogs so that they can be  updated if needed
+		_iconsMutex.lock();
+		bool iconsChanged = _iconsSetChanged;
+		_iconsSetChanged = false;
+		_iconsMutex.unlock();
+		if (iconsChanged) {
+			for (DialogStack::size_type i = 0; i < _dialogStack.size(); ++i) {
+				setTarget(_dialogStack[i]);
+				sendCommand(kIconsSetLoadedCmd, 0);
+			}
 		}
 
 		// Delete GuiObject that have been added to the trash for a delayed deletion

gui/downloadiconsdialog.cpp
@@ -304,7 +304,6 @@ void DownloadIconsDialog::handleCommand(CommandSender *sender, uint32 cmd, uint3
 		break;
 	case kDownloadEndedCmd:
 		setState(kDownloadComplete);
-		setResult(1); // Need tell the options to refresh launcher
 		break;
 	case kListDownloadFinishedCmd:
 		setState(kDownloadStateListDownloaded);

gui/gui-manager.h
@@ -45,6 +45,10 @@ namespace Common {
 
 namespace GUI {
 
+enum {
+	kIconsSetLoadedCmd  = 'icns'
+};
+
 class Dialog;
 class ThemeEval;
 class GuiObject;
@@ -65,7 +69,7 @@ typedef Common::FixedStack<Dialog *> DialogStack;
 /**
  * GUI manager singleton.
  */
-class GuiManager : public Common::Singleton<GuiManager> {
+class GuiManager : public Common::Singleton<GuiManager>, public CommandSender {
 	friend class Dialog;
 	friend class Common::Singleton<SingletonBaseType>;
 	GuiManager();
@@ -168,6 +172,7 @@ protected:
 
 	Common::Mutex _iconsMutex;
 	Common::SearchSet _iconsSet;
+	bool _iconsSetChanged;
 
 	// position and time of last mouse click (used to detect double clicks)
 	struct MousePos {

gui/launcher.cpp
@@ -1477,6 +1477,9 @@ void LauncherGrid::handleCommand(CommandSender *sender, uint32 cmd, uint32 data)
 		ConfMan.flushToDisk();
 		reflowLayout();
 		break;
+	case kIconsSetLoadedCmd:
+		rebuild();
+		break;
 	default:
 		LauncherDialog::handleCommand(sender, cmd, data);
 	}

gui/options.cpp
@@ -2991,12 +2991,7 @@ void GlobalOptionsDialog::handleCommand(CommandSender *sender, uint32 cmd, uint3
 #ifdef USE_LIBCURL
 	case kUpdateIconsCmd: {
 		DownloadIconsDialog dia;
-
-		if (dia.runModal() > 0) {
-			if (_launcher && _launcher->getType() == kLauncherDisplayGrid)
-				_launcher->rebuild();
-		}
-
+		dia.runModal();
 		break;
 	}
 #endif

commit fc2d802c40a754ea436ad2fddb1132159ef421ff

GUI: workaround for "unsupported game" dialogue
This sendCommand() call interrupts the "this game is unsupported" call,
causing it to automatically cancel.

Fixes a bug introduced by c55b1167874a26f0c979871fd304223f6bb96b6d.

gui/gui-manager.cpp
@@ -89,6 +89,7 @@ GuiManager::GuiManager() : CommandSender(nullptr), _redrawStatus(kRedrawDisabled
 
 	initTextToSpeech();
 	initIconsSet();
+	_iconsSetChanged = false;
 
 	ConfMan.registerDefault("gui_theme", "scummremastered");
 	Common::String themefile(ConfMan.get("gui_theme"));