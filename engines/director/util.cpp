sev-/scummvm-new

commit 2dd202894689b52b3294e8579a217ebf7717a510

DIRECTOR: do not try to open dirs as files

engines/director/util.cpp
@@ -393,6 +393,7 @@ Common::String getPath(Common::String path, Common::String cwd) {
 
 bool testPath(Common::String &path, bool directory) {
 	Common::FSNode d = Common::FSNode(*g_director->getGameDataDir());
+	Common::FSNode node;
 
 	// Test if we have it right in the SearchMan
 	if (SearchMan.hasFile(Common::Path(path, g_director->_dirSeparator)))
@@ -426,12 +427,18 @@ bool testPath(Common::String &path, bool directory) {
 			// for each element in the path, choose the first FSNode
 			// with a case-insensitive matcing name
 			if (i->getName().equalsIgnoreCase(token)) {
+				// If this is a directory, it's not a valid candidate
+				node = Common::FSNode(*i);
+				if (node.isDirectory()) {
+					continue;
+				}
+
 				exists = true;
 				newPath += i->getName();
 				if (!directory_list.empty())
 					newPath += (g_director->_dirSeparator);
 
-				d = Common::FSNode(*i);
+				d = node;
 				break;
 			}
 		}

commit 0628b4eca8b924900b780576b48b8948fc6a272e

DIRECTOR: Fix searching for files in subdirectories
Fixes regression introduced in 2dd202894689b52b3294e8579a217ebf7717a510

Fixes resource detection in Total Distortion.

engines/director/util.cpp
@@ -427,9 +427,9 @@ bool testPath(Common::String &path, bool directory) {
 			// for each element in the path, choose the first FSNode
 			// with a case-insensitive matcing name
 			if (i->getName().equalsIgnoreCase(token)) {
-				// If this is a directory, it's not a valid candidate
+				// If this the final path component, check if we're allowed to match with a directory
 				node = Common::FSNode(*i);
-				if (node.isDirectory()) {
+				if (directory_list.empty() && !directory && node.isDirectory()) {
 					continue;
 				}
 