sev-/scummvm-new

commit af641e3e4dcfe49f0864b1567f1146869d0096e7

GRAPHICS: Add rectifyId to check for changed font IDs

graphics/fonts/winfont.cpp
@@ -68,7 +68,7 @@ static WinFontDirEntry readDirEntry(Common::SeekableReadStream &stream) {
 
 	stream.skip(68); // Useless
 	entry.points = stream.readUint16LE();
-	stream.skip(43); // Useless (for now, maybe not in the future)
+	stream.skip(38); // Useless (for now, maybe not in the future)
 	readString(stream);
 	entry.faceName = readString(stream);
 

engines/director/castmember.cpp
@@ -1344,6 +1344,10 @@ void TextCastMember::importStxt(const Stxt *stxt) {
 	_fgpalinfo3 = stxt->_style.b;
 	_ftext = stxt->_ftext;
 	_ptext = stxt->_ptext;
+
+	//Creating a widget to seee if we actually have that font or not
+	Graphics::MacFont *macFont = new Graphics::MacFont(_fontId, _fontSize, _textSlant);
+	_fontId = g_director->_wm->_fontMan->rectifyId(macFont, g_director->_wm->_fontMan->getFont(*macFont));
 }
 
 Graphics::MacWidget *TextCastMember::createWidget(Common::Rect &bbox, Channel *channel, SpriteType spriteType) {

graphics/macgui/macfontmanager.cpp
@@ -492,7 +492,18 @@ const Font *MacFontManager::getFont(MacFont macFont) {
 	if (!font)
 		font = FontMan.getFontByUsage(macFont.getFallback());
 
+
 	return font;
+}
+
+int MacFontManager::rectifyId(const MacFont *macFont, const Font *font) {
+	for (auto it = _fontInfo.begin(); it != _fontInfo.end(); it++) {
+		if (((BdfFont *)font)->getFamilyName() == it->_value->name) {
+			return it->_key;
+		}
+	}
+
+	return macFont->getId();
 }
 
 int MacFontManager::parseFontSlant(Common::String slant) {

graphics/macgui/macfontmanager.h
@@ -143,6 +143,7 @@ public:
 	 */
 	const Common::String getFontName(uint16 id, int size, int slant = kMacFontRegular, bool tryGen = false);
 	const Common::String getFontName(MacFont &font);
+	int rectifyId(const MacFont *macFont, const Font *font);
 	int getFontIdByName(Common::String name);
 
 	Common::Language getFontLanguage(uint16 id);

commit 2733467c28c52ffb0d1a75e8d924a03068bc0c54

GRAPHICS: Fix WinFontDirEntry parsing regression
Fixes HUGO1 crash on startup

Incorrect offset introduced in af641e3e4dcfe49f0864b1567f1146869d0096e7

https://learn.microsoft.com/en-us/windows/win32/menurc/fontdirentry

graphics/fonts/winfont.cpp
@@ -68,7 +68,7 @@ static WinFontDirEntry readDirEntry(Common::SeekableReadStream &stream) {
 
 	stream.skip(68); // Useless
 	entry.points = stream.readUint16LE();
-	stream.skip(38); // Useless (for now, maybe not in the future)
+	stream.skip(43); // Useless (for now, maybe not in the future)
 	readString(stream); // Skip Device Name
 	entry.faceName = readString(stream);
 