sev-/scummvm-new

commit b27086d266b92da2db769d3f057d43f0ddb53401

SCI32: Update kSetShowStyle version checks for Mac
Consolidated kSetShowStyle version/argument checks and updated them to
handle SCI2.1 Mac games and PC demos. (The demos where the wrong
arguments were being applied happened to work anyway.)

Fixes LSL7 Mac pixel dissolve at the start of the game between the
postcard and the boat (rooms 120 and 130)

engines/sci/engine/kgraphics32.cpp
@@ -396,18 +396,23 @@ reg_t kSetShowStyle(EngineState *s, int argc, reg_t *argv) {
 	int16 divisions;
 
 	// SCI 2–2.1early
-	if (getSciVersion() < SCI_VERSION_2_1_MIDDLE) {
+	// KQ7 2.0b uses a mismatched version of the Styler script (SCI2.1early script
+	// for SCI2.1mid engine), so the calls it makes to kSetShowStyle are wrong and
+	// put `divisions` where `pFadeArray` is supposed to be.
+	if (getSciVersion() <= SCI_VERSION_1_EARLY || g_sci->getGameId() == GID_KQ7) {
 		blackScreen = 0;
 		pFadeArray = NULL_REG;
 		divisions = argc > 7 ? argv[7].toSint16() : -1;
 	}
-	// SCI 2.1mid
-	else if (getSciVersion() < SCI_VERSION_2_1_LATE) {
+	// SCI 2.1mid and RAMA demo (2.1late) and noninteractive Lighthouse demo (2.1late)
+	else if (getSciVersion() <= SCI_VERSION_2_1_MIDDLE ||
+		(g_sci->getGameId() == GID_RAMA && g_sci->isDemo()) ||
+		(g_sci->getGameId() == GID_LIGHTHOUSE && g_sci->isDemo() && getSciVersion() == SCI_VERSION_2_1_LATE)) {
 		blackScreen = 0;
 		pFadeArray = argc > 7 ? argv[7] : NULL_REG;
 		divisions = argc > 8 ? argv[8].toSint16() : -1;
 	}
-	// SCI 2.1late-3
+	// SCI 2.1late-3. Includes Mac 2.1late games and LSL7 demo (2.1late)
 	else {
 		blackScreen = argv[7].toSint16();
 		pFadeArray = argc > 8 ? argv[8] : NULL_REG;
@@ -420,7 +425,7 @@ reg_t kSetShowStyle(EngineState *s, int argc, reg_t *argv) {
 
 	// The order of planeObj and showStyle are reversed because this is how
 	// SSCI3 called the corresponding method on the KernelMgr
-	g_sci->_gfxTransitions32->kernelSetShowStyle(argc, planeObj, (ShowStyleType)type, seconds, back, priority, animate, refFrame, pFadeArray, divisions, blackScreen);
+	g_sci->_gfxTransitions32->kernelSetShowStyle(planeObj, (ShowStyleType)type, seconds, back, priority, animate, refFrame, pFadeArray, divisions, blackScreen);
 
 	return s->r_acc;
 }

engines/sci/graphics/transitions32.cpp
@@ -173,31 +173,12 @@ void GfxTransitions32::processEffects(PlaneShowStyle &showStyle) {
 	}
 }
 
-// TODO: 10-argument version is only in SCI3; argc checks are currently wrong for this version
-// and need to be fixed in future
-void GfxTransitions32::kernelSetShowStyle(const uint16 argc, const reg_t planeObj, const ShowStyleType type, const int16 seconds, const int16 back, const int16 priority, const int16 animate, const int16 frameOutNow, reg_t pFadeArray, int16 divisions, const int16 blackScreen) {
+void GfxTransitions32::kernelSetShowStyle(const reg_t planeObj, const ShowStyleType type, const int16 seconds,
+	const int16 back, const int16 priority, const int16 animate, const int16 frameOutNow,
+	const reg_t pFadeArray, const int16 divisions, const int16 blackScreen) {
 
-	bool hasDivisions = false;
-	bool hasFadeArray = false;
-
-	// KQ7 2.0b uses a mismatched version of the Styler script (SCI2.1early script
-	// for SCI2.1mid engine), so the calls it makes to kSetShowStyle are wrong and
-	// put `divisions` where `pFadeArray` is supposed to be
-	if (getSciVersion() == SCI_VERSION_2_1_MIDDLE && g_sci->getGameId() == GID_KQ7) {
-		hasDivisions = argc > 7;
-		hasFadeArray = false;
-		divisions = argc > 7 ? pFadeArray.toSint16() : -1;
-		pFadeArray = NULL_REG;
-	} else if (getSciVersion() < SCI_VERSION_2_1_MIDDLE) {
-		hasDivisions = argc > 7;
-		hasFadeArray = false;
-	} else if (getSciVersion() < SCI_VERSION_3) {
-		hasDivisions = argc > 8;
-		hasFadeArray = argc > 7;
-	} else {
-		hasDivisions = argc > 9;
-		hasFadeArray = argc > 8;
-	}
+	const bool hasFadeArray = !pFadeArray.isNull();
+	const bool hasDivisions = (divisions != -1);
 
 	bool isFadeUp;
 	int16 color;

engines/sci/detection.h
@@ -142,8 +142,8 @@ enum SciVersion {
 	SCI_VERSION_2, // GK1, PQ4 floppy, QFG4 floppy
 	SCI_VERSION_2_1_EARLY, // GK2 demo, KQ7 1.4/1.51, LSL6 hires, PQ4CD, QFG4CD
 	SCI_VERSION_2_1_MIDDLE, // GK2, Hoyle 5, KQ7 2.00b, MUMG Deluxe, Phantasmagoria 1, PQ:SWAT, Shivers 1, SQ6, Torin
-	SCI_VERSION_2_1_LATE, // demos and Mac versions of LSL7, Lighthouse, RAMA
-	SCI_VERSION_3 // LSL7, Lighthouse, RAMA, Phantasmagoria 2
+	SCI_VERSION_2_1_LATE, // Demos and Mac versions of LSL7, Lighthouse, RAMA
+	SCI_VERSION_3 // LSL7, Lighthouse, RAMA, Phantasmagoria 2, interactive Lighthouse demos
 };
 
 /** MIDI devices */

engines/sci/graphics/transitions32.h
@@ -256,7 +256,9 @@ public:
 	 */
 	void processEffects(PlaneShowStyle &showStyle);
 
-	void kernelSetShowStyle(const uint16 argc, const reg_t planeObj, const ShowStyleType type, const int16 seconds, const int16 direction, const int16 priority, const int16 animate, const int16 frameOutNow, reg_t pFadeArray, int16 divisions, const int16 blackScreen);
+	void kernelSetShowStyle(const reg_t planeObj, const ShowStyleType type, const int16 seconds,
+		const int16 direction, const int16 priority, const int16 animate, const int16 frameOutNow,
+		const reg_t pFadeArray, const int16 divisions, const int16 blackScreen);
 
 	/**
 	 * Sets the range that will be used by `GfxFrameout::palMorphFrameOut` to

commit 8b231d9babd5218337847f6ec5f0fc72ddc8ad6c

SCI32: Fix kSetShowStyle version checks
Fix version checks and divisions table. Now the early SQ6 demos start.

Fix version typo introduced in b27086d266b92da2db769d3f057d43f0ddb53401

engines/sci/engine/kgraphics32.cpp
@@ -399,7 +399,7 @@ reg_t kSetShowStyle(EngineState *s, int argc, reg_t *argv) {
 	// KQ7 2.0b uses a mismatched version of the Styler script (SCI2.1early script
 	// for SCI2.1mid engine), so the calls it makes to kSetShowStyle are wrong and
 	// put `divisions` where `pFadeArray` is supposed to be.
-	if (getSciVersion() <= SCI_VERSION_1_EARLY || g_sci->getGameId() == GID_KQ7) {
+	if (getSciVersion() <= SCI_VERSION_2_1_EARLY || g_sci->getGameId() == GID_KQ7) {
 		blackScreen = 0;
 		pFadeArray = NULL_REG;
 		divisions = argc > 7 ? argv[7].toSint16() : -1;
@@ -419,7 +419,16 @@ reg_t kSetShowStyle(EngineState *s, int argc, reg_t *argv) {
 		divisions = argc > 9 ? argv[9].toSint16() : -1;
 	}
 
-	if ((getSciVersion() < SCI_VERSION_2_1_MIDDLE && g_sci->getGameId() != GID_KQ7 && type == 15) || type > 15) {
+	// kShowStyleMorph (15) was introduced during SCI2.1early.
+	// It appears after LSL6 and PQ4, but so far the only known
+	// SCI2.1early games to use it are KQ7 and SQ6 demos.
+	int maxType = 15;
+	if (getSciVersion() < SCI_VERSION_2_1_EARLY ||
+		g_sci->getGameId() == GID_LSL6 ||
+		g_sci->getGameId() == GID_PQ4) {
+		maxType = 14;
+	}
+	if (type > maxType) {
 		error("Illegal show style %d for plane %04x:%04x", type, PRINT_REG(planeObj));
 	}
 

engines/sci/graphics/transitions32.cpp
@@ -35,7 +35,8 @@ static int dissolveSequences[2][20] = {
 	/* SCI2.1mid+ */ { 0, 0, 3, 6, 12, 20, 48, 96, 184, 272, 576, 1280, 3232, 6912, 13568, 24576, 46080, 73728, 132096, 466944 }
 };
 static int16 divisionsDefaults[2][16] = {
-	/* SCI2.1early- */ { 1, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 40, 40, 101, 101 },
+	// the last element in SCI2.1early was added after LSL6 and PQ4
+	/* SCI2.1early- */ { 1, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 40, 40, 101, 101, 2 },
 	/* SCI2.1mid+ */   { 1, 20, 20, 20, 20, 10, 10, 10, 10, 20, 20,  6, 10, 101, 101, 2 }
 };
 