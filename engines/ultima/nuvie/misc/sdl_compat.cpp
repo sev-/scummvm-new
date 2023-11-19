sev-/scummvm-new

commit 4ade2214340e64065caa0014140b54e79c486cd9

ULTIMA: ULTIMA6: Fix crash in GUI on surface blit
SDL_LoadBMP: Return ManagedSurfaces with their PixelFormat
matching that of the screen.

This fixes a crash when a surface created from a bitmap is blitted
to the screen surface in a GUI dialog.
("ERROR: Surface::transBlitFrom: bytesPerPixel must be 1, 2, or 4")

The bug was introduced in commit
6fcef450db845c7d9646432ac6095457d79a55c6
"IMAGE: Speed up 16/24/32bpp BMP decoding"

engines/ultima/nuvie/misc/sdl_compat.cpp
@@ -27,6 +27,7 @@
 #include "common/textconsole.h"
 #include "graphics/managed_surface.h"
 #include "image/bmp.h"
+#include "ultima/nuvie/screen/screen.h"
 
 namespace Ultima {
 namespace Nuvie {
@@ -92,7 +93,11 @@ Graphics::ManagedSurface *SDL_LoadBMP(const char *filename) {
 		error("Could not load bitmap - %s", filename);
 
 	const Graphics::Surface *src = decoder.getSurface();
-	Graphics::ManagedSurface *dest = new Graphics::ManagedSurface(src->w, src->h, src->format);
+	Screen *const screen = Game::get_game()->get_screen();
+	assert (screen);
+	Graphics::ManagedSurface *const screenSurface = screen->get_sdl_surface();
+	assert (screenSurface);
+	Graphics::ManagedSurface *dest = new Graphics::ManagedSurface(src->w, src->h, screenSurface->format);
 	dest->blitFrom(*src);
 
 	return dest;

commit 78f617fafa1d55fd00149a58dd3be008842ab27f

ULTIMA: ULTIMA6: Fix assert on loading roof bitmap
Do not ignore palette when loading bitmaps via SDL_LoadBMP.

Prevents the following assert on game start when roof drawing
is enabled:

"graphics/managed_surface.cpp:298:
void Graphics::ManagedSurface::blitFromInner(const Graphics::Surface&,
 const Common::Rect&, const Common::Rect&, const byte*):
Assertion `!destFormat.isCLUT8() && srcPalette' failed."

The bug was introduced in 4ade2214340e64065caa0014140b54e79c486cd9
"Fix crash in GUI on surface blit"

Fixes #14687

engines/ultima/nuvie/misc/sdl_compat.cpp
@@ -98,7 +98,7 @@ Graphics::ManagedSurface *SDL_LoadBMP(const char *filename) {
 	Graphics::ManagedSurface *const screenSurface = screen->get_sdl_surface();
 	assert (screenSurface);
 	Graphics::ManagedSurface *dest = new Graphics::ManagedSurface(src->w, src->h, screenSurface->format);
-	dest->blitFrom(*src);
+	dest->blitFrom(*src, decoder.getPalette());
 
 	return dest;
 }