scummvm/scummvm

GUI: Add percentage bar for checksum calculation
Based on file size

gui/integrity-dialog.cpp
@@ -27,6 +27,9 @@
 #include "common/file.h"
 #include "common/md5.h"
 
+#include "gui/message.h"
+#include "gui/widget.h"
+
 namespace GUI {
 
 IntegrityDialog::IntegrityDialog(Common::String endpoint, Common::String gameConfig) : Dialog("GameOptions_IntegrityDialog") {
@@ -37,9 +40,52 @@ IntegrityDialog::IntegrityDialog(Common::String endpoint, Common::String gameCon
 	_extra = ConfMan.get("extra", gameConfig);
 	_platform = ConfMan.get("platform", gameConfig);
 	_language = ConfMan.get("language", gameConfig);
+
+	calculateTotalSize(_gamePath);
+	_calculatedSize = 0;
+	_progressPercentage = 0;
+
+	MessageDialog alert(Common::U32String("Verifying file integrity may take a long time to complete.\nAre you sure you want to continue?"), "OK", "Cancel");
+	alert.runModal();
+
+	_progressBar = new SliderWidget(this, "GameOptions_IntegrityDialog.ProgressBar");
+	_progressBar->setMinValue(0);
+	_progressBar->setMaxValue(100);
+	_progressBar->setValue(_progressPercentage);
+	_progressBar->setEnabled(false);
 }
 
 IntegrityDialog::~IntegrityDialog() {
+}
+
+void IntegrityDialog::calculateTotalSize(Common::String gamePath) {
+	const Common::FSNode dir(gamePath);
+
+	if (!dir.exists() || !dir.isDirectory())
+		return;
+
+	Common::FSList fileList;
+	if (!dir.getChildren(fileList, Common::FSNode::kListAll))
+		return;
+
+	if (fileList.empty())
+		return;
+
+	// Process the files and subdirectories in the current directory recursively
+	for (Common::FSList::const_iterator it = fileList.begin(); it != fileList.end(); it++) {
+		const Common::FSNode &entry = *it;
+
+		if (entry.isDirectory())
+			calculateTotalSize(entry.getPath());
+		else {
+			const Common::Path filename(entry.getPath());
+			Common::File file;
+			if (!file.open(entry))
+				continue;
+
+			_totalSize += file.size();
+		}
+	}
 }
 
 Common::Array<Common::StringArray> IntegrityDialog::generateChecksums(Common::String gamePath, Common::Array<Common::StringArray> &fileChecksums) {
@@ -76,6 +122,9 @@ Common::Array<Common::StringArray> IntegrityDialog::generateChecksums(Common::St
 			// Tail checksums with checksize 5000
 			file.seek(-5000, SEEK_END);
 			fileChecksum.push_back(Common::computeStreamMD5AsString(file).c_str());
+
+			_calculatedSize += file.size();
+			_progressPercentage = _calculatedSize / _totalSize;
 
 			file.close();
 			fileChecksums.push_back(fileChecksum);

gui/integrity-dialog.h
@@ -29,6 +29,7 @@
 #include "common/str.h"
 
 #include "gui/dialog.h"
+#include "gui/widget.h"
 
 namespace GUI {
 
@@ -41,17 +42,25 @@ class IntegrityDialog : Dialog {
 	Common::String _platform;
 	Common::String _language;
 
+	int _totalSize;
+	int _calculatedSize;
+	int _progressPercentage;
+
+	SliderWidget *_progressBar;
+
 public:
 	IntegrityDialog(Common::String endpoint, Common::String gameConfig);
 	~IntegrityDialog();
 
 	void sendJSON();
-	static Common::JSONValue *generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
 
 private:
+	void calculateTotalSize(Common::String gamePath);
+
 	void checksumResponseCallback(Common::JSONValue *r);
 	void errorCallback(Networking::ErrorResponse error);
-	static Common::Array<Common::StringArray> generateChecksums(Common::String gamePath, Common::Array<Common::StringArray> &fileChecksums);
+	Common::Array<Common::StringArray> generateChecksums(Common::String gamePath, Common::Array<Common::StringArray> &fileChecksums);
+	Common::JSONValue *generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
 };
 
 } // End of namespace GUI

sev-/scummvm-new

gui/integrity-dialog.cpp
@@ -27,6 +27,9 @@
 #include "common/file.h"
 #include "common/md5.h"
 
+#include "gui/message.h"
+#include "gui/widget.h"
+
 namespace GUI {
 
 IntegrityDialog::IntegrityDialog(Common::String endpoint, Common::String gameConfig) : Dialog("GameOptions_IntegrityDialog") {
@@ -37,9 +40,52 @@ IntegrityDialog::IntegrityDialog(Common::String endpoint, Common::String gameCon
 	_extra = ConfMan.get("extra", gameConfig);
 	_platform = ConfMan.get("platform", gameConfig);
 	_language = ConfMan.get("language", gameConfig);
+
+	calculateTotalSize(_gamePath);
+	_calculatedSize = 0;
+	_progressPercentage = 0;
+
+	MessageDialog alert(Common::U32String("Verifying file integrity may take a long time to complete.\nAre you sure you want to continue?"), "OK", "Cancel");
+	alert.runModal();
+
+	_progressBar = new SliderWidget(this, "GameOptions_IntegrityDialog.ProgressBar");
+	_progressBar->setMinValue(0);
+	_progressBar->setMaxValue(100);
+	_progressBar->setValue(_progressPercentage);
+	_progressBar->setEnabled(false);
 }
 
 IntegrityDialog::~IntegrityDialog() {
+}
+
+void IntegrityDialog::calculateTotalSize(Common::Path gamePath) {
+	const Common::FSNode dir(gamePath);
+
+	if (!dir.exists() || !dir.isDirectory())
+		return;
+
+	Common::FSList fileList;
+	if (!dir.getChildren(fileList, Common::FSNode::kListAll))
+		return;
+
+	if (fileList.empty())
+		return;
+
+	// Process the files and subdirectories in the current directory recursively
+	for (Common::FSList::const_iterator it = fileList.begin(); it != fileList.end(); it++) {
+		const Common::FSNode &entry = *it;
+
+		if (entry.isDirectory())
+			calculateTotalSize(entry.getPath());
+		else {
+			const Common::Path filename(entry.getPath());
+			Common::File file;
+			if (!file.open(entry))
+				continue;
+
+			_totalSize += file.size();
+		}
+	}
 }
 
 Common::Array<Common::StringArray> IntegrityDialog::generateChecksums(Common::Path gamePath, Common::Array<Common::StringArray> &fileChecksums) {
@@ -76,6 +122,9 @@ Common::Array<Common::StringArray> IntegrityDialog::generateChecksums(Common::Pa
 			// Tail checksums with checksize 5000
 			file.seek(-5000, SEEK_END);
 			fileChecksum.push_back(Common::computeStreamMD5AsString(file).c_str());
+
+			_calculatedSize += file.size();
+			_progressPercentage = _calculatedSize / _totalSize;
 
 			file.close();
 			fileChecksums.push_back(fileChecksum);

gui/integrity-dialog.h
@@ -29,29 +29,38 @@
 #include "common/str.h"
 
 #include "gui/dialog.h"
+#include "gui/widget.h"
 
 namespace GUI {
 
 class IntegrityDialog : Dialog {
 	Common::String _endpoint;
-	Common::String _gamePath;
+	Common::Path _gamePath;
 	Common::String _gameid;
 	Common::String _engineid;
 	Common::String _extra;
 	Common::String _platform;
 	Common::String _language;
 
+	int _totalSize;
+	int _calculatedSize;
+	int _progressPercentage;
+
+	SliderWidget *_progressBar;
+
 public:
 	IntegrityDialog(Common::String endpoint, Common::String gameConfig);
 	~IntegrityDialog();
 
 	void sendJSON();
-	static Common::JSONValue *generateJSONRequest(Common::Path gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
 
 private:
+	void calculateTotalSize(Common::Path gamePath);
+
 	void checksumResponseCallback(const Common::JSONValue *r);
 	void errorCallback(const Networking::ErrorResponse &error);
-	static Common::Array<Common::StringArray> generateChecksums(Common::Path gamePath, Common::Array<Common::StringArray> &fileChecksums);
+	Common::Array<Common::StringArray> generateChecksums(Common::Path gamePath, Common::Array<Common::StringArray> &fileChecksums);
+	Common::JSONValue *generateJSONRequest(Common::Path gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
 };
 
 } // End of namespace GUI