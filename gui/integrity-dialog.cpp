scummvm/scummvm

GUI: Create IntegrityDialog class

gui/integrity-dialog.cpp
@@ -29,7 +29,7 @@
 
 namespace GUI {
 
-Common::Array<Common::StringArray> generateChecksums(Common::String gamePath, Common::Array<Common::StringArray> &fileChecksums) {
+Common::Array<Common::StringArray> IntegrityDialog::generateChecksums(Common::String gamePath, Common::Array<Common::StringArray> &fileChecksums) {
 	const Common::FSNode dir(gamePath);
 
 	if (!dir.exists() || !dir.isDirectory())
@@ -72,7 +72,7 @@ Common::Array<Common::StringArray> generateChecksums(Common::String gamePath, Co
 	return fileChecksums;
 }
 
-void generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language) {
+void IntegrityDialog::generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language) {
 	Common::Array<Common::StringArray> fileChecksums = {};
 	fileChecksums = generateChecksums(gamePath, fileChecksums);
 	Common::JSONObject requestObject;

gui/integrity-dialog.h
@@ -25,9 +25,18 @@
 #include "common/array.h"
 #include "common/str.h"
 
+#include "gui/dialog.h"
+
 namespace GUI {
 
-void generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
+class IntegrityDialog : public Dialog {
+
+public:
+	static void generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
+
+private:
+	static Common::Array<Common::StringArray> generateChecksums(Common::String gamePath, Common::Array<Common::StringArray> &fileChecksums);
+};
 
 } // End of namespace GUI
 

sev-/scummvm-new

gui/integrity-dialog.cpp
@@ -29,7 +29,7 @@
 
 namespace GUI {
 
-Common::Array<Common::StringArray> generateChecksums(Common::Path gamePath, Common::Array<Common::StringArray> &fileChecksums) {
+Common::Array<Common::StringArray> IntegrityDialog::generateChecksums(Common::Path gamePath, Common::Array<Common::StringArray> &fileChecksums) {
 	const Common::FSNode dir(gamePath);
 
 	if (!dir.exists() || !dir.isDirectory())
@@ -72,7 +72,7 @@ Common::Array<Common::StringArray> generateChecksums(Common::Path gamePath, Comm
 	return fileChecksums;
 }
 
-void generateJSONRequest(Common::Path gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language) {
+void IntegrityDialog::generateJSONRequest(Common::Path gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language) {
 	Common::Array<Common::StringArray> fileChecksums = {};
 	fileChecksums = generateChecksums(gamePath, fileChecksums);
 	Common::JSONObject requestObject;

gui/integrity-dialog.h
@@ -25,9 +25,18 @@
 #include "common/array.h"
 #include "common/str.h"
 
+#include "gui/dialog.h"
+
 namespace GUI {
 
-void generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
+class IntegrityDialog : public Dialog {
+
+public:
+	static void generateJSONRequest(Common::Path gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language);
+
+private:
+	static Common::Array<Common::StringArray> generateChecksums(Common::Path gamePath, Common::Array<Common::StringArray> &fileChecksums);
+};
 
 } // End of namespace GUI
 