scummvm/scummvm

GUI: Implement checksum calculation for game files

gui/integrity-dialog.cpp
@@ -1,0 +1,116 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "gui/integrity-dialog.h"
+
+#include "common/array.h"
+#include "common/debug.h"
+#include "common/file.h"
+#include "common/formats/json.h"
+#include "common/md5.h"
+
+namespace GUI {
+
+Common::Array<Common::StringArray> generateChecksums(Common::String gamePath, Common::Array<Common::StringArray> &fileChecksums) {
+	const Common::FSNode dir(gamePath);
+
+	if (!dir.exists() || !dir.isDirectory())
+		return {};
+
+	Common::FSList fileList;
+	if (!dir.getChildren(fileList, Common::FSNode::kListAll))
+		return {};
+
+	if (fileList.empty())
+		return {};
+
+	// Process the files and subdirectories in the current directory recursively
+	for (Common::FSList::const_iterator it = fileList.begin(); it != fileList.end(); it++) {
+		const Common::FSNode &entry = *it;
+
+		if (entry.isDirectory())
+			generateChecksums(entry.getPath(), fileChecksums);
+		else {
+			const Common::Path filename(entry.getPath());
+			Common::File file;
+			if (!file.open(entry))
+				continue;
+
+			Common::Array<Common::String> fileChecksum =
+				{filename.toString(), Common::String(file.size())};
+			// Various checksizes
+			for (auto size : {0, 5000, 1024 * 1024}) {
+				fileChecksum.push_back(Common::computeStreamMD5AsString(file, size).c_str());
+			}
+			// Tail checksums with checksize 5000
+			file.seek(-5000, SEEK_END);
+			fileChecksum.push_back(Common::computeStreamMD5AsString(file).c_str());
+
+			fileChecksums.push_back(fileChecksum);
+		}
+	}
+
+	return fileChecksums;
+}
+
+void generateJSONRequest(Common::String gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language) {
+	Common::Array<Common::StringArray> fileChecksums = {};
+	fileChecksums = generateChecksums(gamePath, fileChecksums);
+	Common::JSONObject requestObject;
+
+	requestObject.setVal("gameid", new Common::JSONValue(gameid));
+	requestObject.setVal("engineid", new Common::JSONValue(engineid));
+	requestObject.setVal("extra", new Common::JSONValue(extra));
+	requestObject.setVal("platform", new Common::JSONValue(platform));
+	requestObject.setVal("language", new Common::JSONValue(language));
+
+	Common::JSONArray filesObject;
+
+	for (Common::StringArray fileChecksum : fileChecksums) {
+		Common::JSONObject file;
+		file.setVal("name", new Common::JSONValue(fileChecksum[0]));
+		file.setVal("size", new Common::JSONValue(fileChecksum[1]));
+
+		Common::JSONArray checksums;
+		int index = 0;
+		for (Common::String val : fileChecksum) {
+			Common::JSONObject checksum;
+			if (index < 2) {
+				index++;
+				continue;
+			}
+			checksum.setVal("type", new Common::JSONValue("md5"));
+			checksum.setVal("checksum", new Common::JSONValue(val));
+
+			checksums.push_back(new Common::JSONValue(checksum));
+		}
+		file.setVal("checksums", new Common::JSONValue(checksums));
+
+		filesObject.push_back(new Common::JSONValue(file));
+	}
+
+	requestObject.setVal("files", new Common::JSONValue(filesObject));
+
+	Common::JSONValue request(requestObject);
+	debug(request.stringify().c_str());
+}
+
+} // End of namespace GUI

sev-/scummvm-new

gui/integrity-dialog.cpp
@@ -1,0 +1,116 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "gui/integrity-dialog.h"
+
+#include "common/array.h"
+#include "common/debug.h"
+#include "common/file.h"
+#include "common/formats/json.h"
+#include "common/md5.h"
+
+namespace GUI {
+
+Common::Array<Common::StringArray> generateChecksums(Common::Path gamePath, Common::Array<Common::StringArray> &fileChecksums) {
+	const Common::FSNode dir(gamePath);
+
+	if (!dir.exists() || !dir.isDirectory())
+		return {};
+
+	Common::FSList fileList;
+	if (!dir.getChildren(fileList, Common::FSNode::kListAll))
+		return {};
+
+	if (fileList.empty())
+		return {};
+
+	// Process the files and subdirectories in the current directory recursively
+	for (Common::FSList::const_iterator it = fileList.begin(); it != fileList.end(); it++) {
+		const Common::FSNode &entry = *it;
+
+		if (entry.isDirectory())
+			generateChecksums(entry.getPath(), fileChecksums);
+		else {
+			const Common::Path filename(entry.getPath());
+			Common::File file;
+			if (!file.open(entry))
+				continue;
+
+			Common::Array<Common::String> fileChecksum =
+				{filename.toString(), Common::String(file.size())};
+			// Various checksizes
+			for (auto size : {0, 5000, 1024 * 1024}) {
+				fileChecksum.push_back(Common::computeStreamMD5AsString(file, size).c_str());
+			}
+			// Tail checksums with checksize 5000
+			file.seek(-5000, SEEK_END);
+			fileChecksum.push_back(Common::computeStreamMD5AsString(file).c_str());
+
+			fileChecksums.push_back(fileChecksum);
+		}
+	}
+
+	return fileChecksums;
+}
+
+void generateJSONRequest(Common::Path gamePath, Common::String gameid, Common::String engineid, Common::String extra, Common::String platform, Common::String language) {
+	Common::Array<Common::StringArray> fileChecksums = {};
+	fileChecksums = generateChecksums(gamePath, fileChecksums);
+	Common::JSONObject requestObject;
+
+	requestObject.setVal("gameid", new Common::JSONValue(gameid));
+	requestObject.setVal("engineid", new Common::JSONValue(engineid));
+	requestObject.setVal("extra", new Common::JSONValue(extra));
+	requestObject.setVal("platform", new Common::JSONValue(platform));
+	requestObject.setVal("language", new Common::JSONValue(language));
+
+	Common::JSONArray filesObject;
+
+	for (Common::StringArray fileChecksum : fileChecksums) {
+		Common::JSONObject file;
+		file.setVal("name", new Common::JSONValue(fileChecksum[0]));
+		file.setVal("size", new Common::JSONValue(fileChecksum[1]));
+
+		Common::JSONArray checksums;
+		int index = 0;
+		for (Common::String val : fileChecksum) {
+			Common::JSONObject checksum;
+			if (index < 2) {
+				index++;
+				continue;
+			}
+			checksum.setVal("type", new Common::JSONValue("md5"));
+			checksum.setVal("checksum", new Common::JSONValue(val));
+
+			checksums.push_back(new Common::JSONValue(checksum));
+		}
+		file.setVal("checksums", new Common::JSONValue(checksums));
+
+		filesObject.push_back(new Common::JSONValue(file));
+	}
+
+	requestObject.setVal("files", new Common::JSONValue(filesObject));
+
+	Common::JSONValue request(requestObject);
+	debug(request.stringify().c_str());
+}
+
+} // End of namespace GUI