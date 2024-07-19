scummvm/scummvm

CINE: Add keymapper support

engines/cine/metaengine.cpp
@@ -28,6 +28,10 @@
 #include "common/translation.h"
 #include "common/util.h"
 
+#include "backends/keymapper/action.h"
+#include "backends/keymapper/keymapper.h"
+#include "backends/keymapper/standard-actions.h"
+
 #include "cine/cine.h"
 #include "cine/various.h"
 
@@ -92,6 +96,8 @@ public:
 	void removeSaveState(const char *target, int slot) const override;
 	Common::String getSavegameFile(int saveGameIdx, const char *target = nullptr) const override;
 	SaveStateDescriptor querySaveMetaInfos(const char *target, int slot) const override;
+	
+	Common::KeymapArray initKeymaps(const char *target) const override;
 };
 
 bool CineMetaEngine::hasFeature(MetaEngineFeature f) const {
@@ -243,6 +249,161 @@ SaveStateDescriptor CineMetaEngine::querySaveMetaInfos(const char *target, int s
 	}
 
 	return SaveStateDescriptor();
+}
+
+Common::KeymapArray CineMetaEngine::initKeymaps(const char *target) const {
+	using namespace Common;
+	using namespace Cine;
+
+	Keymap *engineKeyMap = new Keymap(Keymap::kKeymapTypeGame, "cine-main", "CINE main");
+	Keymap *gameKeyMap = new Keymap(Keymap::kKeymapTypeGame, "game-shortcuts", _("Game Keymappings"));
+	Keymap *mouseKeyMap = new Keymap(Keymap::kKeymapTypeGame, "mouse-shortcuts", _("Key to Mouse Keymappings"));
+
+	Action *act;
+
+	act = new Action(kStandardActionLeftClick, _("Left Click"));
+	act->setLeftClickEvent();
+	act->addDefaultInputMapping("MOUSE_LEFT");
+	act->addDefaultInputMapping("JOY_A");
+	engineKeyMap->addAction(act);
+
+	act = new Action(kStandardActionRightClick, _("Right Click"));
+	act->setRightClickEvent();
+	act->addDefaultInputMapping("MOUSE_RIGHT");
+	act->addDefaultInputMapping("JOY_B");
+	engineKeyMap->addAction(act);
+
+	act = new Action("MOUSELEFT", _("Select option / Click in game"));
+	act->setCustomEngineActionEvent(kActionMouseLeft);
+	act->addDefaultInputMapping("RETURN");
+	act->addDefaultInputMapping("KP_ENTER");
+	act->addDefaultInputMapping("KP5");
+	mouseKeyMap->addAction(act);
+
+	act = new Action("MOUSERIGHT", _("Open Action Menu / Close Menu"));
+	act->setCustomEngineActionEvent(kActionMouseRight);
+	act->addDefaultInputMapping("ESCAPE");
+	mouseKeyMap->addAction(act);
+
+	act = new Action("DEFAULTSPEED", _("Default Speed"));
+	act->setCustomEngineActionEvent(kActionGameSpeedDefault);
+	act->addDefaultInputMapping("KP0");
+	gameKeyMap->addAction(act);
+
+	act = new Action("SLOWERSPEED", _("Slower Speed"));
+	act->setCustomEngineActionEvent(kActionGameSpeedSlower);
+	act->addDefaultInputMapping("MINUS");
+	act->addDefaultInputMapping("KP_MINUS");
+	act->allowKbdRepeats();
+	gameKeyMap->addAction(act);
+
+	act = new Action("FASTERSPEED", _("Faster Speed"));
+	act->setCustomEngineActionEvent(kActionGameSpeedFaster);
+	act->addDefaultInputMapping("PLUS");
+	act->addDefaultInputMapping("KP_PLUS");
+	act->addDefaultInputMapping("S+EQUALS");
+	act->allowKbdRepeats();
+	gameKeyMap->addAction(act);
+
+	act = new Action("EXAMINE", _("Examine"));
+	act->setCustomEngineActionEvent(kActionExamine);
+	act->addDefaultInputMapping("F1");
+	gameKeyMap->addAction(act);
+
+	act = new Action("TAKE", _("Take"));
+	act->setCustomEngineActionEvent(kActionTake);
+	act->addDefaultInputMapping("F2");
+	gameKeyMap->addAction(act);
+
+	act = new Action("INVENTORY", _("Inventory"));
+	act->setCustomEngineActionEvent(kActionInventory);
+	act->addDefaultInputMapping("F3");
+	gameKeyMap->addAction(act);
+
+	act = new Action("USE", _("Use"));
+	act->setCustomEngineActionEvent(kActionUse);
+	act->addDefaultInputMapping("F4");
+	gameKeyMap->addAction(act);
+
+	act = new Action("ACTIVATE", _("Activate"));
+	act->setCustomEngineActionEvent(kActionActivate);
+	act->addDefaultInputMapping("F5");
+	gameKeyMap->addAction(act);
+
+	act = new Action("SPEAK", _("Speak"));
+	act->setCustomEngineActionEvent(kActionSpeak);
+	act->addDefaultInputMapping("F6");
+	gameKeyMap->addAction(act);
+
+	act = new Action("ACTMENU", _("Action Menu"));
+	act->setCustomEngineActionEvent(kActionActionMenu);
+	act->addDefaultInputMapping("F9");
+	act->addDefaultInputMapping("JOY_LEFT_SHOULDER");
+	gameKeyMap->addAction(act);
+
+	act = new Action("SYSMENU", _("System Menu"));
+	act->setCustomEngineActionEvent(kActionSystemMenu);
+	act->addDefaultInputMapping("F10");
+	act->addDefaultInputMapping("JOY_RIGHT_SHOULDER");
+	gameKeyMap->addAction(act);
+
+	act = new Action("COLLISIONPAGE", _("Show Collisions"));
+	act->setCustomEngineActionEvent(kActionCollisionPage);
+	act->addDefaultInputMapping("F11");
+	act->addDefaultInputMapping("JOY_Y");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVEUP", _("Move Up"));
+	act->setCustomEngineActionEvent(kActionMoveUp);
+	act->addDefaultInputMapping("KP8");
+	act->addDefaultInputMapping("JOY_UP");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVEDOWN", _("Move Down"));
+	act->setCustomEngineActionEvent(kActionMoveDown);
+	act->addDefaultInputMapping("KP2");
+	act->addDefaultInputMapping("JOY_DOWN");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVELEFT", _("Move Left"));
+	act->setCustomEngineActionEvent(kActionMoveLeft);
+	act->addDefaultInputMapping("KP4");
+	act->addDefaultInputMapping("JOY_LEFT");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVERIGHT", _("Move Right"));
+	act->setCustomEngineActionEvent(kActionMoveRight);
+	act->addDefaultInputMapping("KP6");
+	act->addDefaultInputMapping("JOY_RIGHT");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVEUPLEFT", _("Move Up-Left"));
+	act->setCustomEngineActionEvent(kActionMoveUpLeft);
+	act->addDefaultInputMapping("KP7");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVEUPRIGHT", _("Move Up-Right"));
+	act->setCustomEngineActionEvent(kActionMoveUpRight);
+	act->addDefaultInputMapping("KP9");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVEDOWNLEFT", _("Move Down-Left"));
+	act->setCustomEngineActionEvent(kActionMoveDownLeft);
+	act->addDefaultInputMapping("KP1");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MOVEDOWNRIGHT", _("Move Down-Right"));
+	act->setCustomEngineActionEvent(kActionMoveDownRight);
+	act->addDefaultInputMapping("KP3");
+	gameKeyMap->addAction(act);
+
+		
+	KeymapArray keymaps(3);
+	keymaps[0] = engineKeyMap;
+	keymaps[1] = mouseKeyMap;
+	keymaps[2] = gameKeyMap;
+
+	return keymaps;
 }
 
 void CineMetaEngine::removeSaveState(const char *target, int slot) const {

sev-/scummvm-new

engines/cine/metaengine.cpp
@@ -28,6 +28,10 @@
 #include "common/translation.h"
 #include "common/util.h"
 
+#include "backends/keymapper/action.h"
+#include "backends/keymapper/keymapper.h"
+#include "backends/keymapper/standard-actions.h"
+
 #include "cine/cine.h"
 #include "cine/various.h"
 
@@ -92,6 +96,8 @@ public:
 	void removeSaveState(const char *target, int slot) const override;
 	Common::String getSavegameFile(int saveGameIdx, const char *target = nullptr) const override;
 	SaveStateDescriptor querySaveMetaInfos(const char *target, int slot) const override;
+	
+	Common::KeymapArray initKeymaps(const char *target) const override;
 };
 
 bool CineMetaEngine::hasFeature(MetaEngineFeature f) const {
@@ -243,6 +249,190 @@ SaveStateDescriptor CineMetaEngine::querySaveMetaInfos(const char *target, int s
 	}
 
 	return SaveStateDescriptor();
+}
+
+Common::KeymapArray CineMetaEngine::initKeymaps(const char *target) const {
+	using namespace Common;
+	using namespace Cine;
+
+	Keymap *engineKeyMap = new Keymap(Keymap::kKeymapTypeGame, "cine-default", _("Default keymappings"));
+	Keymap *gameKeyMap = new Keymap(Keymap::kKeymapTypeGame, "game-shortcuts", _("Game keymappings"));
+	Keymap *mouseKeyMap = new Keymap(Keymap::kKeymapTypeGame, "mouse-shortcuts", _("Key to mouse keymappings"));
+	Keymap *introKeyMap = new Keymap(Keymap::kKeymapTypeGame, "intro-shortcuts", _("Exit screen keymappings"));
+
+	Action *act;
+
+	act = new Action(kStandardActionLeftClick, _("Left click"));
+	act->setLeftClickEvent();
+	act->addDefaultInputMapping("MOUSE_LEFT");
+	act->addDefaultInputMapping("JOY_A");
+	engineKeyMap->addAction(act);
+
+	act = new Action(kStandardActionRightClick, _("Right click"));
+	act->setRightClickEvent();
+	act->addDefaultInputMapping("MOUSE_RIGHT");
+	act->addDefaultInputMapping("JOY_B");
+	engineKeyMap->addAction(act);
+
+	act = new Action("SKIPSONY", _("Exit Sony intro screen"));
+	act->setCustomEngineActionEvent(kActionExitSonyScreen);
+	act->addDefaultInputMapping("ESCAPE");
+	act->addDefaultInputMapping("JOY_A");
+	introKeyMap->addAction(act);
+
+	act = new Action("MOUSELEFT", _("Select option / Click in game"));
+	act->setCustomEngineActionEvent(kActionMouseLeft);
+	act->addDefaultInputMapping("RETURN");
+	act->addDefaultInputMapping("KP_ENTER");
+	act->addDefaultInputMapping("KP5");
+	mouseKeyMap->addAction(act);
+
+	act = new Action("MOUSERIGHT", _("Open action menu / Close menu"));
+	act->setCustomEngineActionEvent(kActionMouseRight);
+	act->addDefaultInputMapping("ESCAPE");
+	mouseKeyMap->addAction(act);
+
+	act = new Action("DEFAULTSPEED", _("Default game speed"));
+	act->setCustomEngineActionEvent(kActionGameSpeedDefault);
+	act->addDefaultInputMapping("KP0");
+	gameKeyMap->addAction(act);
+
+	act = new Action("SLOWERSPEED", _("Slower game speed"));
+	act->setCustomEngineActionEvent(kActionGameSpeedSlower);
+	act->addDefaultInputMapping("MINUS");
+	act->addDefaultInputMapping("KP_MINUS");
+	act->allowKbdRepeats();
+	gameKeyMap->addAction(act);
+
+	act = new Action("FASTERSPEED", _("Faster game speed"));
+	act->setCustomEngineActionEvent(kActionGameSpeedFaster);
+	act->addDefaultInputMapping("PLUS");
+	act->addDefaultInputMapping("KP_PLUS");
+	act->addDefaultInputMapping("S+EQUALS");
+	act->allowKbdRepeats();
+	gameKeyMap->addAction(act);
+
+	act = new Action("EXAMINE", _("Examine"));
+	act->setCustomEngineActionEvent(kActionExamine);
+	act->addDefaultInputMapping("F1");
+	gameKeyMap->addAction(act);
+
+	act = new Action("TAKE", _("Take"));
+	act->setCustomEngineActionEvent(kActionTake);
+	act->addDefaultInputMapping("F2");
+	gameKeyMap->addAction(act);
+
+	act = new Action("INVENTORY", _("Inventory"));
+	act->setCustomEngineActionEvent(kActionInventory);
+	act->addDefaultInputMapping("F3");
+	gameKeyMap->addAction(act);
+
+	act = new Action("USE", _("Use"));
+	act->setCustomEngineActionEvent(kActionUse);
+	act->addDefaultInputMapping("F4");
+	gameKeyMap->addAction(act);
+
+	act = new Action("ACTIVATE", _("Activate"));
+	act->setCustomEngineActionEvent(kActionActivate);
+	act->addDefaultInputMapping("F5");
+	gameKeyMap->addAction(act);
+
+	act = new Action("SPEAK", _("Speak"));
+	act->setCustomEngineActionEvent(kActionSpeak);
+	act->addDefaultInputMapping("F6");
+	gameKeyMap->addAction(act);
+
+	act = new Action("ACTMENU", _("Action menu"));
+	act->setCustomEngineActionEvent(kActionActionMenu);
+	act->addDefaultInputMapping("F9");
+	act->addDefaultInputMapping("JOY_LEFT_SHOULDER");
+	gameKeyMap->addAction(act);
+
+	act = new Action("SYSMENU", _("System menu"));
+	act->setCustomEngineActionEvent(kActionSystemMenu);
+	act->addDefaultInputMapping("F10");
+	act->addDefaultInputMapping("JOY_RIGHT_SHOULDER");
+	gameKeyMap->addAction(act);
+
+	// I18N: Opens collision map of where the actor can freely move
+	act = new Action("COLLISIONPAGE", _("Show collisions"));
+	act->setCustomEngineActionEvent(kActionCollisionPage);
+	act->addDefaultInputMapping("F11");
+	act->addDefaultInputMapping("JOY_Y");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to upwards direction
+	act = new Action("MOVEUP", _("Move up"));
+	act->setCustomEngineActionEvent(kActionMoveUp);
+	act->addDefaultInputMapping("KP8");
+	act->addDefaultInputMapping("JOY_UP");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to downwards direction
+	act = new Action("MOVEDOWN", _("Move down"));
+	act->setCustomEngineActionEvent(kActionMoveDown);
+	act->addDefaultInputMapping("KP2");
+	act->addDefaultInputMapping("JOY_DOWN");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to left direction
+	act = new Action("MOVELEFT", _("Move left"));
+	act->setCustomEngineActionEvent(kActionMoveLeft);
+	act->addDefaultInputMapping("KP4");
+	act->addDefaultInputMapping("JOY_LEFT");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to right direction
+	act = new Action("MOVERIGHT", _("Move right"));
+	act->setCustomEngineActionEvent(kActionMoveRight);
+	act->addDefaultInputMapping("KP6");
+	act->addDefaultInputMapping("JOY_RIGHT");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to top-left direction
+	act = new Action("MOVEUPLEFT", _("Move up-left"));
+	act->setCustomEngineActionEvent(kActionMoveUpLeft);
+	act->addDefaultInputMapping("KP7");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to top-right direction
+	act = new Action("MOVEUPRIGHT", _("Move up-right"));
+	act->setCustomEngineActionEvent(kActionMoveUpRight);
+	act->addDefaultInputMapping("KP9");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to bottom-left direction
+	act = new Action("MOVEDOWNLEFT", _("Move down-left"));
+	act->setCustomEngineActionEvent(kActionMoveDownLeft);
+	act->addDefaultInputMapping("KP1");
+	gameKeyMap->addAction(act);
+
+	// I18N: Move Actor to bottom-right direction
+	act = new Action("MOVEDOWNRIGHT", _("Move down-right"));
+	act->setCustomEngineActionEvent(kActionMoveDownRight);
+	act->addDefaultInputMapping("KP3");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MENUUP", _("Menu option up"));
+	act->setCustomEngineActionEvent(kActionMenuOptionUp);
+	act->addDefaultInputMapping("UP");
+	gameKeyMap->addAction(act);
+
+	act = new Action("MENUDOWN", _("Menu option down"));
+	act->setCustomEngineActionEvent(kActionMenuOptionDown);
+	act->addDefaultInputMapping("DOWN");
+	gameKeyMap->addAction(act);
+
+		
+	KeymapArray keymaps(4);
+	keymaps[0] = engineKeyMap;
+	keymaps[1] = mouseKeyMap;
+	keymaps[2] = gameKeyMap;
+	keymaps[3] = introKeyMap;
+
+	introKeyMap->setEnabled(false);
+
+	return keymaps;
 }
 
 void CineMetaEngine::removeSaveState(const char *target, int slot) const {