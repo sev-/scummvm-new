sev-/scummvm-new

commit 88aeed557684f72310ec7a5e1b877689a80020ec

ULTIMA: NUVIE: Use foreach style loops for cleaner code
Includes some small other cleanups in MapWindow.

engines/ultima/nuvie/pathfinder/astar_path.cpp
@@ -134,7 +134,9 @@ bool AStarPath::path_search(const MapCoord &start, const MapCoord &goal) {
 //DEBUG(0,LEVEL_DEBUGGING,"FAIL\n");
 	delete_nodes();
 	return false; // out of open nodes - failure
-}/* Return the cost of moving one step from `c1' to `c2', which is always 1. This
+}
+
+/* Return the cost of moving one step from `c1' to `c2', which is always 1. This
  * isn't very helpful, so subclasses should provide their own function.
  * Returns -1 if c2 is blocked. */
 sint32 AStarPath::step_cost(const MapCoord &c1, const MapCoord &c2) {
@@ -142,34 +144,44 @@ sint32 AStarPath::step_cost(const MapCoord &c1, const MapCoord &c2) {
 	        || c2.distance(c1) > 1)
 		return -1;
 	return 1;
-}/* Return an item in the list of closed nodes whose location matches `ncmp'.
- */astar_node *AStarPath::find_closed_node(astar_node *ncmp) {
-	Std::list<astar_node *>::iterator n;
-	for (n = closed_nodes.begin(); n != closed_nodes.end(); n++)
-		if ((*n)->loc == ncmp->loc)
-			return *n;
+}
+
+/* Return an item in the list of closed nodes whose location matches `ncmp'.
+ */
+astar_node *AStarPath::find_closed_node(astar_node *ncmp) {
+	for (astar_node *n : open_nodes)
+		if (n->loc == ncmp->loc)
+			return n;
 	return nullptr;
-}/* Return an item in the list of closed nodes whose location matches `ncmp'.
- */astar_node *AStarPath::find_open_node(astar_node *ncmp) {
-	Std::list<astar_node *>::iterator n;
-	for (n = open_nodes.begin(); n != open_nodes.end(); n++)
-		if ((*n)->loc == ncmp->loc)
-			return *n;
+}
+
+/* Return an item in the list of closed nodes whose location matches `ncmp'.
+ */
+astar_node *AStarPath::find_open_node(astar_node *ncmp) {
+	for (astar_node *n : open_nodes)
+		if (n->loc == ncmp->loc)
+			return n;
 	return nullptr;
-}/* Add new node pointer to the list of open nodes (sorting by score).
- */void AStarPath::push_open_node(astar_node *node) {
-	Std::list<astar_node *>::iterator n, next;
+}
+
+/* Add new node pointer to the list of open nodes (sorting by score).
+ */
+void AStarPath::push_open_node(astar_node *node) {
 	if (open_nodes.empty()) {
 		open_nodes.push_front(node);
 		return;
 	}
-	n = open_nodes.begin();
+
+	Std::list<astar_node *>::iterator n = open_nodes.begin();
 	// get to end of list or to a node with equal or greater score
 	while (n != open_nodes.end() && (*n++)->score < node->score);
 	open_nodes.insert(n, node); // and add before that location
-}/* Return pointer to the highest priority node from the list of open nodes, and
+}
+
+/* Return pointer to the highest priority node from the list of open nodes, and
  * remove it.
- */astar_node *AStarPath::pop_open_node() {
+ */
+astar_node *AStarPath::pop_open_node() {
 	astar_node *best = open_nodes.front();
 	open_nodes.pop_front(); // remove it
 	return best;

engines/ultima/nuvie/actors/actor.cpp
@@ -310,8 +310,7 @@ void Actor::set_poisoned(bool poisoned) {
 const char *Actor::get_name(bool force_real_name) {
 	ActorManager *actor_manager = Game::get_game()->get_actor_manager();
 	Converse *converse = Game::get_game()->get_converse();
-	Party *party = Game::get_game()->get_party();
-	//Actor *player = Game::get_game()->get_player()->get_actor();
+	const Party *party = Game::get_game()->get_party();
 	const char *talk_name = nullptr; // name from conversation script
 	bool statue = (Game::get_game()->get_game_type() == NUVIE_GAME_U6 && id_n >= 189 && id_n <= 200);
 
@@ -336,12 +335,10 @@ void Actor::add_surrounding_obj(Obj *obj) {
 void Actor::unlink_surrounding_objects(bool make_objects_temporary) {
 //	if(make_objects_temporary)
 	{
-		Std::list<Obj *>::iterator obj;
-
-		for (obj = surrounding_objects.begin(); obj != surrounding_objects.end(); obj++) {
+		for (Obj *obj : surrounding_objects) {
 			if (make_objects_temporary)
-				(*obj)->set_temporary();
-			(*obj)->set_actor_obj(false);
+				obj->set_temporary();
+			obj->set_actor_obj(false);
 		}
 	}
 	surrounding_objects.clear();
@@ -1408,9 +1405,8 @@ void Actor::clear() {
 void Actor::show() {
 	visible_flag = true;
 
-	Std::list<Obj *>::iterator obj_iter;
-	for (obj_iter = surrounding_objects.begin(); obj_iter != surrounding_objects.end(); obj_iter++) {
-		(*obj_iter)->set_invisible(false);
+	for (Obj *obj : surrounding_objects) {
+		obj->set_invisible(false);
 	}
 
 }
@@ -1418,9 +1414,8 @@ void Actor::show() {
 void Actor::hide() {
 	visible_flag = false;
 
-	Std::list<Obj *>::iterator obj_iter;
-	for (obj_iter = surrounding_objects.begin(); obj_iter != surrounding_objects.end(); obj_iter++) {
-		(*obj_iter)->set_invisible(true);
+	for (Obj *obj : surrounding_objects) {
+		obj->set_invisible(true);
 	}
 }
 
@@ -1650,12 +1645,12 @@ void Actor::subtract_light(uint8 val) {
 //        light -= val;
 //    else
 //        light = 0;
-	vector<uint8>::iterator l = light_source.begin();
-	for (; l != light_source.end(); l++)
+	for (vector<uint8>::iterator l = light_source.begin(); l != light_source.end(); l++) {
 		if (*l == val) {
 			light_source.erase(l);
 			break;
 		}
+	}
 	light = 0; // change to next highest light source
 	for (unsigned int lCtr = 0; lCtr < light_source.size(); lCtr++)
 		if (light_source[lCtr] > light)

engines/ultima/nuvie/actors/actor.h
@@ -564,8 +564,8 @@ public:
 	void clear_error();
 	ActorError *get_error();
 
-	list<Obj *> *get_surrounding_obj_list() {
-		return surrounding_objects.empty() ? nullptr : &surrounding_objects;
+	const list<Obj *> &get_surrounding_obj_list() const {
+		return surrounding_objects;
 	}
 	void add_surrounding_obj(Obj *obj);
 	void unlink_surrounding_objects(bool make_objects_temporary = false);

engines/ultima/nuvie/actors/actor_manager.cpp
@@ -1123,8 +1123,7 @@ void ActorManager::loadAvatarTiles(const Std::string &datadir) {
 
 	Std::set<Std::string> files = getCustomTileFilenames(datadir, "avatar_");
 
-	for (Std::set<Std::string>::iterator iter = files.begin(); iter != files.end(); iter++) {
-		Std::string filename = *iter;
+	for (const Std::string &filename : files) {
 		if (filename.length() != 19) { // avatar_nnn_nnnn.bmp
 			continue;
 		}
@@ -1152,8 +1151,7 @@ void ActorManager::loadNPCTiles(const Std::string &datadir) {
 
 	Std::set<Std::string> files = getCustomTileFilenames(datadir, "actor_");
 
-	for (Std::set<Std::string>::iterator iter = files.begin(); iter != files.end(); iter++) {
-		Std::string filename = *iter;
+	for (const Std::string &filename : files) {
 		if (filename.length() != 18) { // actor_nnn_nnnn.bmp
 			continue;
 		}

engines/ultima/nuvie/actors/u6_actor.cpp
@@ -705,17 +705,12 @@ bool U6Actor::weapon_can_hit(const CombatType *weapon, Actor *target, uint16 *hi
 		return true;
 	}
 
-	Std::list<Obj *> *surrounding_objs = target->get_surrounding_obj_list();
-
-	if (surrounding_objs) {
-		Std::list<Obj *>::iterator obj_iter;
-		for (obj_iter = surrounding_objs->begin(); obj_iter != surrounding_objs->end(); obj_iter++) {
-			Obj *obj = *obj_iter;
-			if (Actor::weapon_can_hit(weapon, obj->x, obj->y)) {
-				*hit_x = obj->x;
-				*hit_y = obj->y;
-				return true;
-			}
+	const Std::list<Obj *> &surrounding_objs = target->get_surrounding_obj_list();
+	for (Obj *obj : surrounding_objs) {
+		if (Actor::weapon_can_hit(weapon, obj->x, obj->y)) {
+			*hit_x = obj->x;
+			*hit_y = obj->y;
+			return true;
 		}
 	}
 
@@ -1022,32 +1017,24 @@ inline bool U6Actor::has_surrounding_objs() {
 }
 
 inline void U6Actor::remove_surrounding_objs_from_map() {
-	Std::list<Obj *>::iterator obj;
-
-	for (obj = surrounding_objects.begin(); obj != surrounding_objects.end(); obj++)
-		obj_manager->remove_obj_from_map((*obj));
+	for (Obj *obj : surrounding_objects)
+		obj_manager->remove_obj_from_map(obj);
 
 	return;
 }
 
 inline void U6Actor::add_surrounding_objs_to_map() {
-	Std::list<Obj *>::reverse_iterator obj;
-
-	for (obj = surrounding_objects.rbegin(); obj != surrounding_objects.rend(); ++obj)
-		obj_manager->add_obj((*obj), OBJ_ADD_TOP);
+	for (Obj *obj : surrounding_objects)
+		obj_manager->add_obj(obj, OBJ_ADD_TOP);
 
 	return;
 }
 
 inline void U6Actor::move_surrounding_objs_relative(sint16 rel_x, sint16 rel_y) {
-	Std::list<Obj *>::iterator obj_iter;
-	Obj *obj;
-
 	if (obj_n == OBJ_U6_SILVER_SERPENT) {
 		move_silver_serpent_objs_relative(rel_x, rel_y);
 	} else {
-		for (obj_iter = surrounding_objects.begin(); obj_iter != surrounding_objects.end(); obj_iter++) {
-			obj = *obj_iter;
+		for (Obj *obj : surrounding_objects) {
 			obj->x = WRAPPED_COORD(obj->x + rel_x, z);
 			obj->y = WRAPPED_COORD(obj->y + rel_y, z);
 		}
@@ -1356,12 +1343,8 @@ inline void U6Actor::set_direction_of_surrounding_dragon_objs(NuvieDir new_direc
 }
 
 inline void U6Actor::twitch_surrounding_objs() {
-	Std::list<Obj *>::iterator obj;
-
-	for (obj = surrounding_objects.begin(); obj != surrounding_objects.end(); obj++) {
-		twitch_obj(*obj);
-	}
-
+	for (Obj *obj : surrounding_objects)
+		twitch_obj(obj);
 }
 
 inline void U6Actor::twitch_surrounding_dragon_objs() {
@@ -1409,8 +1392,6 @@ inline void U6Actor::twitch_obj(Obj *obj) {
 }
 
 inline void U6Actor::clear_surrounding_objs_list(bool delete_objs) {
-	Std::list<Obj *>::iterator obj;
-
 	if (surrounding_objects.empty())
 		return;
 
@@ -1419,7 +1400,7 @@ inline void U6Actor::clear_surrounding_objs_list(bool delete_objs) {
 		return;
 	}
 
-	obj = surrounding_objects.begin();
+	Std::list<Obj *>::iterator obj = surrounding_objects.begin();
 
 	for (; !surrounding_objects.empty();) {
 		obj_manager->remove_obj_from_map(*obj);

engines/ultima/nuvie/conf/configuration.cpp
@@ -41,9 +41,8 @@ Configuration::Configuration() : _configChanged(false) {
 }
 
 Configuration::~Configuration() {
-	for (Std::vector<Shared::XMLTree *>::iterator i = _trees.begin();
-	        i != _trees.end(); ++i) {
-		delete(*i);
+	for (Shared::XMLTree *t : _trees) {
+		delete(t);
 	}
 
 	if (_configChanged)
@@ -65,17 +64,15 @@ bool Configuration::readConfigFile(const Std::string &fname, const Std::string &
 }
 
 void Configuration::write() {
-	for (Std::vector<Shared::XMLTree *>::iterator i = _trees.begin();
-	        i != _trees.end(); ++i) {
-		if (!(*i)->isReadonly())
-			(*i)->write();
+	for (Shared::XMLTree *t : _trees) {
+		if (!t->isReadonly())
+			t->write();
 	}
 }
 
 void Configuration::clear() {
-	for (Std::vector<Shared::XMLTree *>::iterator i = _trees.begin();
-	        i != _trees.end(); ++i) {
-		delete(*i);
+	for (Shared::XMLTree *t : _trees) {
+		delete(t);
 	}
 	_trees.clear();
 }
@@ -283,10 +280,9 @@ Std::set<Std::string> Configuration::listKeys(const Std::string &key, bool longf
 }
 
 void Configuration::getSubkeys(KeyTypeList &ktl, const Std::string &basekey) {
-	for (Std::vector<Shared::XMLTree *>::iterator tree = _trees.begin();
-	        tree != _trees.end(); ++tree) {
+	for (Shared::XMLTree *tree : _trees) {
 		Shared::XMLTree::KeyTypeList l;
-		(*tree)->getSubkeys(l, basekey);
+		tree->getSubkeys(l, basekey);
 
 		for (Shared::XMLTree::KeyTypeList::iterator i = l.begin();
 		        i != l.end(); ++i) {

engines/ultima/nuvie/core/effect.cpp
@@ -1314,14 +1314,9 @@ void TileFadeEffect::add_actor_anim() {
 	Tile *from = actor->get_tile();
 	add_tile_anim(loc, from);
 
-	Std::list<Obj *> *surrounding_objs = actor->get_surrounding_obj_list();
-
-	if (surrounding_objs) {
-		Std::list<Obj *>::iterator obj_iter;
-		for (obj_iter = surrounding_objs->begin(); obj_iter != surrounding_objs->end(); obj_iter++) {
-			add_obj_anim(*obj_iter);
-		}
-	}
+	const Std::list<Obj *> &surrounding_objs = actor->get_surrounding_obj_list();
+	for (Obj *obj : surrounding_objs)
+		add_obj_anim(obj);
 }
 
 void TileFadeEffect::add_obj_anim(Obj *obj) {
@@ -1420,14 +1415,9 @@ void TileBlackFadeEffect::add_actor_anim() {
 	Tile *from = actor->get_tile();
 	add_tile_anim(loc, from);
 
-	Std::list<Obj *> *surrounding_objs = actor->get_surrounding_obj_list();
-
-	if (surrounding_objs) {
-		Std::list<Obj *>::iterator obj_iter;
-		for (obj_iter = surrounding_objs->begin(); obj_iter != surrounding_objs->end(); obj_iter++) {
-			add_obj_anim(*obj_iter);
-		}
-	}
+	const Std::list<Obj *> &surrounding_objs = actor->get_surrounding_obj_list();
+	for (Obj *o : surrounding_objs)
+		add_obj_anim(o);
 }
 
 void TileBlackFadeEffect::add_obj_anim(Obj *o) {

engines/ultima/nuvie/core/egg_manager.cpp
@@ -112,36 +112,30 @@ void EggManager::remove_egg(Obj *egg_obj, bool keep_obj) {
 }
 
 void EggManager::set_egg_visibility(bool show_eggs) {
-	Std::list<Egg *>::iterator egg_iter;
-
-	for (egg_iter = egg_list.begin(); egg_iter != egg_list.end(); egg_iter++)
-		(*egg_iter)->obj->set_invisible(!show_eggs);
+	for (Egg *egg : egg_list)
+		egg->obj->set_invisible(!show_eggs);
 }
 
 void EggManager::spawn_eggs(uint16 x, uint16 y, uint8 z, bool teleport) {
-	Std::list<Egg *>::iterator egg;
-	sint16 dist_x, dist_y;
-	uint8 hatch_probability;
-
-	for (egg = egg_list.begin(); egg != egg_list.end();) {
-		uint8 quality = (*egg)->obj->quality;
-		dist_x = abs((sint16)(*egg)->obj->x - x);
-		dist_y = abs((sint16)(*egg)->obj->y - y);
+	for (Egg *egg : egg_list) {
+		uint8 quality = egg->obj->quality;
+		sint16 dist_x = abs((sint16)egg->obj->x - x);
+		sint16 dist_y = abs((sint16)egg->obj->y - y);
 
 		//Deactivate eggs that are more than 20 tiles from player.
-		if (((*egg)->obj->status & OBJ_STATUS_EGG_ACTIVE) && ((*egg)->obj->z != z || (dist_x >= 20 || dist_y >= 20))) {
-			(*egg)->obj->status &= (0xff ^ OBJ_STATUS_EGG_ACTIVE);
-			DEBUG(0, LEVEL_DEBUGGING, "Reactivate egg at (%x,%x,%d)\n", (*egg)->obj->x, (*egg)->obj->y, (*egg)->obj->z);
+		if ((egg->obj->status & OBJ_STATUS_EGG_ACTIVE) && (egg->obj->z != z || (dist_x >= 20 || dist_y >= 20))) {
+			egg->obj->status &= (0xff ^ OBJ_STATUS_EGG_ACTIVE);
+			DEBUG(0, LEVEL_DEBUGGING, "Reactivate egg at (%x,%x,%d)\n", egg->obj->x, egg->obj->y, egg->obj->z);
 		}
 
-		if (dist_x < 20 && dist_y < 20 && (*egg)->obj->z == z
+		if (dist_x < 20 && dist_y < 20 && egg->obj->z == z
 		        && (dist_x > 8 || dist_y > 8 || !Game::get_game()->is_orig_style() || teleport)) {
 
-			if (((*egg)->obj->status & OBJ_STATUS_EGG_ACTIVE) == 0) {
-				(*egg)->obj->status |= OBJ_STATUS_EGG_ACTIVE;
+			if ((egg->obj->status & OBJ_STATUS_EGG_ACTIVE) == 0) {
+				egg->obj->status |= OBJ_STATUS_EGG_ACTIVE;
 
-				hatch_probability = (NUVIE_RAND() % 100) + 1;
-				DEBUG(0, LEVEL_DEBUGGING, "Checking Egg (%x,%x,%x). Rand: %d Probability: %d%%", (*egg)->obj->x, (*egg)->obj->y, (*egg)->obj->z, hatch_probability, (*egg)->obj->qty);
+				uint8 hatch_probability = (NUVIE_RAND() % 100) + 1;
+				DEBUG(0, LEVEL_DEBUGGING, "Checking Egg (%x,%x,%x). Rand: %d Probability: %d%%", egg->obj->x, egg->obj->y, egg->obj->z, hatch_probability, egg->obj->qty);
 
 				DEBUG(1, LEVEL_DEBUGGING, " Align: %s", get_actor_alignment_str(static_cast<ActorAlignment>(quality % 10)));
 
@@ -150,11 +144,9 @@ void EggManager::spawn_eggs(uint16 x, uint16 y, uint8 z, bool teleport) {
 				else if (quality < 30) DEBUG(1, LEVEL_DEBUGGING, " (night)");  // 20-29
 				else if (quality < 40) DEBUG(1, LEVEL_DEBUGGING, " (day+night)"); // 30-39
 				DEBUG(1, LEVEL_DEBUGGING, "\n");
-				spawn_egg((*egg)->obj, hatch_probability);
+				spawn_egg(egg->obj, hatch_probability);
 			}
 		}
-
-		egg++;
 	}
 
 	return;

commit 941b384182cd23c53a56a93fd1a99adea1fd3a2c

ULTIMA: NUVIE: Fix copy-paste error in AStarPath
Fix regression introduced in
88aeed557684f72310ec7a5e1b877689a80020ec
"ULTIMA: NUVIE: Use foreach style loops for cleaner code"

engines/ultima/nuvie/pathfinder/astar_path.cpp
@@ -149,13 +149,13 @@ sint32 AStarPath::step_cost(const MapCoord &c1, const MapCoord &c2) {
 /* Return an item in the list of closed nodes whose location matches `ncmp'.
  */
 astar_node *AStarPath::find_closed_node(astar_node *ncmp) {
-	for (astar_node *n : open_nodes)
+	for (astar_node *n : closed_nodes)
 		if (n->loc == ncmp->loc)
 			return n;
 	return nullptr;
 }
 
-/* Return an item in the list of closed nodes whose location matches `ncmp'.
+/* Return an item in the list of open nodes whose location matches `ncmp'.
  */
 astar_node *AStarPath::find_open_node(astar_node *ncmp) {
 	for (astar_node *n : open_nodes)