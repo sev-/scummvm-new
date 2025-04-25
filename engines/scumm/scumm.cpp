sev-/scummvm-new

commit ec5456bc69c5181204704373e0a8616f03bd5fa2

SCUMM: Fix support of the macOS Steam releases using DOS resources (Trac#15849)
Back when modern macOS still had 32-bit support, Steam provided macOS
ports of some LEC titles, such as indy3-steam-mac.

Although marked as "Steam/Macintosh", they're not based on the
original Macintosh releases, but they're macOS ports of the original
DOS VGA releases.

Thus, we need to make sure not to override their midi settings with
MDT_MACINTOSH, since they use the usual DOS audio resources.

Fixes a fatal "IMuseDriver_Macintosh(): Unsupported game ID 3!" error,
when trying to start such games.

engines/scumm/scumm.cpp
@@ -1219,8 +1219,10 @@ Common::Error ScummEngine::init() {
 	// Steam Win and Mac versions share the same DOS data files. We show Windows or Mac
 	// for the platform the detector, but internally we force the platform to DOS, so that
 	// the code for handling the original DOS data files is used.
-	if (_filenamePattern.genMethod == kGenDiskNumSteam || _filenamePattern.genMethod == kGenRoomNumSteam)
+	if (_filenamePattern.genMethod == kGenDiskNumSteam || _filenamePattern.genMethod == kGenRoomNumSteam) {
 		_game.platform = Common::kPlatformDOS;
+		_game.midi = MDT_PCSPK | MDT_PCJR | MDT_CMS | MDT_ADLIB;
+	}
 
 	// Load CJK font, if present
 	// Load it earlier so _useCJKMode variable could be set

engines/scumm/metaengine.cpp
@@ -440,12 +440,15 @@ Common::Error ScummMetaEngine::createInstance(OSystem *syst, Engine **engine,
 		res.language = Common::parseLanguage(ConfMan.get("language"));
 
 	// V3 FM-TOWNS games *always* should use the corresponding music driver,
-	// anything else makes no sense for them. Same for Mac (but not limited to V3).
+	// anything else makes no sense for them. Same for Mac (but not limited to V3),
+	// except for the Steam macOS releases actually using DOS content.
 	// TODO: Maybe allow the null driver, too?
 	if (res.game.platform == Common::kPlatformFMTowns && res.game.version == 3)
 		res.game.midi = MDT_TOWNS;
-	else if (res.game.platform == Common::kPlatformMacintosh && res.game.version < 7 && res.game.heversion == 0)
-		res.game.midi = MDT_MACINTOSH;
+	else if (res.game.platform == Common::kPlatformMacintosh && res.game.version < 7 && res.game.heversion == 0) {
+		if (!(res.extra && strcmp(res.extra, "Steam") == 0))
+			res.game.midi = MDT_MACINTOSH;
+	}
 
 	// Finally, we have massaged the GameDescriptor to our satisfaction, and can
 	// instantiate the appropriate game engine. Hooray!

commit 47e930f1e3f0b1cdfcd74248ef8a3752f3f7c1ec

SCUMM: Unbreak (at least) the Steam/Windows release of Loom
Fixes triggering a startSound() assert in player_ad.cpp.

When fixing a regression with the macOS Steam releases, it was decided
to also reset the game MIDI options in ScummEngine::init(), but the
chosen values are incorrect for loom-steam-win.

Just rely on the ScummMetaEngine::createInstance() fix, instead, that
should be enough.

Bug introduced in commit ec5456bc69c5181204704373e0a8616f03bd5fa2.

engines/scumm/scumm.cpp
@@ -1219,10 +1219,8 @@ Common::Error ScummEngine::init() {
 	// Steam Win and Mac versions share the same DOS data files. We show Windows or Mac
 	// for the platform the detector, but internally we force the platform to DOS, so that
 	// the code for handling the original DOS data files is used.
-	if (_filenamePattern.genMethod == kGenDiskNumSteam || _filenamePattern.genMethod == kGenRoomNumSteam) {
+	if (_filenamePattern.genMethod == kGenDiskNumSteam || _filenamePattern.genMethod == kGenRoomNumSteam)
 		_game.platform = Common::kPlatformDOS;
-		_game.midi = MDT_PCSPK | MDT_PCJR | MDT_CMS | MDT_ADLIB;
-	}
 
 	// Load CJK font, if present
 	// Load it earlier so _useCJKMode variable could be set