sev-/scummvm-new

commit 12285632d88a6c4dc24b5a20cc1eab73adccd34d

AWE: Fix potential buffer overrun. CID 1609874

engines/awe/resource_win31.cpp
@@ -146,6 +146,10 @@ struct LzHuffman {
 		int i = _child[kHuffmanRoot];
 		while (i < kTableSize) {
 			i += _stream.readBit();
+
+			if (i < kTableSize)
+				i = _child[i];
+
 			i = _child[i];
 		}
 		i -= kTableSize;

commit 7c90c972caa20c25343ce0f26e29093eb63bd438

AWE: Fix regression introduced in 12285632d88a6c4dc24b5a20cc1eab73adccd34d

engines/awe/resource_win31.cpp
@@ -149,8 +149,6 @@ struct LzHuffman {
 
 			if (i < kTableSize)
 				i = _child[i];
-
-			i = _child[i];
 		}
 		i -= kTableSize;
 		update(i);