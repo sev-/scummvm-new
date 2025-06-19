scummvm/scummvm

GRAPHICS: MACGUI: Add text-to-speech (TTS) to submenus and dialog buttons

graphics/macgui/macwindowmanager.cpp
@@ -21,6 +21,7 @@
 #include "common/array.h"
 #include "common/list.h"
 #include "common/system.h"
+#include "common/text-to-speech.h"
 
 #include "graphics/cursorman.h"
 #include "graphics/managed_surface.h"
@@ -246,6 +247,8 @@ MacWindowManager::MacWindowManager(uint32 mode, MacPatterns *patterns, Common::L
 		replaceCursor(kMacCursorArrow);
 		CursorMan.showMouse(true);
 	}
+
+	_ttsEnabled = false;
 
 	loadDataBundle();
 	setDesktopMode(mode);
@@ -1529,6 +1532,17 @@ const Common::U32String::value_type *readHex(uint16 *res, const Common::U32Strin
 	}
 
 	return s;
+}
+
+void MacWindowManager::sayText(const Common::String &text) const {
+	Common::TextToSpeechManager *ttsMan = g_system->getTextToSpeechManager();
+	if (ttsMan && _ttsEnabled) {
+		ttsMan->say(text, Common::TextToSpeechManager::INTERRUPT);
+	}
+}
+
+void MacWindowManager::setTTSEnabled(bool enabled) {
+	_ttsEnabled = enabled;
 }
 
 

graphics/macgui/macwindowmanager.h
@@ -394,6 +394,9 @@ public:
 	MacMenu *getMenu();
 	MacMenu *getMenu(int id);
 
+	void sayText(const Common::String &text) const;
+	void setTTSEnabled(bool enabled);
+
 public:
 	MacFontManager *_fontMan;
 	uint32 _mode;
@@ -484,6 +487,8 @@ private:
 	Common::Archive *_dataBundle;
 
 	Common::U32String _clipboard;
+
+	bool _ttsEnabled;
 };
 
 const Common::U32String::value_type *readHex(uint16 *res, const Common::U32String::value_type *s, int len);

sev-/scummvm-new

graphics/macgui/macwindowmanager.cpp
@@ -21,6 +21,7 @@
 #include "common/array.h"
 #include "common/list.h"
 #include "common/system.h"
+#include "common/text-to-speech.h"
 
 #include "graphics/cursorman.h"
 #include "graphics/managed_surface.h"
@@ -246,6 +247,8 @@ MacWindowManager::MacWindowManager(uint32 mode, MacPatterns *patterns, Common::L
 		replaceCursor(kMacCursorArrow);
 		CursorMan.showMouse(true);
 	}
+
+	_ttsEnabled = false;
 
 	loadDataBundle();
 	setDesktopMode(mode);
@@ -1529,6 +1532,17 @@ const Common::U32String::value_type *readHex(uint16 *res, const Common::U32Strin
 	}
 
 	return s;
+}
+
+void MacWindowManager::sayText(const Common::U32String &text) const {
+	Common::TextToSpeechManager *ttsMan = g_system->getTextToSpeechManager();
+	if (ttsMan && _ttsEnabled) {
+		ttsMan->say(text, Common::TextToSpeechManager::INTERRUPT);
+	}
+}
+
+void MacWindowManager::setTTSEnabled(bool enabled) {
+	_ttsEnabled = enabled;
 }
 
 

graphics/macgui/macwindowmanager.h
@@ -394,6 +394,9 @@ public:
 	MacMenu *getMenu();
 	MacMenu *getMenu(int id);
 
+	void sayText(const Common::U32String &text) const;
+	void setTTSEnabled(bool enabled);
+
 public:
 	MacFontManager *_fontMan;
 	uint32 _mode;
@@ -484,6 +487,8 @@ private:
 	Common::Archive *_dataBundle;
 
 	Common::U32String _clipboard;
+
+	bool _ttsEnabled;
 };
 
 const Common::U32String::value_type *readHex(uint16 *res, const Common::U32String::value_type *s, int len);