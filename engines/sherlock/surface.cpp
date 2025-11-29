sev-/scummvm-new

commit 31b7a3d37ba94f017f15d0e65c88b837dfca0340

SHERLOCK: Replace use of ManagedSurface override colour

engines/sherlock/surface.cpp
@@ -55,19 +55,43 @@ void BaseSurface::writeFancyString(const Common::String &str, const Common::Poin
 }
 
 void BaseSurface::SHtransBlitFrom(const ImageFrame &src, const Common::Point &pt,
-		bool flipped, int overrideColor, int scaleVal) {
+		bool flipped, int scaleVal) {
 	Common::Point drawPt(pt.x + src.sDrawXOffset(scaleVal), pt.y + src.sDrawYOffset(scaleVal));
-	SHtransBlitFrom(src._frame, drawPt, flipped, overrideColor, scaleVal);
+	SHtransBlitFrom(src._frame, drawPt, flipped, scaleVal);
 }
 
 void BaseSurface::SHtransBlitFrom(const Graphics::Surface &src, const Common::Point &pt,
-		bool flipped, int overrideColor, int scaleVal) {
+		bool flipped, int scaleVal) {
 	Common::Rect srcRect(0, 0, src.w, src.h);
 	Common::Rect destRect(pt.x, pt.y, pt.x + src.w * SCALE_THRESHOLD / scaleVal,
 		pt.y + src.h * SCALE_THRESHOLD / scaleVal);
 
 	Graphics::Screen::transBlitFrom(src, srcRect, destRect, IS_3DO ? 0 : TRANSPARENCY,
-		flipped, overrideColor);
+		flipped);
+}
+
+void BaseSurface::SHoverrideBlitFrom(const ImageFrame &src, const Common::Point &pt,
+		int overrideColor) {
+	Common::Point drawPt(pt.x + src.sDrawXOffset(SCALE_THRESHOLD), pt.y + src.sDrawYOffset(SCALE_THRESHOLD));
+	SHoverrideBlitFrom(src._frame, drawPt, overrideColor);
+}
+
+void BaseSurface::SHoverrideBlitFrom(const Graphics::Surface &src, const Common::Point &pt,
+		int overrideColor) {
+	Common::Rect srcRect(0, 0, src.w, src.h);
+	Common::Rect destRect(pt.x, pt.y, pt.x + src.w, pt.y + src.h);
+	clip(srcRect, destRect);
+
+	const uint32 transColor = IS_3DO ? 0 : TRANSPARENCY;
+
+	for (uint y = 0; y < destRect.height(); y++) {
+		for (uint x = 0; x < destRect.width(); x++) {
+			const uint8 srcVal = src.getPixel(srcRect.left + x, srcRect.top + y);
+			if (srcVal == transColor)
+				continue;
+			setPixel(destRect.left + x, destRect.top + y, overrideColor);
+		}
+	}
 }
 
 void BaseSurface::SHbitmapBlitFrom(const byte *src, int widthSrc, int heightSrc, int pitchSrc, const Common::Point &pt,

engines/sherlock/fonts.cpp
@@ -318,7 +318,11 @@ void Fonts::writeString(BaseSurface *surface, const Common::String &str,
 
 		if (curChar < _charCount) {
 			ImageFrame &frame = (*_font)[curChar];
-			surface->SHtransBlitFrom(frame, Common::Point(charPos.x, charPos.y + _yOffsets[curChar]), false, overrideColor);
+			if (overrideColor) {
+				surface->SHoverrideBlitFrom(frame, Common::Point(charPos.x, charPos.y + _yOffsets[curChar]), overrideColor);
+			} else {
+				surface->SHtransBlitFrom(frame, Common::Point(charPos.x, charPos.y + _yOffsets[curChar]));
+			}
 			charPos.x += frame._frame.w + 1;
 		} else {
 			warning("Invalid character encountered - %d", (int)curChar);

engines/sherlock/scalpel/3do/scalpel_3do_screen.cpp
@@ -123,14 +123,14 @@ void Scalpel3DOScreen::SHfillRect(const Common::Rect &r, uint color) {
 }
 
 void Scalpel3DOScreen::SHtransBlitFrom(const ImageFrame &src, const Common::Point &pt,
-		bool flipped, int overrideColor, int scaleVal) {
-	ScalpelScreen::SHtransBlitFrom(src, pt, flipped, overrideColor,
+		bool flipped, int scaleVal) {
+	ScalpelScreen::SHtransBlitFrom(src, pt, flipped,
 		_vm->_isScreenDoubled ? scaleVal / 2 : scaleVal);
 }
 
 void Scalpel3DOScreen::SHtransBlitFrom(const Graphics::Surface &src, const Common::Point &pt,
-		bool flipped, int overrideColor, int scaleVal) {
-	ScalpelScreen::SHtransBlitFrom(src, pt, flipped, overrideColor,
+		bool flipped, int scaleVal) {
+	ScalpelScreen::SHtransBlitFrom(src, pt, flipped,
 		_vm->_isScreenDoubled ? scaleVal / 2 : scaleVal);
 }
 

engines/sherlock/scalpel/3do/scalpel_3do_screen.h
@@ -72,13 +72,13 @@ public:
 	 * Draws an image frame at a given position within this surface with transparency
 	 */
 	void SHtransBlitFrom(const ImageFrame &src, const Common::Point &pt,
-		bool flipped = false, int overrideColor = 0, int scaleVal = SCALE_THRESHOLD) override;
+		bool flipped = false, int scaleVal = SCALE_THRESHOLD) override;
 
 	/**
 	 * Draws an image frame at a given position within this surface with transparency
 	 */
 	void SHtransBlitFrom(const Graphics::Surface &src, const Common::Point &pt,
-		bool flipped = false, int overrideColor = 0, int scaleVal = SCALE_THRESHOLD) override;
+		bool flipped = false, int scaleVal = SCALE_THRESHOLD) override;
 
 	/**
 	 * Fill a given area of the surface with a given color

engines/sherlock/scalpel/scalpel.cpp
@@ -650,9 +650,9 @@ bool ScalpelEngine::scrollCredits() {
 
 		// Write the text appropriate for the next frame
 		if (idx < 400)
-			_screen->SHtransBlitFrom(creditsImages[0], Common::Point(10, 200 - idx), false, 0);
+			_screen->SHtransBlitFrom(creditsImages[0], Common::Point(10, 200 - idx), false);
 		if (idx > 200)
-			_screen->SHtransBlitFrom(creditsImages[1], Common::Point(10, 400 - idx), false, 0);
+			_screen->SHtransBlitFrom(creditsImages[1], Common::Point(10, 400 - idx), false);
 
 		// Don't show credit text on the top and bottom ten rows of the screen
 		_screen->SHblitFrom(_screen->_backBuffer1, Common::Point(0, 0), Common::Rect(0, 0, _screen->width(), 10));

engines/sherlock/surface.h
@@ -79,13 +79,25 @@ public:
 	 * Draws an image frame at a given position within this surface with transparency
 	 */
 	virtual void SHtransBlitFrom(const ImageFrame &src, const Common::Point &pt,
-		bool flipped = false, int overrideColor = 0, int scaleVal = SCALE_THRESHOLD);
+		bool flipped = false, int scaleVal = SCALE_THRESHOLD);
 
 	/**
 	 * Draws an image frame at a given position within this surface with transparency
 	 */
 	virtual void SHtransBlitFrom(const Graphics::Surface &src, const Common::Point &pt,
-		bool flipped = false, int overrideColor = 0, int scaleVal = SCALE_THRESHOLD);
+		bool flipped = false, int scaleVal = SCALE_THRESHOLD);
+
+	/**
+	 * Draws an image frame at a given position within this surface with transparency
+	 */
+	virtual void SHoverrideBlitFrom(const ImageFrame &src, const Common::Point &pt,
+		int overrideColor);
+
+	/**
+	 * Draws an image frame at a given position within this surface with transparency
+	 */
+	virtual void SHoverrideBlitFrom(const Graphics::Surface &src, const Common::Point &pt,
+		int overrideColor);
 
 	/**
 	 * Fill a given area of the surface with a given color

engines/sherlock/tattoo/tattoo_map.cpp
@@ -405,7 +405,7 @@ void TattooMap::showCloseUp(int closeUpNum) {
 			screen._currentScroll.y + closeUp.y / 100 - picSize.y / 2);
 
 		restoreArea(oldBounds);
-		screen._backBuffer1.SHtransBlitFrom(pic[0], pt, false, 0, scaleVal);
+		screen._backBuffer1.SHtransBlitFrom(pic[0], pt, false, scaleVal);
 
 		screen.slamRect(oldBounds);
 		screen.slamArea(pt.x, pt.y, picSize.x, picSize.y);

engines/sherlock/tattoo/tattoo_scene.cpp
@@ -142,14 +142,14 @@ void TattooScene::drawAllShapes() {
 			if (obj._quickDraw && obj._scaleVal == SCALE_THRESHOLD)
 				screen._backBuffer1.SHblitFrom(*obj._imageFrame, obj._position);
 			else
-				screen._backBuffer1.SHtransBlitFrom(*obj._imageFrame, obj._position, obj._flags & OBJ_FLIPPED, 0, obj._scaleVal);
+				screen._backBuffer1.SHtransBlitFrom(*obj._imageFrame, obj._position, obj._flags & OBJ_FLIPPED, obj._scaleVal);
 		}
 	}
 
 	// Draw the animation if it is behind the person
 	if (_activeCAnim.active() && _activeCAnim._zPlacement == BEHIND)
 		screen._backBuffer1.SHtransBlitFrom(_activeCAnim._imageFrame, _activeCAnim._position,
-			(_activeCAnim._flags & 4) >> 1, 0, _activeCAnim._scaleVal);
+			(_activeCAnim._flags & 4) >> 1, _activeCAnim._scaleVal);
 
 	screen.resetDisplayBounds();
 
@@ -196,11 +196,11 @@ void TattooScene::drawAllShapes() {
 				screen._backBuffer1.SHblitFrom(*se._shape->_imageFrame, se._shape->_position);
 			else
 				screen._backBuffer1.SHtransBlitFrom(*se._shape->_imageFrame, se._shape->_position,
-					se._shape->_flags & OBJ_FLIPPED, 0, se._shape->_scaleVal);
+					se._shape->_flags & OBJ_FLIPPED, se._shape->_scaleVal);
 		} else if (se._isAnimation) {
 			// It's an active animation
 			screen._backBuffer1.SHtransBlitFrom(_activeCAnim._imageFrame, _activeCAnim._position,
-				(_activeCAnim._flags & 4) >> 1, 0, _activeCAnim._scaleVal);
+				(_activeCAnim._flags & 4) >> 1, _activeCAnim._scaleVal);
 		} else {
 			// Drawing person
 			TattooPerson &p = *se._person;
@@ -212,7 +212,7 @@ void TattooScene::drawAllShapes() {
 			if (p._tempScaleVal == SCALE_THRESHOLD) {
 				p._tempX += adjust.x;
 				screen._backBuffer1.SHtransBlitFrom(*p._imageFrame, Common::Point(p._tempX, p._position.y / FIXED_INT_MULTIPLIER
-					- p.frameHeight() - adjust.y), p._walkSequences[p._sequenceNumber]._horizFlip, 0, p._tempScaleVal);
+					- p.frameHeight() - adjust.y), p._walkSequences[p._sequenceNumber]._horizFlip, p._tempScaleVal);
 			} else {
 				if (adjust.x) {
 					if (!p._tempScaleVal)
@@ -242,7 +242,7 @@ void TattooScene::drawAllShapes() {
 				}
 
 				screen._backBuffer1.SHtransBlitFrom(*p._imageFrame, Common::Point(p._tempX, p._position.y / FIXED_INT_MULTIPLIER
-					- p._imageFrame->sDrawYSize(p._tempScaleVal) - adjust.y), p._walkSequences[p._sequenceNumber]._horizFlip, 0, p._tempScaleVal);
+					- p._imageFrame->sDrawYSize(p._tempScaleVal) - adjust.y), p._walkSequences[p._sequenceNumber]._horizFlip, p._tempScaleVal);
 			}
 		}
 	}
@@ -256,13 +256,13 @@ void TattooScene::drawAllShapes() {
 			if (obj._quickDraw && obj._scaleVal == SCALE_THRESHOLD)
 				screen._backBuffer1.SHblitFrom(*obj._imageFrame, obj._position);
 			else
-				screen._backBuffer1.SHtransBlitFrom(*obj._imageFrame, obj._position, obj._flags & OBJ_FLIPPED, 0, obj._scaleVal);
+				screen._backBuffer1.SHtransBlitFrom(*obj._imageFrame, obj._position, obj._flags & OBJ_FLIPPED, obj._scaleVal);
 		}
 	}
 
 	// Draw the canimation if it is set as FORWARD
 	if (_activeCAnim.active() && _activeCAnim._zPlacement == FORWARD)
-		screen._backBuffer1.SHtransBlitFrom(_activeCAnim._imageFrame, _activeCAnim._position, (_activeCAnim._flags & 4) >> 1, 0, _activeCAnim._scaleVal);
+		screen._backBuffer1.SHtransBlitFrom(_activeCAnim._imageFrame, _activeCAnim._position, (_activeCAnim._flags & 4) >> 1, _activeCAnim._scaleVal);
 
 	// Draw all NO_SHAPE shapes which have their flag bits clear
 	for (uint idx = 0; idx < _bgShapes.size(); ++idx) {

commit 3dc1fd38dcfcb7d8eb02f4ff28fa9af2bc07a1cd

SHERLOCK: Fix obtaining the source pixel value for the 3DO version
This should fix one of the regressions caused by commit 31b7a3d37

engines/sherlock/surface.cpp
@@ -86,7 +86,7 @@ void BaseSurface::SHoverrideBlitFrom(const Graphics::Surface &src, const Common:
 
 	for (int y = 0; y < destRect.height(); y++) {
 		for (int x = 0; x < destRect.width(); x++) {
-			const uint8 srcVal = src.getPixel(srcRect.left + x, srcRect.top + y);
+			const uint32 srcVal = src.getPixel(srcRect.left + x, srcRect.top + y);
 			if (srcVal == transColor)
 				continue;
 			setPixel(destRect.left + x, destRect.top + y, overrideColor);