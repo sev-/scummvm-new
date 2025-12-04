sev-/scummvm-new

commit a765caeeb4677e25465f1dbfd868a37840fcd4ea

JANITORIAL: Add missing references to const function parameters
Applied to all 'const Common::' and 'const Graphics::' instances.

engines/pink/pda_mgr.cpp
@@ -94,7 +94,7 @@ void PDAMgr::execute(const Command &command) {
 	}
 }
 
-void PDAMgr::goToPage(const Common::String pageName) {
+void PDAMgr::goToPage(const Common::String &pageName) {
 	if (_page && !_page->getName().compareToIgnoreCase(pageName))
 		return;
 

engines/pink/pda_mgr.h
@@ -44,7 +44,7 @@ public:
 	void saveState(Archive &archive);
 
 	void execute(const Command &command);
-	void goToPage(const Common::String pageName);
+	void goToPage(const Common::String &pageName);
 
 	void update() { _cursorMgr.update(); }
 

commit 4b1f80d0933745558d92aec69d275d408cabe380

PINK: Fix regression introduced in a765caeeb4677e25465f1
This was leading to crash due to attempt to change const
memory location in serializer

engines/pink/pda_mgr.cpp
@@ -94,7 +94,7 @@ void PDAMgr::execute(const Command &command) {
 	}
 }
 
-void PDAMgr::goToPage(const Common::String &pageName) {
+void PDAMgr::goToPage(const Common::String pageName) {
 	if (_page && !_page->getName().compareToIgnoreCase(pageName))
 		return;
 

engines/pink/pda_mgr.h
@@ -44,7 +44,7 @@ public:
 	void saveState(Archive &archive);
 
 	void execute(const Command &command);
-	void goToPage(const Common::String &pageName);
+	void goToPage(const Common::String pageName);
 
 	void update() { _cursorMgr.update(); }
 