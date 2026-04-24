f2fdc0c18b7fee9363bf995cea25c71aaec6ec87
Simon Delamarre <simon.delamarre14@gmail.com>
2026-04-24 23:02:05 +0200

    GOB: Fix a delayed blit when a sound-only video is playing (Adi4)
    Fix text not being visible while the voice-over is not terminated, in the
    reading comprehension exercises of Adi4 / Language 8-9 years.

--- engines/gob/videoplayer.cpp
+++ engines/gob/videoplayer.cpp
@@ -177,6 +177,10 @@ int VideoPlayer::openVideo(bool primary, const Common::String &file, Properties 
 		if (!(video->decoder = openVideo(file, properties)))
 			return -1;
 
+		if (!(properties.flags & kFlagNoVideo) && !video->decoder->hasVideo()
+				&& (_vm->getGameType() == kGameTypeAdibou2 || _vm->getGameType() == kGameTypeAdi4)) // TODO: May be needed by other games
+			_vm->_draw->blitInvalidated();
+
 		if (video->decoder->hasVideo() && !(properties.flags & kFlagNoVideo) &&
 		    (video->decoder->isPaletted() != !_vm->isTrueColor())) {
 			if (properties.switchColorMode) {
