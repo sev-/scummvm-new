sev-/scummvm-new

commit a16e412195b67e87d647b3ceeef0649b5c6d2b6d

NANCY: Add a sanity check for checking for held items in Nancy10+
There's a special case used in Nancy10+, where held items with a
large item ID are checked, when adding tasks to the notebook. Add
a sanity check for now, to avoid crashing

engines/nancy/state/scene.cpp
@@ -392,8 +392,17 @@ void Scene::setNoHeldItem() {
 byte Scene::hasItem(int16 id) const {
 	if (getHeldItem() == id) {
 		return g_nancy->_true;
-	} else {
+	} else if (id > 0 && (uint)id < _flags.items.size()) {
 		return _flags.items[id];
+	} else {
+		// TODO: Happens in Nancy10+. Gets called for item IDs
+		// 1824, 1825, 1826, 1827, when adding tasks to the
+		// notebook, for an array of 70 items in total. Looks
+		// like a case where a flag is contained for the held
+		// item ID to be checked.
+		debug(2, "Scene::hasItem: out-of-range id %d (items.size=%u)", id,
+			  (uint)_flags.items.size());
+		return g_nancy->_false;
 	}
 }
 

commit b9a08b432863a50ff0b73f006212a9ed048a75b4

NANCY: Fix an off-by-one check in Scene::hasItem()
Check introduced in a16e412

engines/nancy/state/scene.cpp
@@ -392,7 +392,7 @@ void Scene::setNoHeldItem() {
 byte Scene::hasItem(int16 id) const {
 	if (getHeldItem() == id) {
 		return g_nancy->_true;
-	} else if (id > 0 && (uint)id < _flags.items.size()) {
+	} else if (id >= 0 && (uint)id < _flags.items.size()) {
 		return _flags.items[id];
 	} else {
 		// TODO: Happens in Nancy10+. Gets called for item IDs